ffbad3eaef8c27e7a19ffc9eac9ee22e01c14dcf
manu <emmanuel.marie@esperantotech.com>
2018-09-21 17:17:09 +0200

    [sw-sysemu] Reverse ET atomics operands semantic
    Former-commit-id: ad7e7db83e6e3b5510e5db90ab14e6a1ab64a077 [formerly 73c6b3579011ddf2b9f8298348137d7908691cb5]
    Former-commit-id: 5f098b83100b0eb356fec636f942f50ca2994199

--- sw-sysemu/emu_defines.h
+++ sw-sysemu/emu_defines.h
@@ -653,7 +653,7 @@ DECLARE_TRAP_TVAL_Y(CAUSE_MCODE_INSTRUCTION,    trap_mcode_instruction)
 // Atomics functions
 //
 
-#define ET_AMO_EMU_W_FUNC(NAME, OPC) \
+#define AMO_EMU_W_FUNC(NAME, OPC) \
 void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
 {\
    DISASM(gsprintf(dis,"I: " #NAME " x%d, x%d, (x%d)%s%s", dst, src1, src2, comm ? " # " : "", comm ? comm : "");)\
@@ -661,7 +661,7 @@ void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
    amo_emu_w(OPC, dst, src1, src2);\
 }
 
-#define ET_AMO_EMU_D_FUNC(NAME, OPC) \
+#define AMO_EMU_D_FUNC(NAME, OPC) \
 void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
 {\
    DISASM(gsprintf(dis,"I: " #NAME " x%d, x%d, (x%d)%s%s", dst, src1, src2, comm ? " # " : "", comm ? comm : "");)\
@@ -669,7 +669,7 @@ void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
    amo_emu_d(OPC, dst, src1, src2);\
 }
 
-#define ET_AMO_EMU_F_FUNC(NAME, OPC) \
+#define AMO_EMU_F_FUNC(NAME, OPC) \
 void NAME(freg dst, freg src1, xreg src2, const char* comm)\
 {\
    DISASM(gsprintf(dis,"I: " #NAME " ft%d, ft%d, (x%d)%s%s", dst, src1, src2, comm ? " # " : "", comm ? comm : "");)\
@@ -677,22 +677,6 @@ void NAME(freg dst, freg src1, xreg src2, const char* comm)\
    amo_emu_f(OPC, dst, src1, src2);\
 }
 
-// RISC-V A extension uses opposite src1/src2 semantic compared to ET Atomics
-#define RV_AMO_EMU_W_FUNC(NAME, OPC) \
-void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
-{\
-   DISASM(gsprintf(dis,"I: " #NAME " x%d, x%d, (x%d)%s%s", dst, src1, src2, comm ? " # " : "", comm ? comm : "");)\
-   DEBUG_EMU(gprintf("%s\n",dis);)\
-   amo_emu_w(OPC, dst, src2, src1);\
-}
-
-#define RV_AMO_EMU_D_FUNC(NAME, OPC) \
-void NAME(xreg dst, xreg src1, xreg src2, const char* comm)\
-{\
-   DISASM(gsprintf(dis,"I: " #NAME " x%d, x%d, (x%d)%s%s", dst, src1, src2, comm ? " # " : "", comm ? comm : "");)\
-   DEBUG_EMU(gprintf("%s\n",dis);)\
-   amo_emu_d(OPC, dst, src2, src1);\
-}
 
 #endif // _EMU_DEFINES_H
 
