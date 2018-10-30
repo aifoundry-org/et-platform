9a357b2ba6d0ad3c98349025a84259189b1848e8
Sebastia Tortella <sebastia.tortella@esperantotech.com>
2018-10-30 15:00:10 +0100

    [sw-sysemu] adusting log levels
    Former-commit-id: 5ffb22ea8edc7ad3e24837dfbd667a250303ab29

--- sw-sysemu/sys_emu/sys_emu_log.cpp
+++ sw-sysemu/sys_emu/sys_emu_log.cpp
@@ -2,9 +2,8 @@
 
 // static members of testLog
 logLevel testLog::globalLogLevel_ = LOG_DEBUG;
-bool testLog::globalLogLevelSet_ = false;
 logLevel testLog::defaultLogLevel_ = LOG_INFO;
-bool testLog::defaultLogLevelSet_ = false;
+bool testLog::logLevelsSet_ = false;
 unsigned testLog::errors_ = 0;
 unsigned testLog::maxErrors_ = (unsigned) -1;
 
@@ -15,14 +14,9 @@ void endSim() { exit(1); }
 bool simEnded(){  return false; }
 uint64_t testLog::simTime() { return 0; }
 
-void testLog::setGlobalLogLevel() {
-  globalLogLevelSet_ = true;
+void testLog::setLogLevels() {
+  logLevelsSet_ = true;
 }
-
-void testLog::setDefaultLogLevel() {
-  defaultLogLevelSet_ = true;
-}
-
 
 std::string testLog::simTimeStr(){
   return std::string ("");
