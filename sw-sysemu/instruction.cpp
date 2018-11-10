59464946946a3a2d5b04ff2be5da21f77d5b9fc4
Guillem Sole <guillem.sole@esperantotech.com>
2018-11-10 21:47:30 +0100

    [sw-sysemu] - Fixes in EMU for TensorQuant - Started to add validation plumbing for TensorQuant - TensorQuant FSM in RTL starts sending some operations - Now F6MP stage correctly muxes all the signals between F5 and F6 - Some fixes related to F6MP stage in minion monitor - Fixed a bug for TensorQuant and TensorFMA where one VPU instruction   could go to pipeline if it was executed right after the TQuant or TFMA
    Former-commit-id: 8fef62dd598c94b017814585e1c37950dc7afe26 [formerly f6833d02505ca77a8f08c2059a2943b09b03bfe9]
    Former-commit-id: 22579052e344ffd582709b20b125cff4d7881568

--- sw-sysemu/instruction.cpp
+++ sw-sysemu/instruction.cpp
@@ -643,7 +643,8 @@ static const emu_opnd_hash_t operand_cache({
     {"tensor_conv_ctrl", csr_tconvctrl},
     {"tensor_coop",      csr_tcoop},
     {"tensor_mask",      csr_tmask},
-    //"tensor_quant",
+    {"tensor_quant",     csr_tquant},
+    {"unknown_806",      csr_tquant},
     {"tex_send",         csr_texsend},
     {"tensor_error",     csr_terror},
     {"scratchpad_ctrl",  csr_scpctrl},
@@ -1189,11 +1190,12 @@ void instruction::set_mnemonic(std::string mnemonic_)
     // Checks if it is a tensor/reduce operation
     if ((opcode == "csrrw") || (opcode == "cssrwi"))
     {
-        is_reduce      = (params[1] == csr_treduce);
-        is_tensor_load = (params[1] == csr_tloadctrl);
-        is_tensor_fma  = (params[1] == csr_tfmastart);
-        is_flb         = (params[1] == csr_flbarrier);
-        is_fcc         = (params[1] == csr_fccounter);
+        is_reduce       = (params[1] == csr_treduce);
+        is_tensor_load  = (params[1] == csr_tloadctrl);
+        is_tensor_fma   = (params[1] == csr_tfmastart);
+        is_tensor_quant = (params[1] == csr_tquant);
+        is_flb          = (params[1] == csr_flbarrier);
+        is_fcc          = (params[1] == csr_fccounter);
     }
 
     // Get the emulation function pointer for the opcode
