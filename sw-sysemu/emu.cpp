8576cece70d423009508029ad128a630274eb226
rafa <rafael.jimenez@esperantotech.com>
2019-01-16 14:55:09 +0100

    [sw-sysemu] Change WFI by wfi avoid conflicts with include/et_test_common.h On emu use LOG instead of LOG_ALL when modify FastlocalBarrier.
    Former-commit-id: e21515bfd6281f3ee4521569b5a1931a16266624 [formerly e49ad9e26d7bba16dd7e1a3647cce1e372e5f70d]
    Former-commit-id: 2672139260f4039c576176d4f83bd3561a2dc9f0

--- sw-sysemu/emu.cpp
+++ sw-sysemu/emu.cpp
@@ -8141,19 +8141,23 @@ static uint64_t flbarrier(uint64_t value)
     uint64_t orig_value = vmemread64(addr);
     uint64_t result = -1;
 
-    LOG_ALL_MINIONS(DEBUG,"FastLocalBarrier: Shire %i: Minion %i Thread %i doing barrier %" PRIu64 " value  %" PRIu64 ", limit %" PRIu64 " ",
+    //    LOG_ALL_MINIONS(DEBUG,"FastLocalBarrier: Shire %i: Minion %i Thread %i doing barrier %" PRIu64 " value  %" PRIu64 ", limit %" PRIu64 " ",
+    LOG(DEBUG,"FastLocalBarrier: Shire %i: Minion %i Thread %i doing barrier %" PRIu64 " value  %" PRIu64 ", limit %" PRIu64 " ",
         (int) shire, current_thread / EMU_THREADS_PER_MINION, current_thread % EMU_THREADS_PER_MINION, barrier, orig_value, limit );
     // Last guy, return 1 and zero barrier
     if (orig_value == limit)
     {
-        LOG_ALL_MINIONS(DEBUG,"FastLocalBarrier: last minion Shire %i!!", (int) shire);
+        //LOG_ALL_MINIONS(DEBUG,"FastLocalBarrier: last minion Shire %i!!", (int) shire);
+        LOG(DEBUG,"FastLocalBarrier: last minion Shire %i!!", (int) shire);        
+        
         vmemwrite64(addr, 0);
         result = 1;
     }
     // Not the last guy, return 0 and increment barrier
     else
     {
-        LOG_ALL_MINIONS(DEBUG, "FastLocalBarrier: Limit %" PRIu64", Incrementing to %" PRIu64 "!!", limit, orig_value + 1);
+        //LOG_ALL_MINIONS(DEBUG, "FastLocalBarrier: Limit %" PRIu64", Incrementing to %" PRIu64 "!!", limit, orig_value + 1);
+        LOG(DEBUG, "FastLocalBarrier: Limit %" PRIu64", Incrementing to %" PRIu64 "!!", limit, orig_value + 1);        
         vmemwrite64(addr, orig_value + 1);
         result = 0;
     }
