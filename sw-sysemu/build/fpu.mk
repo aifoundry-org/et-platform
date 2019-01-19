12fac61667f6ba5146fc85418475256d8eec3f4e
Grigorios Magklis <grigorios.magklis@esperantotech.com>
2019-01-19 02:21:25 +0100

    [sw-sysemu] BEMU: rework build process
    - The FPU emulation code is built as a separate library (libfpu) from libemu.
    - 'sys_emu' and 'checker' do not link libemu. They link libfpu and compile all
      the necessary files directly into their build. This allows using the SYS_EMU
      define in the BEMU source code.
    - 'sys_emu' does not link with libbfd and libopcode anymore. It uses the BEMU
      disassembly instead.
    - libemu should now be deprecated and should not be built anymore, but some of
      the tbox testbenches need RBOX/TBOX emulation functions that are currently
      in libemu. Eventually we should compile this functionality directly into the
      testbench and stop using libemu.
    
    
    Former-commit-id: f3b1462b8cc2c21c321f47825f18bd2020291134 [formerly 46db2fefe2b60192ea936ecef005f89d229432ee]
    Former-commit-id: 62a95720705fc6b3dad84a380f331daf29ffeb3a

---
+++ sw-sysemu/build/fpu.mk
@@ -0,0 +1,16 @@
+fpu_hdrs := \
+	fpu/cvt.h \
+	fpu/fpu.h \
+	fpu/fpu_casts.h \
+	fpu/fpu_types.h \
+	fpu/texp.h \
+	fpu/tlog.h \
+	fpu/trcp.h \
+	fpu/trsqrt.h \
+	fpu/tsin.h \
+	fpu/ttrans.h
+
+fpu_cpp_srcs := \
+	fpu/cvt.cpp \
+	fpu/fpu.cpp \
+	fpu/ttrans.cpp
