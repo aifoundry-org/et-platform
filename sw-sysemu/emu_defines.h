099f60cd0896f68811734eaa3bbcb686c6879c22
Shankar Jayaratnam <shankar.jayaratnam@esperantotech.com>
2019-02-01 12:41:17 -0800

    [sw-sysemu] Fixing BEMU # of Shires
    Former-commit-id: 72feefa8c97ac0d90cca33ebd423de6a105f2545 [formerly 1834b6fbce5308d0aba174ef25c1e6d470891b81]
    Former-commit-id: 07db1526f699eede56d23a28bc0604548fe49b64

--- sw-sysemu/emu_defines.h
+++ sw-sysemu/emu_defines.h
@@ -5,10 +5,10 @@
 #include <bitset>
 
 // Maximum number of threads
-#define EMU_NUM_SHIRES          35 // at most 33 shires (Pending fix from JIRA: RTLMIN-1949)
+#define EMU_NUM_SHIRES          34 // 32 Compute + Master + IO Shire SP 
 #define EMU_NUM_COMPUTE_SHIRES  32
 #define EMU_MASTER_SHIRE        32
-#define EMU_IO_SHIRE_SP		256
+#define EMU_IO_SHIRE_SP		34
 #define EMU_NEIGH_PER_SHIRE     4
 #define EMU_MINIONS_PER_NEIGH   8
 #define EMU_TBOXES_PER_SHIRE    4
