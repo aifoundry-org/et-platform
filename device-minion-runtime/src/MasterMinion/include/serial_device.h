5d50c36ec148e5da968219031138272e8dd6a690
Doug Crowder <doug.crowder@esperantotech.com>
2019-02-28 21:30:37 +0000

    [device-minion-runtime] -Print to all 4 UARTs on SP startup (SP 0/1 and PU 0/1) -Switch to Semifore generated UART header. manually checked in with custom include statement for now.

--- device-minion-runtime/src/MasterMinion/include/serial_device.h
+++ device-minion-runtime/src/MasterMinion/include/serial_device.h
@@ -3,9 +3,11 @@
 #ifndef SERIAL_DEVICE_H
 #define SERIAL_DEVICE_H
 
-#include "UART.h"
+#include "DW_apb_uart.h"
 
-#define PU_UART_BASE_ADDRESS 0x0012002000ULL
-#define UART0 ((volatile SPIO_UART_t*)PU_UART_BASE_ADDRESS)
+#define UART0_BASE_ADDRESS 0x0012002000ULL
+#define UART1_BASE_ADDRESS 0x0012007000ULL
 
+#define UART0 ((volatile Uart_t*)UART0_BASE_ADDRESS)
+#define UART1 ((volatile Uart_t*)UART1_BASE_ADDRESS)
 #endif
