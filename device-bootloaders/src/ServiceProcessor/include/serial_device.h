ab6e3bc1e68d7a0a20ae8184c583b700d0d69beb
Doug Crowder <doug.crowder@esperantotech.com>
2019-02-28 21:30:37 +0000

    [device-bootloaders] -Print to all 4 UARTs on SP startup (SP 0/1 and PU 0/1) -Switch to Semifore generated UART header. manually checked in with custom include statement for now.

--- device-bootloaders/src/ServiceProcessor/include/serial_device.h
+++ device-bootloaders/src/ServiceProcessor/include/serial_device.h
@@ -3,9 +3,16 @@
 #ifndef SERIAL_DEVICE_H
 #define SERIAL_DEVICE_H
 
-#include "UART.h"
+#include "DW_apb_uart.h"
 
 #define SPIO_UART0_BASE_ADDRESS 0x0052022000ULL
-#define UART0 ((volatile SPIO_UART_t*)SPIO_UART0_BASE_ADDRESS)
+#define SPIO_UART1_BASE_ADDRESS 0x0054052000ULL
+#define PU_UART0_BASE_ADDRESS   0x0012002000ULL
+#define PU_UART1_BASE_ADDRESS   0x0012007000ULL
+
+#define UART0 ((volatile Uart_t*)SPIO_UART0_BASE_ADDRESS)
+#define UART1 ((volatile Uart_t*)SPIO_UART1_BASE_ADDRESS)
+#define PU_UART0 ((volatile Uart_t*)PU_UART0_BASE_ADDRESS)
+#define PU_UART1 ((volatile Uart_t*)PU_UART1_BASE_ADDRESS)
 
 #endif
