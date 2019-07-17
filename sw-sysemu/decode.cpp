77f94e79ee8b1903559a6a6730674b9e39c00ba1
Jordi <jordi.munoz@esperantotech.com>
2019-07-17 15:40:15 +0200

    [sw-sysemu] ARCHSIM-485: Removing flag_1ULP for FEXP, so checker only allows 0 ULP between BEMU and RTL. Verified against resnet50 softmax layer inputs

--- sw-sysemu/decode.cpp
+++ sw-sysemu/decode.cpp
@@ -785,7 +785,9 @@ static insn_exec_funct_t dec_custom3(uint32_t bits, uint16_t& flags)
                case 0x01: return insn_fround_ps;
                case 0x02: return (funct3 == 0) ? insn_ffrc_ps : insn_unknown;
                case 0x03: return (funct3 == 0) ? insn_flog_ps : insn_unknown;
-               case 0x04: flags |= insn_t::flag_1ULP; return (funct3 == 0) ? insn_fexp_ps : insn_unknown;
+               case 0x04: return (funct3 == 0) ? insn_fexp_ps : insn_unknown;
+
+
                case 0x06: flags |= insn_t::flag_1ULP; return (funct3 == 0) ? insn_fsin_ps : insn_unknown;
                case 0x07: return (funct3 == 0) ? insn_frcp_ps : insn_unknown;
                case 0x08: flags |= insn_t::flag_1ULP; return (funct3 == 0) ? insn_frsq_ps : insn_unknown;
