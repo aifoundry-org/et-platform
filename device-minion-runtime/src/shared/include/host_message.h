fe1c9eec852326d29b29362a50dfdbe2a42997bd
Ioannis Doudalis <ioannis.doudalis@esperantotech.com>
2019-07-29 16:59:32 -0700

    [device-minion-runtime] [MM] Move the host_message_t in host_message.h
    To each type exchange with the host runtime create host_message.h
    header and move there the host_message_t type declaration. This
    header will be included by the runtime in order to populate the
    same message.

---
+++ device-minion-runtime/src/shared/include/host_message.h
@@ -0,0 +1,15 @@
+#ifndef HOST_MESSAGE_H
+#define HOST_MESSAGE_H
+
+#include "kernel_params.h"
+#include "kernel_info.h"
+#include "mailbox_id.h"
+
+typedef struct {
+    mbox_message_id_t message_id;
+    kernel_params_t kernel_params;
+    kernel_info_t kernel_info;
+} host_message_t;
+
+
+#endif // HOST_MESSAGE_H
