0afe26e99dbb06dededf9ec58c454da86a1bcd5a
Doug Crowder <doug.crowder@esperantotech.com>
2019-08-30 21:36:27 +0000

    [test-compute-kernels] Invalidate shared L1 I-cache before kernel launch BEEF kernel cleanup - only 1 minion runs, read back and compare write results

--- test-compute-kernels/beef/beef.c
+++ test-compute-kernels/beef/beef.c
@@ -1,4 +1,5 @@
 #include "kernel_params.h"
+#include "hart.h"
 
 #include <stdint.h>
 #include <stddef.h>
@@ -8,6 +9,12 @@
 // tensor_b = length
 int64_t main(const kernel_params_t* const kernel_params_ptr)
 {
+    // Only run on one minion
+    if (get_hart_id() != 0)
+    {
+        return 0;
+    }
+
     if ((kernel_params_ptr == NULL) ||
         ((uint64_t*)kernel_params_ptr->tensor_a == NULL) ||
         (kernel_params_ptr->tensor_b == 0))
@@ -16,7 +23,7 @@ int64_t main(const kernel_params_t* const kernel_params_ptr)
         return -1;
     }
 
-    uint64_t* data_ptr = (uint64_t*)kernel_params_ptr->tensor_a;
+    volatile uint64_t* data_ptr = (uint64_t*)kernel_params_ptr->tensor_a;
     const uint64_t length = kernel_params_ptr->tensor_b;
 
     if ((uint64_t)data_ptr % 8)
@@ -28,6 +35,18 @@ int64_t main(const kernel_params_t* const kernel_params_ptr)
     for (uint64_t i = 0; i < (length / 8); i++)
     {
         *data_ptr++ = 0xBEEFBEEFBEEFBEEFULL;
+    }
+
+    data_ptr = (uint64_t*)kernel_params_ptr->tensor_a;
+
+    // Read back data and return comparison pass/fail
+    for (uint64_t i = 0; i < (length / 8); i++)
+    {
+        if (*data_ptr++ != 0xBEEFBEEFBEEFBEEFULL)
+        {
+            // Miscompare
+            return -3;
+        }
     }
 
     return 0;
