e5dbb85d8c476c5a0435b657c63a506217607fcb
Patryk Kaminski <patryk.kaminski@esperantotech.com>
2019-09-09 16:08:40 +0000

    [device-bootloaders] port MESSAGE_xxx macros from SP_ROM

--- device-bootloaders/src/ServiceProcessorBL1/include/bl1_main.h
+++ device-bootloaders/src/ServiceProcessorBL1/include/bl1_main.h
@@ -12,8 +12,21 @@
 #ifndef __BL1_MAIN_H__
 #define __BL1_MAIN_H__
 
+//#define RELEASE_BUILD
+
 #include "service_processor_BL1_data.h"
 
 SERVICE_PROCESSOR_BL1_DATA_t * get_service_processor_bl1_data(void);
+
+#ifdef RELEASE_BUILD
+#define MESSAGE_ERROR_DEBUG(cformat, ...)
+#define MESSAGE_INFO_DEBUG(cformat, ...)
+#else
+#define MESSAGE_ERROR_DEBUG(cformat, ...) printx(cformat, ##__VA_ARGS__)
+#define MESSAGE_INFO_DEBUG(cformat, ...) printx(cformat, ##__VA_ARGS__)
+#endif
+
+#define MESSAGE_ERROR(cformat, ...) printx(cformat, ##__VA_ARGS__)
+#define MESSAGE_INFO(cformat, ...) printx(cformat, ##__VA_ARGS__)
 
 #endif
