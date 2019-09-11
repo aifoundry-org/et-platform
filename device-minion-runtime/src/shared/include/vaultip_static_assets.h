66e881266dd953f791435a97e38da3f6be256e79
Patryk Kaminski <patryk.kaminski@esperantotech.com>
2019-09-11 12:46:52 +0000

    [device-minion-runtime] port cryptographic API code from SP ROM to BL1/BL2

--- device-minion-runtime/src/shared/include/vaultip_static_assets.h
+++ device-minion-runtime/src/shared/include/vaultip_static_assets.h
@@ -10,6 +10,12 @@ typedef enum VAULTIP_STATIC_ASSET_ID_e {
     VAULTIP_STATIC_ASSET_AUTHKEY_DYNAMIC_ASSET_UNWRAP_KEY,  // AES-256 Secure Debug Auth Key Dynamic Asset unwrap key
     VAULTIP_STATIC_ASSET_PCI_CFG_DATA_REVOCATION_COUNTER,   // monotonic counter - minimum version of PCIe PHY config data
     VAULTIP_STATIC_ASSET_SP_BL1_REVOCATION_COUNTER,         // monotonic counter - minimum version of SP BL1 image
+    // the following values will be used by later stage firmware (post SP-ROM)
+    VAULTIP_STATIC_ASSET_SP_BL2_REVOCATION_COUNTER,         // monotonic counter - minimum version of SP BL2 image
+    VAULTIP_STATIC_ASSET_MACHINE_MINION_REVOCATION_COUNTER, // monotonic counter - minimum version of Machine Minion image
+    VAULTIP_STATIC_ASSET_MASTER_MINION_REVOCATION_COUNTER,  // monotonic counter - minimum version of Master Minion image
+    VAULTIP_STATIC_ASSET_WORKER_MINION_REVOCATION_COUNTER,  // monotonic counter - minimum version of Worker Minion image
+    VAULTIP_STATIC_ASSET_MAXION_BL1_REVOCATION_COUNTER,     // monotonic counter - minimum version of Maxion BL1 image
     VAULTIP_STATIC_ASSET_COUNT
 } VAULTIP_STATIC_ASSET_ID_t;
 
