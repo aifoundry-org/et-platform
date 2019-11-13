df03aeac1800e2947cc2faec4884bbf807331e74
Patryk Kaminski <patryk.kaminski@esperantotech.com>
2019-11-13 15:39:44 +0000

    [device-minion-runtime] port latest low-voltage PLL and DLL code from DV into SP BL2

--- device-minion-runtime/src/shared/include/minion_esr_defines.h
+++ device-minion-runtime/src/shared/include/minion_esr_defines.h
@@ -11,14 +11,18 @@
 } esr_shire_config_t;*/
 /* ESR values */
 #define ESR_SHIRE_CONFIG_32_EN  0x001f20
-#define ESR_SHIRE_CONFIG_EN  0x000100
+#define ESR_SHIRE_CONFIG_32_REMAP  0x001f00
 #define ESR_SHIRE_CONFIG_32_DIS  0x000120
 #define ESR_SHIRE_CONFIG_0_EN  0x001f00
+#define ESR_SHIRE_CONFIG_0_REMAP  0x001f20
 #define ESR_SHIRE_CONFIG_0_DIS 0x000100
-/* ESR addresses */
+#define ESR_SHIRE_CONFIG_EN  0x000100
+/* ESR addresses: REGION_NEIGH */
+#define MINION_BOOT 0x1e003
+/* ESR addresses: REGION_OTHER */
 #define SHIRE_OTHER_CONFIG 0x08001
 #define SHIRE_PLL_READ_DATA 0x08051
-#define SHIRE_DLL_READ_DATA 0x0805b
+#define SHIRE_DLL_READ_DATA 0x0805c
 #define SHIRE_OTHER_THREAD0_DISABLE 0x08048
 #define SHIRE_OTHER_THREAD1_DISABLE 0x08002
 #define SHIRE_OTHER_CTRL_CLOCKMUX 0x08053
@@ -28,20 +32,43 @@
 #define SHIRE_OTHER_PLL_CONFIG_DATA_1 0x0804c
 #define SHIRE_OTHER_PLL_CONFIG_DATA_2 0x0804d
 #define SHIRE_OTHER_PLL_CONFIG_DATA_3 0x0804e
-#define SHIRE_OTHER_PLL_CONFIG_DATA_4 0x0804f
-#define SHIRE_OTHER_PLL_CONFIG_DATA_5 0x08050
 #define SHIRE_OTHER_DLL_CONFIG_DATA_0 0x0805a
+#define SHIRE_OTHER_DLL_CONFIG_DATA_1 0x0805b
+
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_PCLK_SEL_OFF   15
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_LOCK_RST_OFF   14
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_REG_NUM_OFF    10
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_REG_FIRST_OFF  4
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_WRITE_OFF      3
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_RUN_OFF        2
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_ENABLE_OFF     1
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_RESET_OFF      0
+
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_DLL_EN_OFF     13
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_PCLK_SEL_OFF   11
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_LOCK_RST_OFF   10
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_REG_NUM_OFF    7
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_REG_FIRST_OFF  4
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_WRITE_OFF      3
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_RUN_OFF        2
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_ENABLE_OFF     1
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_RESET_OFF      0
+
+#define SHIRE_OTHER_PLL_AUTO_CONFIG_PCLK_SEL       (2 << SHIRE_OTHER_PLL_AUTO_CONFIG_PCLK_SEL_OFF)
+#define SHIRE_OTHER_DLL_AUTO_CONFIG_PCLK_SEL       (2 << SHIRE_OTHER_DLL_AUTO_CONFIG_PCLK_SEL_OFF)
+
 /*typedef struct packed {
       logic [1:0] pclk_sel = 0
       logic lock_reset_disable = 0
-      logic [4:0] reg_last = 0x16
-      logic [4:0] reg_first = 00
+      logic [3:0] reg_num = 0x16
+      logic [5:0] reg_first = 00
       logic write = 0
       logic run = 0
       logic enable = 0
       logic reset = 1
-} esr_pll_auto_config_t;
-   typedef struct packed {
+} esr_pll_auto_config_t;*/
+
+/*typedef struct packed {
       logic dll_enable;
       logic [1:0] pclk_sel;
       logic lock_reset_disable;
@@ -53,14 +80,3 @@
       logic reset;
    } esr_dll_auto_config_t;
 */
-#define  REG_SHIRE_PLL_AUTO_CONFIG_SHADOW1  0x02c01
-#define  REG_SHIRE_PLL_AUTO_CONFIG_SHADOW2  0x02c02
-#define  REG_SHIRE_PLL_AUTO_CONFIG_SHADOW3  0x02c0e
-#define  REG_SHIRE_PLL_AUTO_CONFIG_SHADOW4  0x02c00
-#define  REG_SHIRE_PLL_AUTO_CONFIG_SHADOW5  0x02c06
-
-#define  REG_SHIRE_DLL_AUTO_CONFIG_SHADOW1  0x0181
-#define  REG_SHIRE_DLL_AUTO_CONFIG_SHADOW2  0x0182
-#define  REG_SHIRE_DLL_AUTO_CONFIG_SHADOW3  0x018e
-#define  REG_SHIRE_DLL_AUTO_CONFIG_SHADOW4  0x0180
-#define  REG_SHIRE_DLL_AUTO_CONFIG_SHADOW5  0x0186
