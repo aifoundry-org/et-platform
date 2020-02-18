a8efbf558bfaa41923642501ad57d8d72ac293b4
Sergi Granell <sergi.granell@esperantotech.com>
2020-02-18 11:32:43 +0100

    [device-minion-runtime] [SW-2012] Remove unused syscall aliases
    After separating the U-to-S and S-to-M
    syscall into different files and before
    updating the kernels to use the updated ones,
    some aliases had to be created.

--- device-minion-runtime/src/shared/include/syscall.h
+++ device-minion-runtime/src/shared/include/syscall.h
@@ -3,20 +3,14 @@
 
 #include <stdint.h>
 
-// [SW-2012] TODO: Remove after Neuralizer is updated
-#define SYSCALL_DRAIN_COALESCING_BUFFER_ALT SYSCALL_DRAIN_COALESCING_BUFFER
-#define SYSCALL_EVICT_L1_ALT                SYSCALL_EVICT_L1
-#define SYSCALL_SHIRE_CACHE_BANK_OP_ALT     SYSCALL_SHIRE_CACHE_BANK_OP
-
 enum {
     // From U-mode to S-mode for compute firmware [300-399]
-    //SYSCALL_KERNEL_CLEAR            = 300,
+    SYSCALL_RETURN_FROM_KERNEL      = 300,
     SYSCALL_DRAIN_COALESCING_BUFFER = 301,
     SYSCALL_CACHE_CONTROL           = 302,
     SYSCALL_FLUSH_L3                = 303,
     SYSCALL_SHIRE_CACHE_BANK_OP     = 304,
     SYSCALL_EVICT_L1                = 305,
-    SYSCALL_RETURN_FROM_KERNEL      = 306,
 
     // From U-mode to S-mode for both master and compute firmwares [400-499]
     SYSCALL_LOG_WRITE               = 400,
