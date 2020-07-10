f2f8a5b866e6493b66120ee94327f1267ab487cb
Sergi Granell <sergi.granell@esperantotech.com>
2020-07-10 08:54:07 +0200

    [device-minion-runtime] Master, Worker: Log when unexpected exception happens

--- device-minion-runtime/src/MasterMinion/src/exception.c
+++ device-minion-runtime/src/MasterMinion/src/exception.c
@@ -1,6 +1,14 @@
+#include "log.h"
+#include "macros.h"
+
 void exception_handler(void);
 
 void exception_handler(void)
 {
     // TODO exception decoding
+    uint64_t scause;
+    asm volatile("csrr %0, scause\n" : "=r"(scause));
+
+    log_write(LOG_LEVEL_CRITICAL, "MasterMinon exception: unhandled exception: %lx\n", scause);
+    C_TEST_FAIL
 }
--- device-minion-runtime/src/WorkerMinion/src/exception.c
+++ device-minion-runtime/src/WorkerMinion/src/exception.c
@@ -1,6 +1,14 @@
+#include "log.h"
+#include "macros.h"
+
 void exception_handler(void);
 
 void exception_handler(void)
 {
     /* Should never get here, ecalls are handled elsewhere and no other exceptions are delegated to supervisor */
+    uint64_t scause;
+    asm volatile("csrr %0, scause\n" : "=r"(scause));
+
+    log_write(LOG_LEVEL_CRITICAL, "WorkerMinon exception: unhandled exception: %lx\n", scause);
+    C_TEST_FAIL
 }
