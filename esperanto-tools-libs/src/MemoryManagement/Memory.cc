391c178a9864d2c12b47ae446926d25d3203648b
Ioannis Doudalis <ioannis.doudalis@esperantotech.com>
2020-07-26 02:36:56 +0000

    [esperanto-tools-libs] [SW-1292] Create DeviceBuffer smart pointer
    This commit introduces class DeviceBuffer that will become
    the return value of all memory allocations from the runtime
    and will support both "smart" and "fat" pointerr semantics moving
    formward.
    
    As of now the smart-pointer implementation is complete and the
    associated unit-tests have been introcuded.

---
+++ esperanto-tools-libs/src/MemoryManagement/Memory.cc
@@ -0,0 +1,102 @@
+//******************************************************************************
+// Copyright (C) 2020, Esperanto Technologies Inc.
+// The copyright to the computer program(s) herein is the2
+// property of Esperanto Technologies, Inc. All Rights Reserved.
+// The program(s) may be used and/or copied only with
+// the written permission of Esperanto Technologies and
+// in accordance with the terms and conditions stipulated in the
+// agreement/contract under which the program(s) have been supplied.
+//------------------------------------------------------------------------------
+
+#include "esperanto/runtime/Core/Memory.h"
+#include "esperanto/runtime/Core/MemoryManager.h"
+#include "esperanto/runtime/Support/Logging.h"
+
+namespace et_runtime {
+
+DeviceBuffer::DeviceBuffer()
+    : buffer_id_(0), offset_(0), ref_cntr_(nullptr), deallocator_(nullptr) {}
+
+DeviceBuffer::DeviceBuffer(BufferID id, BufferOffsetTy offset,
+                           Deallocator *deallocator)
+    : buffer_id_(id), offset_(offset), ref_cntr_(new RefCounter()),
+      deallocator_(deallocator) {
+  ref_cntr_->inc();
+}
+
+DeviceBuffer::~DeviceBuffer() {
+  auto res = ReleaseBuffer();
+  if (!res) {
+    RTERROR << "Failed to de-allocate buffer: " << buffer_id_ << "\n";
+    std::terminate();
+  }
+}
+
+DeviceBuffer::DeviceBuffer(const DeviceBuffer &other) {
+  copyBuffer(other);
+  ref_cntr_->inc();
+}
+
+DeviceBuffer::DeviceBuffer(DeviceBuffer &&other) {
+  copyBuffer(other);
+  other.zeroFields();
+}
+
+DeviceBuffer &DeviceBuffer::operator=(DeviceBuffer &other) {
+  ReleaseBuffer();
+  copyBuffer(other);
+  ref_cntr_->inc();
+  return *this;
+}
+
+DeviceBuffer &DeviceBuffer::operator=(DeviceBuffer &&other) {
+  ReleaseBuffer();
+  copyBuffer(other);
+  other.zeroFields();
+  return *this;
+}
+
+bool DeviceBuffer::unique() const { return ref_cntr_->val() == 0; }
+
+DeviceBuffer::operator bool() const { return buffer_id_ == 0; }
+
+void DeviceBuffer::copyBuffer(const DeviceBuffer &other) {
+  buffer_id_ = other.buffer_id_;
+  offset_ = other.offset_;
+  ref_cntr_ = other.ref_cntr_;
+  deallocator_ = other.deallocator_;
+}
+
+void DeviceBuffer::zeroFields() {
+  buffer_id_ = 0;
+  offset_ = 0;
+  ref_cntr_ = nullptr;
+  deallocator_ = nullptr;
+}
+
+bool DeviceBuffer::ReleaseBuffer() {
+  if (ref_cntr_) {
+    ref_cntr_->dec();
+    if (ref_cntr_->val() == 0) {
+      delete ref_cntr_;
+      ref_cntr_ = nullptr;
+
+      auto res = (*deallocator_)(buffer_id_);
+      deallocator_ = nullptr;
+      return (res == etrtSuccess);
+    }
+  }
+  return true;
+}
+
+bool operator<(const et_runtime::DeviceBuffer &a,
+               const et_runtime::DeviceBuffer &b) {
+  return a.id() < b.id();
+}
+
+bool operator==(const et_runtime::DeviceBuffer &a,
+                const et_runtime::DeviceBuffer &b) {
+  return a.id() == b.id();
+}
+
+} // namespace et_runtime
