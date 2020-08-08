e0bdbf2eebf6a1a83a5af5d66aeba42f36da0708
Shankar Jayaratnam <shankar.jayaratnam@esperantotech.com>
2020-08-07 22:42:35 -0700

    [device-minion-runtime] SW-3876 Multi Phase Minion Boot Phasing

---
+++ device-minion-runtime/src/shared/include/minion_cfg.h
@@ -0,0 +1,16 @@
+
+/*-------------------------------------------------------------------------
+* Copyright (C) 2020, Esperanto Technologies Inc.
+* The copyright to the computer program(s) herein is the
+* property of Esperanto Technologies, Inc. All Rights Reserved.
+* The program(s) may be used and/or copied only with
+* the written permission of Esperanto Technologies and
+* in accordance with the terms and conditions stipulated in the
+* agreement/contract under which the program(s) have been supplied.
+*-------------------------------------------------------------------------
+*/
+
+// Configure Minion :
+//    Program PLL to mode - all Minion programmed to same mode
+//    Enable Minion Cores - Pull Minion Cores out of Reset
+int64_t configure_compute_minion(uint64_t shire_mask, uint64_t pll_mode);
