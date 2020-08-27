1a6c91962155cdb636873bc2f1cc769ac2b76605
Sergi Granell <sergi.granell@esperantotech.com>
2020-08-27 16:32:39 +0200

    [sw-sysemu] [SW-3611] SysEMU: Secure Mailbox support

--- sw-sysemu/devices/plic.h
+++ sw-sysemu/devices/plic.h
@@ -320,6 +320,38 @@ struct PU_PLIC : public PLIC<Base, N, 41, 12>
     }
 };
 
+template <unsigned long long Base, size_t N>
+struct SP_PLIC : public PLIC<Base, N, 148, 2>
+{
+    static void Target_SP_Supervisor_external_interrupt(bool raise) {
+        (void) raise;
+#ifdef SYS_EMU
+        if (raise)
+            sys_emu::raise_external_interrupt(IO_SHIRE_ID);
+        else
+            sys_emu::clear_external_interrupt(IO_SHIRE_ID);
+#endif
+    }
+
+    static void Target_SP_Machine_external_interrupt(bool raise) {
+        (void) raise;
+#ifdef SYS_EMU
+        if (raise)
+            sys_emu::raise_external_supervisor_interrupt(IO_SHIRE_ID);
+        else
+            sys_emu::clear_external_supervisor_interrupt(IO_SHIRE_ID);
+#endif
+    }
+
+    const std::vector<PLIC_Interrupt_Target> &get_target_list() const {
+        static const std::vector<PLIC_Interrupt_Target> targets = {
+            {0, 0, Target_SP_Machine_external_interrupt},
+            {1, 1, Target_SP_Supervisor_external_interrupt},
+        };
+        return targets;
+    }
+};
+
 } // namespace bemu
 
 #endif // BEMU_PLIC_H
