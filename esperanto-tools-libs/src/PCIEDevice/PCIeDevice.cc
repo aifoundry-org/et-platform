a538fceb3ac0458c1bbb3253146cb87a1b7a7c55
Javier Vera <javier.vera@esperantotech.com>
2020-09-01 06:27:42 -0700

    [esperanto-tools-libs] Done SW-3879. Made also a refactor and cleaned up some code. Removed redundant classes which bring no value

--- esperanto-tools-libs/src/PCIEDevice/PCIeDevice.cc
+++ esperanto-tools-libs/src/PCIEDevice/PCIeDevice.cc
@@ -8,48 +8,190 @@
 // agreement/contract under which the program(s) have been supplied.
 //------------------------------------------------------------------------------
 
-#include "PCIeDevice.h"
+#include "PCIeDevice.h" 
 
 #include "Core/DeviceFwTypes.h"
 #include "esperanto/runtime/Core/CommandLineOptions.h"
 #include "esperanto/runtime/Support/Logging.h"
 
 #include <absl/strings/str_format.h>
-#include <experimental/filesystem>
+#include <asm-generic/errno.h>
+#include <chrono>
+#include <cstdint>
 #include <iostream>
 #include <regex>
-#include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <et_ioctl.h>
+#include <thread>
+#include <experimental/filesystem>
+#include "Tracing/Tracing.h"
+#include "esperanto/runtime/Support/TimeHelpers.h"
 
-namespace fs = std::experimental::filesystem;
+namespace fs = std::experimental::filesystem; 
 
 namespace et_runtime {
 namespace device {
+namespace {
+
+  //FIX magic numbers
+  TimeDuration kPollingInterval = std::chrono::milliseconds(5);
+  auto kWaitTime = std::chrono::seconds(5 * 60);
+
+  auto constexpr getDramSize() {
+    return HOST_MANAGED_DRAM_END - HOST_MANAGED_DRAM_START;
+  }
+
+//helper to make sure a trace is recorded when creating the object and when exiting the basic block
+  struct ScopedTrace {
+    ScopedTrace(std::function<void()> start, std::function<void()> end)
+      : end_(move(end)) {
+        start();
+    }
+    ~ScopedTrace() {
+      end_();
+    }
+    std::function<void()> end_;
+  };
+
+  struct IoctlResult {
+    int rc_;
+    operator bool() const {return rc_ >= 0;}
+  };
+
+
+  template<typename ...Types>
+  IoctlResult wrap_ioctl(int fd, unsigned long int request, Timepoint retryEnd, Types... args) {
+    auto res = ::ioctl(fd, request, args...);
+    if (res < 0) {
+      auto error = errno;
+      RTERROR << "Failed to execute IOCTL: " << std::strerror(error) << "\n";
+      //check if ioctl failed due to timeout, check the retryEnd time limit and retry after a while
+      if (errno == ETIMEDOUT && Clock::now() < retryEnd) { 
+        RTERROR << "IOCTL not ready in time, retrying after " << std::chrono::duration_cast<std::chrono::milliseconds>(kPollingInterval).count() << "ms \n";
+        std::this_thread::sleep_for(kPollingInterval);
+        return wrap_ioctl(fd, request, retryEnd, args...);
+      }
+    }
+    return {res};
+  }
+
+  void enableMMIO(int fd) {
+    if (!wrap_ioctl(fd, ETSOC1_IOCTL_SET_BULK_CFG, Clock::now(), static_cast<uint32_t>(BULK_CFG_MMIO))) {
+      RTERROR << "Failed to enable MMIO\n";
+      std::terminate();
+    }
+    TRACE_PCIeDevice_enable_mmio();
+  }
+  void enableDMA(int fd) {
+    if (!wrap_ioctl(fd, ETSOC1_IOCTL_SET_BULK_CFG, Clock::now(), static_cast<uint32_t>(BULK_CFG_DMA))) {
+      RTERROR << "Failed to enable DMA\n";
+      std::terminate();
+    }
+    TRACE_PCIeDevice_enable_dma();
+  }
+}
 
 PCIeDevice::PCIeDevice(int index)
-    : DeviceTarget(index),                     //
-      index_(index),                           //
-      prefix_(absl::StrFormat("et%d", index)), //
-      bulk_("/dev/" + prefix_ + "_ops"),       //
-      mm_("/dev/" + prefix_ + "_ops")          //
-{}
+  : DeviceTarget(index)
+  , path_(absl::StrFormat("/dev/et%d_ops", index)) {
+    fd_ = open(path_.c_str(), O_RDWR);
+    if (fd_ < 0) {
+      RTERROR << "Error opening driver\n";
+      //#TODO deal with errors in the same way across all the code
+      std::terminate();
+    }
+    auto res = wrap_ioctl(fd_, ETSOC1_IOCTL_GET_MBOX_MAX_MSG, Clock::now(), &mboxMaxMsgSize_);
+    if (!res) {
+      RTERROR << "Failed to get maximum mailbox message size \n";
+      std::terminate();
+    }
+    RTDEBUG << "Maximum mbox message size: " << mboxMaxMsgSize_ << "\n";
+}                     
+
+bool PCIeDevice::read(uintptr_t addr, void *data, ssize_t size) {
+  RTINFO << "Dev: " << path_ << "PCIE Read: 0x" << std::hex << addr << " size "
+          << std::dec << size << "\n";
+    
+    off_t target_offset = static_cast<off_t>(addr);
+    auto offset = lseek(fd_, target_offset, SEEK_SET);
+    auto err = errno;
+    if (offset != target_offset) {
+      RTERROR << "Failed to seek to device offset " << std::hex << addr;
+      RTERROR << " Error: " << std::strerror(err) << "\n";
+      return false;
+    }
+
+  auto rc = ::read(fd_, data, size);
+  err = errno;
+  if (rc != size) {
+    RTERROR << "Failed to read full data, wrote: " << rc;
+    RTERROR << " Error: " << std::strerror(err) << "\n";
+    return false;
+  }
+  return true;
+}
+
+bool PCIeDevice::write(uintptr_t addr, const void* data, ssize_t size) {
+  RTINFO << "Dev: " << path_ << "PCIE Write: 0x" << std::hex << addr << " size "
+         << std::dec << size << "\n";
+  off_t target_offset = static_cast<off_t>(addr);
+  auto offset = lseek(fd_, target_offset, SEEK_SET);
+  auto err = errno;
+  if (offset != target_offset) {
+    RTERROR << "Failed to seek to device offset " << std::hex << addr;
+    RTERROR << " Error: " << std::strerror(err) << "\n";
+    return false;
+  }
+
+  auto rc = ::write(fd_, data, size);
+  err = errno;
+  if (rc != size) {
+    RTERROR << "Failed to write full data, wrote: " << rc;
+    RTERROR << " Error: " << std::strerror(err) << "\n";
+    return false;
+  }
+  return true;
+
+}
+
+void PCIeDevice::resetMbox(TimeDuration wait_time) {
+  auto valid = wrap_ioctl(fd_, ETSOC1_IOCTL_RESET_MBOX, Clock::now() + wait_time);
+  if (!valid) {
+    RTERROR << "Failed to g \n";
+    std::terminate();
+  }
+  TRACE_PCIeDevice_mailbox_reset();
+}
+
+bool PCIeDevice::readyMbox(TimeDuration wait_time) {
+  uint64_t ready = 0;
+  auto res = wrap_ioctl(fd_, ETSOC1_IOCTL_GET_MBOX_READY, Clock::now() + wait_time, &ready);
+
+  TRACE_PCIeDevice_mailbox_ready(static_cast<bool>(ready));
+  if (!res) {
+    if (errno == ETIMEDOUT) {
+      RTERROR << "Mailbox not ready in time \n";
+    } else {
+      RTERROR << "Failed to get the status of the mailbox. Errno: " << strerror(errno) << std::endl;
+      std::terminate();
+    }
+  }
+  return static_cast<bool>(ready);
+}
 
 bool PCIeDevice::init() {
   // FIXME current we perform no initialization action that will not apply in
   // the future.
   RTINFO << "PCIeDevice: Initialization \n";
-  auto res = mm_.reset();
-  assert(res);
+  resetMbox(kWaitTime);
   RTINFO << "PCIEDevice: Reset MM mailbox \n";
-  // Wait for the device to be ready
-  // FIXME "random" wait time and polling interval
-  auto wait_time = std::chrono::seconds(5 * 60);
-  bool mb_ready = mm_.ready(wait_time);
+  // Wait for the device to be ready  
+  auto mb_ready = readyMbox(kWaitTime);
   RTINFO << "PCIEDevice: MM mailbox ready " << mb_ready << "\n";
-  if (!mb_ready) {
-    return false;
-  }
-  device_alive_ = true;
-  return true;
+  device_alive_ = mb_ready;
+  return device_alive_;
 }
 
 bool PCIeDevice::deinit() { return true; }
@@ -80,29 +222,65 @@ bool PCIeDevice::registerDeviceEventCallback() {
 }
 
 bool PCIeDevice::readDevMemMMIO(uintptr_t dev_addr, size_t size, void *buf) {
-  return bulk_.read_mmio(dev_addr, buf, size);
+  enableMMIO(fd_);
+  TRACE_PCIeDevice_mmio_read_start(dev_addr, (uint64_t)buf, size);
+  auto res = read(dev_addr, buf, size);
+  TRACE_PCIeDevice_mmio_read_end();
+  return res;
 }
 
-bool PCIeDevice::writeDevMemMMIO(uintptr_t dev_addr, size_t size,
-                                 const void *buf) {
-  return bulk_.write_mmio(dev_addr, buf, size);
+bool PCIeDevice::writeDevMemMMIO(uintptr_t dev_addr, size_t size, const void *buf) {
+  enableMMIO(fd_);
+  TRACE_PCIeDevice_mmio_write_start(dev_addr, (uint64_t)buf, size);
+  auto res = write(dev_addr, buf, size);
+  TRACE_PCIeDevice_mmio_write_end();
+  return res;
 }
 
 bool PCIeDevice::readDevMemDMA(uintptr_t dev_addr, size_t size, void *buf) {
-  return bulk_.read_dma(dev_addr, buf, size);
+  enableDMA(fd_);
+  TRACE_PCIeDevice_dma_read_start(dev_addr, (uint64_t)buf, size);
+  auto res = read(dev_addr, buf, size);
+  TRACE_PCIeDevice_dma_read_end();
+  return res;
+  
 }
 
 bool PCIeDevice::writeDevMemDMA(uintptr_t dev_addr, size_t size,
                                 const void *buf) {
-  return bulk_.write_dma(dev_addr, buf, size);
+  enableDMA(fd_);
+  TRACE_PCIeDevice_dma_write_start(dev_addr, (uint64_t)buf, size);
+  auto res = write(dev_addr, buf, size);
+  TRACE_PCIeDevice_dma_write_end();
+  return res;
 }
 
 bool PCIeDevice::mb_write(const void *data, ssize_t size) {
-  return mm_.write(data, size);
+  TRACE_PCIeDevice_mailbox_write_start((uint64_t)data, size);
+  auto res = wrap_ioctl(fd_, ETSOC1_IOCTL_PUSH_MBOX(size), Clock::now(), data);
+  TRACE_PCIeDevice_mailbox_write_end();
+  return res.rc_ == size;
 }
 
 ssize_t PCIeDevice::mb_read(void *data, ssize_t size, TimeDuration wait_time) {
-  return mm_.read(data, size, wait_time);
+  int dataRead = 0;
+  auto trace = ScopedTrace(
+    [&](){TRACE_PCIeDevice_mailbox_read_start((uint64_t)data, size);},
+    [&](){TRACE_PCIeDevice_mailbox_read_end(dataRead);}
+  );
+  auto start = Clock::now();
+  auto end = start + wait_time;
+  while (auto res = wrap_ioctl(fd_, ETSOC1_IOCTL_POP_MBOX(size), Clock::now(), data)) {
+    if (res.rc_ > 0) {
+      dataRead = res.rc_;
+      break;
+    }
+    std::this_thread::sleep_for(kPollingInterval);
+    if (end < Clock::now()) {
+      break;
+    }
+  }
+  return dataRead;
 }
 
 bool PCIeDevice::launch() {
@@ -120,11 +298,13 @@ bool PCIeDevice::shutdown() {
   return true;
 }
 
-uintptr_t PCIeDevice::dramBaseAddr() const { return bulk_.baseAddr(); }
+//FIXME: Ioannis had a comment meaning this should be fixed; I guess we should get the 
+// baseAddr from the device with an ioctl
+uintptr_t PCIeDevice::dramBaseAddr() const { return HOST_MANAGED_DRAM_START; }
 
-uintptr_t PCIeDevice::dramSize() const { return bulk_.size(); }
+uintptr_t PCIeDevice::dramSize() const { return getDramSize(); }
 
-ssize_t PCIeDevice::mboxMsgMaxSize() const { return mm_.mboxMaxMsgSize(); }
+ssize_t PCIeDevice::mboxMsgMaxSize() const { return mboxMaxMsgSize_; }
 
 std::vector<DeviceInformation> PCIeDevice::enumerateDevices() {
   std::vector<DeviceInformation> infos;
