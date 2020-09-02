044bd70e960f8f82de6da9e1430cf8b3e806ded8
Mahad Saleem <Mahad_Saleem@mentor.com>
2020-09-02 06:08:29 +0500

    [test-compute-kernels] [SW-3993]: Fixing TRACE_kernel_launch event logging in trace.c kernel.

--- test-compute-kernels/trace/trace.c
+++ test-compute-kernels/trace/trace.c
@@ -1,16 +1,13 @@
-#include "kernel_params.h"
 #include "device-mrt-trace.h"
 
 #include <stdint.h>
 #include <stddef.h>
 
 // Use all trace subsystem provided functions
-int64_t main(const kernel_params_t* const kernel_params_ptr)
+int64_t main(void)
 {
     // Call all trace logging functions with dummy values
-    TRACE_kernel_launch(LOG_LEVELS_ERROR, kernel_params_ptr->tensor_a, kernel_params_ptr->tensor_b,
-                        kernel_params_ptr->tensor_c, kernel_params_ptr->tensor_d, kernel_params_ptr->tensor_e,
-                        kernel_params_ptr->tensor_f, kernel_params_ptr->tensor_g, kernel_params_ptr->tensor_h, kernel_params_ptr->kernel_id);
+    TRACE_kernel_launch(LOG_LEVELS_ERROR, 0, 1, 2, 3, 4, 5, 6, 7, 1);
     TRACE_string(LOG_LEVELS_WARNING, "Trace message from kernel");
     TRACE_perfctr(LOG_LEVELS_INFO, 1, 2);
     TRACE_uber_kernel_marker(LOG_LEVELS_DEBUG, 1);
