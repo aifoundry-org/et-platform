4586f0bb0ea6996ec0f16ce4bc95145a910fa3cf
Javier Vera <javier.vera@esperantotech.com>
2020-11-10 00:20:05 -0800

    [test-compute-kernels] Jvera/fix new kernels

--- test-compute-kernels/add_vector/add_vector.c
+++ test-compute-kernels/add_vector/add_vector.c
@@ -1,14 +1,19 @@
+#include "kernel_params.h"
 #include "shire.h"
-struct MyVectors {
+
+inline int min(int a, int b) { return a < b ? a : b; }
+
+typedef struct {
   int* a;
   int* b;
   int* result;
   int numElements;
-};
-inline int min(int a, int b) { return a < b ? a : b; }
+} MyVectors;
 
 // use only one shire to exec this simple test
-int main(struct MyVectors* vectors) {
+int main(const kernel_params_t* const kernel_params_ptr) {
+  // the real params are inside tensor_a until we change the ABI
+  MyVectors* vectors = (MyVectors*)kernel_params_ptr->tensor_a;
   int tid = (int)get_thread_id();
   if (tid == 0) return 0;
   int mid = (int)get_minion_id();
@@ -16,6 +21,7 @@ int main(struct MyVectors* vectors) {
   int elemsPerWorker = (vectors->numElements + numWorkers - 1) / numWorkers;
   int init = elemsPerWorker * mid;
   int end = min(elemsPerWorker * (mid + 1), vectors->numElements);
+
   for (int i = init; i < end; ++i) {
     vectors->result[i] = vectors->a[i] + vectors->b[i];
   }
--- test-compute-kernels/memset/memset.c
+++ test-compute-kernels/memset/memset.c
@@ -1,20 +1,18 @@
+#include "kernel_params.h"
 #include "shire.h"
-struct Parameters {
+
+typedef struct {
   int* a;
   int value;
   int numElements;
-};
-inline int min(int a, int b) { return a < b ? a : b; }
-// use only one shire to exec this test
-int main(struct Parameters* params) {
-  int tid = (int)get_thread_id();
-  if (tid == 0) return 0;
-  int mid = (int)get_minion_id();
-  int numWorkers = SOC_MINIONS_PER_SHIRE;
-  int elemsPerWorker = (params->numElements + numWorkers - 1) / numWorkers;
-  int init = elemsPerWorker * mid;
-  int end = min(elemsPerWorker * (mid + 1), params->numElements);
-  for (int i = init; i < end; ++i) {
+  int numShires;
+} Parameters;
+
+int main(const kernel_params_t* const kernel_params_ptr) {
+  Parameters* params = (Parameters*)kernel_params_ptr->tensor_a;
+  int hart = (int)get_hart_id();
+  int numWorkers = params->numShires * SOC_MINIONS_PER_SHIRE * 2;
+  for (int i = hart; i < params->numElements; i += numWorkers) {
     params->a[i] = params->value;
   }
   return 0;
