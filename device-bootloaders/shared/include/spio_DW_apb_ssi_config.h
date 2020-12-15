0b64889a465d36f2dbfe4ec7aa7759944d3fe33f
Sergi Granell <sergi.granell@esperantotech.com>
2020-12-15 22:49:16 +0100

    [device-bootloaders] [SW-5561] Remove dependency from bootrom

---
+++ device-bootloaders/shared/include/spio_DW_apb_ssi_config.h
@@ -0,0 +1,23 @@
+/*-------------------------------------------------------------------------
+* Copyright (C) 2018,2020 Esperanto Technologies Inc.
+* The copyright to the computer program(s) herein is the
+* property of Esperanto Technologies, Inc. All Rights Reserved.
+* The program(s) may be used and/or copied only with
+* the written permission of Esperanto Technologies and
+* in accordance with the terms and conditions stipulated in the
+* agreement/contract under which the program(s) have been supplied.
+*-------------------------------------------------------------------------
+*/
+
+#ifndef __SPIO_DW_APB_SSI_CONFIG_H__
+#define __SPIO_DW_APB_SSI_CONFIG_H__
+
+#include <stdint.h>
+
+#define SPI_SSI_NUM_SLAVES 2
+#define SPI_TX_FIFO_MAX_DEPTH 128
+#define SPI_RX_FIFO_MAX_DEPTH 128
+#define TX_ABW 8
+
+#endif
+
