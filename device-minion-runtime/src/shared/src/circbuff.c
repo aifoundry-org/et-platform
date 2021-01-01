2b59bd58ce69322b895cf87e7bb35c0bf4a8e765
Mahad Saleem <mahad.saleem@esperantotech.com>
2021-01-01 09:29:33 -0800

    [device-minion-runtime] [SW-5725]: Adding support for performing cached, uncached and L2 cache memory reads/writes.

--- device-minion-runtime/src/shared/src/circbuff.c
+++ device-minion-runtime/src/shared/src/circbuff.c
@@ -17,9 +17,29 @@
         Circbuffer_Push
         Circbuffer_Pop
         Circbuffer_Peek
+        Circbuffer_Get_Used_Space
+        Circbuffer_Get_Avail_Space
 */
 /***********************************************************************/
 #include "circbuff.h"
+
+/*! \var void memory_read
+    \brief An array containing function pointers to ETSOC memory read functions.
+    \warning Not thread safe!
+*/
+static void (*memory_read[MEM_TYPES_COUNT]) 
+    (void *src_ptr, void *dest_ptr, uint32_t length) __attribute__((aligned(8))) = 
+    { ETSOC_Memory_Read_Local_Atomic, ETSOC_Memory_Read_Uncacheable, 
+    ETSOC_Memory_Read_Write_Cacheable };
+
+/*! \var void memory_write
+    \brief An array containing function pointers to ETSOC memory write functions.
+    \warning Not thread safe!
+*/
+static void (*memory_write[MEM_TYPES_COUNT]) 
+    (void *src_ptr, void *dest_ptr, uint32_t length) __attribute__((aligned(8))) = 
+    { ETSOC_Memory_Write_Local_Atomic, ETSOC_Memory_Write_Uncacheable, 
+    ETSOC_Memory_Read_Write_Cacheable };
 
 /************************************************************************
 *
@@ -43,20 +63,19 @@
 *
 ***********************************************************************/
 int8_t Circbuffer_Init(circ_buff_cb_t *circ_buff_cb_ptr, 
-        uint32_t buffer_length)
+            uint32_t buffer_length, uint32_t flags)
 {
+    circ_buff_cb_t circ_buff;
+
     /* Reset the head and tail offsets */
-    circ_buff_cb_ptr->head_offset = 0;
-    circ_buff_cb_ptr->tail_offset = 0;
+    circ_buff.head_offset = 0;
+    circ_buff.tail_offset = 0;
 
     /* Set the buffer length */
-    circ_buff_cb_ptr->length = buffer_length;
+    circ_buff.length = buffer_length;
 
-    /* Reset the circular buffer memory */
-    for (uint32_t i = 0; i < buffer_length; i++) 
-    {
-        circ_buff_cb_ptr->buffer_ptr[i] = 0;
-    }
+    /* Write the circular buffer CB to memory */
+    (*memory_write[flags]) (&circ_buff, circ_buff_cb_ptr, sizeof(circ_buff));
 
     return CIRCBUFF_OPERATION_SUCCESS;
 }
@@ -85,42 +104,56 @@ int8_t Circbuffer_Init(circ_buff_cb_t *circ_buff_cb_ptr,
 *       int8_t            Returns successful status or error code.
 *
 ***********************************************************************/
-int8_t Circbuffer_Push(volatile circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
-    const void *restrict const src_buffer, uint32_t src_length, uint32_t flags)
+int8_t Circbuffer_Push(circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
+            void *restrict const src_buffer, uint32_t src_length, uint32_t flags)
 {
-    uint32_t head_offset = circ_buff_cb_ptr->head_offset;
-    const uint32_t cbuffer_length = circ_buff_cb_ptr->length;
-    const uint8_t *const src_data_ptr = src_buffer;
+    int8_t status = CIRCBUFF_OPERATION_SUCCESS;
+    circ_buff_cb_t circ_buff;
 
-    (void)flags;
+    /* Read the circular buffer CB from memory */
+    (*memory_read[flags]) (circ_buff_cb_ptr, &circ_buff, sizeof(circ_buff));
 
-    if (src_length > cbuffer_length) 
+    /* Verify the available space in circular buffer */
+    if (Circbuffer_Get_Avail_Space(&circ_buff, CIRCBUFF_FLAG_NO_READ) >= src_length) 
     {
-        return CIRCBUFF_ERROR_BAD_LENGTH;
-    }
-
-    if (Circbuffer_Get_Avail_Space(circ_buff_cb_ptr, flags) == 0) 
-    {
-        return CIRCBUFF_ERROR_FULL;
-    }
-
-    if (head_offset >= cbuffer_length) 
-    {
-        return CIRCBUFF_ERROR_BAD_HEAD_INDEX;
-    } 
-    else 
-    {
-        for (uint32_t i = 0; i < src_length; i++) 
+        /* Verify the head offset */
+        if (circ_buff.head_offset >= circ_buff.length) 
         {
-            /* TODO: Use flags filed specified memory type to access circ buff */
-            circ_buff_cb_ptr->buffer_ptr[head_offset] = src_data_ptr[i];
-            head_offset = (uint32_t)((head_offset + 1) % cbuffer_length);
+            status = CIRCBUFF_ERROR_BAD_HEAD_INDEX;
         }
-
-        circ_buff_cb_ptr->head_offset = head_offset;
-
-        return CIRCBUFF_OPERATION_SUCCESS;
     }
+    else
+    {
+        status = CIRCBUFF_ERROR_FULL;
+    }
+
+    /* If previous operations are successful */
+    if (status == CIRCBUFF_OPERATION_SUCCESS)
+    {
+        /* Calculate the number of bytes till buffer wrap */
+        uint32_t bytes_till_wrap = circ_buff.length - circ_buff.head_offset;
+
+        if (bytes_till_wrap >= src_length) {
+            /* No buffer wrap needed */
+            (*memory_write[flags]) (src_buffer, 
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.head_offset], src_length);
+            circ_buff.head_offset += src_length;
+        }
+        else
+        {
+            /* Buffer wrap required */
+            (*memory_write[flags]) (src_buffer, 
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.head_offset], bytes_till_wrap);
+            (*memory_write[flags]) (src_buffer, 
+                (void*)&circ_buff_cb_ptr->buffer_ptr[0], (src_length - bytes_till_wrap));
+            circ_buff.head_offset = (src_length - bytes_till_wrap);
+        }
+        /* Update the head offset */
+        (*memory_write[flags]) ((void*)&(circ_buff.head_offset), 
+            (void*)&circ_buff_cb_ptr->head_offset, sizeof(circ_buff.head_offset));
+    }
+
+    return status;
 }
 
 /************************************************************************
@@ -147,43 +180,71 @@ int8_t Circbuffer_Push(volatile circ_buff_cb_t *restrict const circ_buff_cb_ptr,
 *       int8_t            Returns successful status or error code.
 *
 ***********************************************************************/
-int8_t Circbuffer_Pop(volatile circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
-        void *restrict const dest_buffer, uint32_t dest_length, uint32_t flags)
+int8_t Circbuffer_Pop(circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
+            void *restrict const dest_buffer, uint32_t dest_length, uint32_t flags)
 {
-    uint32_t tail_offset = circ_buff_cb_ptr->tail_offset;
-    const uint32_t cbuffer_length = circ_buff_cb_ptr->length;
-    uint8_t *const dest_data_ptr = dest_buffer;
-    const uint32_t used_space = Circbuffer_Get_Used_Space(circ_buff_cb_ptr, flags);
+    int8_t status = CIRCBUFF_OPERATION_SUCCESS;
+    circ_buff_cb_t circ_buff;
+    uint32_t used_space;
 
-    (void)flags;
+    /* Read the circular buffer CB from memory */
+    (*memory_read[flags]) (circ_buff_cb_ptr, &circ_buff, sizeof(circ_buff));
 
-    if (used_space == 0) 
-    {
-        return CIRCBUFF_ERROR_EMPTY;
-    }
+    /* Get the used space in circular buffer */
+    used_space = Circbuffer_Get_Used_Space(&circ_buff, CIRCBUFF_FLAG_NO_READ);
 
-    if (dest_length > used_space) 
+    /* Verify if circular buffer has some data */
+    if (used_space > 0) 
     {
-        return CIRCBUFF_ERROR_BAD_LENGTH;
-    }
-
-    if (tail_offset >= cbuffer_length) 
-    {
-        return CIRCBUFF_ERROR_BAD_TAIL_INDEX;
-    } 
-    else 
-    {
-        for (uint32_t i = 0; i < dest_length; i++) 
+        /* Verify the length of data requested to read */
+        if (dest_length > used_space)
         {
-            /* TODO: Use flags filed specified memory type to access circ buff */
-            dest_data_ptr[i] = circ_buff_cb_ptr->buffer_ptr[tail_offset];
-            tail_offset = (uint32_t)((tail_offset + 1U) % cbuffer_length);
+            status = CIRCBUFF_ERROR_BAD_LENGTH;
         }
-
-        circ_buff_cb_ptr->tail_offset = tail_offset;
-
-        return CIRCBUFF_OPERATION_SUCCESS;
     }
+    else
+    {
+        status = CIRCBUFF_ERROR_EMPTY;
+    }
+
+    /* Verify the circular buffer tail offset */
+    if ((status == CIRCBUFF_OPERATION_SUCCESS) && 
+        (circ_buff.tail_offset >= circ_buff.length))
+    {
+        status = CIRCBUFF_ERROR_BAD_TAIL_INDEX;
+    }
+
+    /* If previous operations are successful */
+    if (status == CIRCBUFF_OPERATION_SUCCESS)
+    {
+        /* Calculate the number of bytes till buffer wrap */
+        uint32_t bytes_till_wrap = circ_buff.length - circ_buff.tail_offset;
+
+        if (bytes_till_wrap >= dest_length) 
+        {
+            /* No buffer wrap needed */
+            (*memory_read[flags]) (
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.tail_offset], 
+                dest_buffer, dest_length);
+            circ_buff.tail_offset += dest_length;
+        }
+        else
+        {
+            /* Buffer wrap required */
+            (*memory_read[flags]) (
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.tail_offset], 
+                dest_buffer, bytes_till_wrap);
+            (*memory_read[flags]) ((void*)&circ_buff_cb_ptr->buffer_ptr[0], 
+                (void*)((uint8_t*)dest_buffer + bytes_till_wrap), 
+                (dest_length - bytes_till_wrap));
+            circ_buff.tail_offset = (dest_length - bytes_till_wrap);
+        }
+        /* Update the tail offset */
+        (*memory_write[flags]) ((void*)&(circ_buff.tail_offset), 
+            (void*)&circ_buff_cb_ptr->tail_offset, sizeof(circ_buff.tail_offset));
+    }
+
+    return status;
 }
 
 /************************************************************************
@@ -211,40 +272,143 @@ int8_t Circbuffer_Pop(volatile circ_buff_cb_t *restrict const circ_buff_cb_ptr,
 *       int8_t            Returns successful status or error code.
 *
 ***********************************************************************/
-int8_t Circbuffer_Peek(volatile circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
-    void *restrict const dest_buffer, uint16_t peek_offset, uint16_t peek_length, 
-    uint32_t flags)
+int8_t Circbuffer_Peek(circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
+        void *restrict const dest_buffer, uint16_t peek_offset, 
+        uint16_t peek_length, uint32_t flags)
 {
-    uint32_t tail_offset = circ_buff_cb_ptr->tail_offset;
-    const uint32_t cbuffer_length = circ_buff_cb_ptr->length;
-    uint8_t *const dest_data_ptr = dest_buffer;
-    const uint32_t used_space = Circbuffer_Get_Used_Space(circ_buff_cb_ptr, flags);
+    int8_t status = CIRCBUFF_OPERATION_SUCCESS;
+    circ_buff_cb_t circ_buff;
+    uint32_t used_space;
 
-    (void)flags;
+    /* Read the circular buffer CB from memory */
+    (*memory_read[flags]) (circ_buff_cb_ptr, &circ_buff, sizeof(circ_buff));
 
-    if (used_space == 0) 
-    {
-        return CIRCBUFF_ERROR_EMPTY;
-    }
+    /* Get the used space in circular buffer */
+    used_space = Circbuffer_Get_Used_Space(&circ_buff, CIRCBUFF_FLAG_NO_READ);
 
-    if (peek_length > used_space) 
+    /* Verify if circular buffer has some data */
+    if (used_space > 0) 
     {
-        return CIRCBUFF_ERROR_BAD_LENGTH;
-    }
-
-    if (tail_offset >= cbuffer_length) 
-    {
-        return CIRCBUFF_ERROR_BAD_TAIL_INDEX;
-    } 
-    else 
-    {
-        for (uint16_t i = 0; i < peek_length; i++) 
+        /* Verify the length and offset of data requested to peek */
+        if ((peek_length + peek_offset) > used_space)
         {
-            /* TODO: Use flags filed specified memory type to access circ buff */
-            dest_data_ptr[i] = circ_buff_cb_ptr->buffer_ptr[tail_offset + peek_offset];
-            tail_offset = (uint32_t)((tail_offset + 1U) % cbuffer_length);
+            status = CIRCBUFF_ERROR_BAD_LENGTH;
         }
-
-        return CIRCBUFF_OPERATION_SUCCESS;
     }
+    else
+    {
+        status = CIRCBUFF_ERROR_EMPTY;
+    }
+
+    /* Verify the circular buffer tail offset */
+    if ((status == CIRCBUFF_OPERATION_SUCCESS) && 
+        (circ_buff.tail_offset >= circ_buff.length))
+    {
+        status = CIRCBUFF_ERROR_BAD_TAIL_INDEX;
+    }
+
+    /* If previous operations are successful */
+    if (status == CIRCBUFF_OPERATION_SUCCESS)
+    {
+        /* Calculate the number of bytes till buffer wrap */
+        uint32_t bytes_till_wrap = circ_buff.length - (circ_buff.tail_offset + peek_offset);
+
+        if (bytes_till_wrap >= peek_length) 
+        {
+            /* No buffer wrap needed */
+            (*memory_read[flags]) (
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.tail_offset + peek_offset], 
+                dest_buffer, peek_length);
+            circ_buff.tail_offset += peek_length;
+        }
+        else
+        {
+            /* Buffer wrap required */
+            (*memory_read[flags]) (
+                (void*)&circ_buff_cb_ptr->buffer_ptr[circ_buff.tail_offset + peek_offset], 
+                dest_buffer, bytes_till_wrap);
+            (*memory_read[flags]) ((void*)&circ_buff_cb_ptr->buffer_ptr[0], 
+                (void*)((uint8_t*)dest_buffer + bytes_till_wrap), 
+                (peek_length - bytes_till_wrap));
+            circ_buff.tail_offset = (peek_length - bytes_till_wrap);
+        }
+    }
+
+    return status;
+}
+
+/************************************************************************
+*
+*   FUNCTION
+*
+*       Circbuffer_Get_Used_Space
+*  
+*   DESCRIPTION
+*
+*       This function is used to get the number of used bytes in circular 
+*       buffer.
+*
+*   INPUTS
+*
+*       circ_buff_cb_ptr  Pointer to circular buffer control block.
+*       flags             Indicates memory access type.
+*
+*   OUTPUTS
+*
+*       uint32_t          Returns number of used bytes.
+*
+***********************************************************************/
+uint32_t Circbuffer_Get_Used_Space(circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
+            uint32_t flags)
+{
+    circ_buff_cb_t *circ_buff = circ_buff_cb_ptr;
+
+    /* Read from memory if no read flag is not set */
+    if (flags != CIRCBUFF_FLAG_NO_READ)
+    {
+        /* Read the circular buffer CB from memory */
+        (*memory_read[flags]) (circ_buff_cb_ptr, circ_buff, sizeof(*circ_buff));
+    }
+
+    return (uint32_t)((circ_buff->head_offset >= circ_buff->tail_offset) ? 
+                      circ_buff->head_offset - circ_buff->tail_offset :
+                      (circ_buff->length + circ_buff->head_offset - circ_buff->tail_offset));
+}
+
+/************************************************************************
+*
+*   FUNCTION
+*
+*       Circbuffer_Get_Avail_Space
+*  
+*   DESCRIPTION
+*
+*       This function is used to get the free space (in bytes) in a 
+*       circular buffer.
+*
+*   INPUTS
+*
+*       circ_buff_cb_ptr  Pointer to circular buffer control block.
+*       flags             Indicates memory access type.
+*
+*   OUTPUTS
+*
+*       uint32_t          Returns the free space.
+*
+***********************************************************************/
+uint32_t Circbuffer_Get_Avail_Space(circ_buff_cb_t *restrict const circ_buff_cb_ptr, 
+            uint32_t flags)
+{
+    circ_buff_cb_t *circ_buff = circ_buff_cb_ptr;
+
+    /* Read from memory if no read flag is not set */
+    if (flags != CIRCBUFF_FLAG_NO_READ)
+    {
+        /* Read the circular buffer CB from memory */
+        (*memory_read[flags]) (circ_buff_cb_ptr, circ_buff, sizeof(*circ_buff));
+    }
+
+    return (uint32_t)((circ_buff->head_offset >= circ_buff->tail_offset) ? 
+                      (circ_buff->length - 1) - (circ_buff->head_offset - circ_buff->tail_offset) :
+                       circ_buff->tail_offset - circ_buff->head_offset - 1);
 }
