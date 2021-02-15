c0615141cd5da57700f1e17e2db83ebe67cbe63b
Sergi Granell <sergi.granell@esperantotech.com>
2021-02-15 19:49:17 +0100

    [test-compute-kernels] Add exception kernel

---
+++ test-compute-kernels/exception/exception.c
@@ -0,0 +1,16 @@
+#include <stdint.h>
+#include "hart.h"
+
+// Exception and then hangs - useful for testing kernel exception handling of firmware
+int64_t main(void)
+{
+    if ((get_shire_id() == 0) && (get_minion_id() == 0) && (get_thread_id() == 0)) {
+        *(volatile uint64_t *)0 = 0xDEADBEEF;
+    }
+
+    while (1) {
+        // Do nothing
+    }
+
+    return 0;
+}
