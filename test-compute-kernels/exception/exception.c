8384d0f58c31f28a0a806d5ecfb924a02ed9477a
Sergi Granell <sergi.granell@esperantotech.com>
2021-02-16 08:32:41 +0100

    [test-compute-kernels] exception kernel: fix minion id mask

--- test-compute-kernels/exception/exception.c
+++ test-compute-kernels/exception/exception.c
@@ -4,7 +4,7 @@
 // Exception and then hangs - useful for testing kernel exception handling of firmware
 int64_t main(void)
 {
-    if ((get_shire_id() == 0) && (get_minion_id() == 0) && (get_thread_id() == 0)) {
+    if ((get_shire_id() == 0) && ((get_minion_id() & 0x1f) == 0) && (get_thread_id() == 0)) {
         *(volatile uint64_t *)0 = 0xDEADBEEF;
     }
 
