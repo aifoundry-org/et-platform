f471db0b062e707781b36d8a6446be7f70554e36
Sergi Granell <sergi.granell@esperantotech.com>
2021-03-03 17:56:26 +0100

    [device-api] Remove old device-api

--- device-api/cmake/GenerateApi.cmake
+++ device-api/cmake/GenerateApi.cmake
@@ -7,103 +7,6 @@
 # in accordance with the terms and conditions stipulated in the
 # agreement/contract under which the program(s) have been supplied.
 #------------------------------------------------------------------------------
-function(jinja_generate_api_cpp SRCS HDRS)
-    if(NOT ARGN)
-        message(SEND_ERROR "Error: jinja_generate_api_cpp() called without any jinja files")
-        return()
-    endif()
-
-    set(options PRIVILEGED NON_PRIVILEGED NO_POSTFIX)
-    set(oneValueArgs TARGET DEVICE_API_CODEGEN_DIR)
-    set(multiValueArgs JINJA_TEMPLATES)
-    cmake_parse_arguments(JINJA_GENERATE_API "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    if (NOT (JINJA_GENERATE_API_PRIVILEGED OR JINJA_GENERATE_API_NON_PRIVILEGED))
-        message(SEND_ERROR "Error: jinja_generate_api_cpp() called without argument: PRIVILEGED | NON_PRIVILEGED")
-        return()
-    endif()
-
-    if (NOT JINJA_GENERATE_API_TARGET)
-        message(SEND_ERROR "Error: jinja_generate_api_cpp() called without argument: TARGET")
-        return()
-    endif()
-
-    if (NOT JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR)
-        message(SEND_ERROR "Error: jinja_generate_api_cpp() called without argument: DEVICE_API_CODEGEN_DIR")
-        return()
-    endif()
-
-    if (NOT JINJA_GENERATE_API_JINJA_TEMPLATES)
-        message(SEND_ERROR "Error: jinja_generate_api_cpp() called without argument: JINJA_TEMPLATES")
-        return()
-    endif()
-
-    if (JINJA_GENERATE_API_NO_POSTFIX)
-        set(GENERATE_API_TYPE_POSTFIX "")
-    elseif (JINJA_GENERATE_API_PRIVILEGED)
-        set(GENERATE_API_TYPE_POSTFIX "_privileged")
-    elseif(JINJA_GENERATE_API_NON_PRIVILEGED)
-        set(GENERATE_API_TYPE_POSTFIX "_non_privileged")
-    endif()
-
-    file(GLOB_RECURSE DEVICE_API_SCHEMA_FILES RELATIVE ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR} *.yaml)
-
-    if (JINJA_GENERATE_API_PRIVILEGED)
-        set(DEVICE_API_SPEC_FILE ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/privileged-api/device-api-privileged.yaml)
-    elseif (JINJA_GENERATE_API_NON_PRIVILEGED)
-        set(DEVICE_API_SPEC_FILE ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/non-privileged-api/device-api-non-privileged.yaml)
-    endif()
-
-    set(${SRCS})
-    set(${HDRS})
-    foreach(FIL ${JINJA_GENERATE_API_JINJA_TEMPLATES})
-        get_filename_component(FIL_ABS  ${FIL} ABSOLUTE)  # file name absolute path
-        get_filename_component(FIL_EXT  ${FIL} LAST_EXT)  # file name last extension (.c from d/a.b.c)
-        get_filename_component(FIL_DIR  ${FIL} DIRECTORY) # Directory without file name
-        get_filename_component(FIL_WE   ${FIL} NAME_WE)   # file name without directory or longest extension (a from d/a.b.c)
-        get_filename_component(FIL_WLE  ${FIL} NAME_WLE)  # file name without directory or last extension (a.b from d/a.b.c)
-        get_filename_component(SRC_EXT  ${FIL_WLE} LAST_EXT) # extract '.h' or '.cc/.cpp' from name
-
-        if (NOT FIL_EXT STREQUAL ".jinja")
-            message(SEND_ERROR "Error: jinja_generate_api_cpp() called with file without jinja extension (${FIL_EXT}). Abort")
-            return()
-        endif()
-
-        # take a XXX.{h|cpp}.jinja file and generate a XXX_priviledged.{h|cpp} file
-        # take a XXX.{h|cpp}.jinja file and generate a YYY/X_type_XX.{h|cpp} file
-        set(JINJA_TEMPLATE      ${FIL_ABS})
-        set(GENERATED_FILE_NAME ${FIL_WE}${GENERATE_API_TYPE_POSTFIX}${SRC_EXT})
-        set(GENERATED_DIR       "${CMAKE_CURRENT_BINARY_DIR}/esperanto/device-api")
-        set(GENERATED_FILE      "${GENERATED_DIR}/${FIL_DIR}${GENERATED_FILE_NAME}")
-
-        if (SRC_EXT MATCHES ".h")
-            list(APPEND ${HDRS} ${GENERATED_FILE})
-        else()
-            list(APPEND ${SRCS} ${GENERATED_FILE})
-        endif()        
-
-        add_custom_command(
-            OUTPUT  ${GENERATED_FILE}
-            COMMAND ${CMAKE_COMMAND} -E make_directory ${GENERATED_DIR}
-            COMMAND ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/device_api_codegen.py
-            ARGS  --spec     ${DEVICE_API_SPEC_FILE}
-                  --schema   ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/schema/device-api.schema.json
-                  --template ${JINJA_TEMPLATE}
-                  --output   ${GENERATED_FILE}
-            DEPENDS ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/device_api_codegen.py
-                    ${JINJA_GENERATE_API_DEVICE_API_CODEGEN_DIR}/schema/device-api.schema.json
-                    ${DEVICE_API_SCHEMA_FILES}
-                    ${JINJA_TEMPLATE}
-        )
-        list(APPEND GENERATED_FILES ${GENERATED_FILE})
-    endforeach()
-    add_custom_target(${JINJA_GENERATE_API_TARGET} ALL DEPENDS ${GENERATED_FILES} COMMENT "Generated files")
-
-    set_source_files_properties(${${SRCS}} ${${HDRS}} PROPERTIES GENERATED TRUE)
-    set(${SRCS} ${${SRCS}} PARENT_SCOPE)
-    set(${HDRS} ${${HDRS}} PARENT_SCOPE)
-endfunction()
-
 
 function(jinja_generate_apis_cpp SRCS HDRS)
     if(NOT ARGN)
@@ -186,7 +89,7 @@ function(jinja_generate_apis_cpp SRCS HDRS)
             list(APPEND ${HDRS} ${GENERATED_FILE})
         else()
             list(APPEND ${SRCS} ${GENERATED_FILE})
-        endif()        
+        endif()
 
         add_custom_command(
             OUTPUT  ${GENERATED_FILE}
