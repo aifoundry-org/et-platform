5ef8804c23502ab674c59c1b412a62ad94c99516
dusicav <dusica.veselinovic@esperantotech.com>
2021-03-30 13:15:02 +0000

    [device-bootloaders] [SW-6751] Adding support for SPIO_WDT timer driver

--- device-bootloaders/ServiceProcessorBL2/driver/bl2_watchdog.c
+++ device-bootloaders/ServiceProcessorBL2/driver/bl2_watchdog.c
@@ -1,24 +1,99 @@
+/*-------------------------------------------------------------------------
+* Copyright (C) 2020, Esperanto Technologies Inc.
+* The copyright to the computer program(s) herein is the
+* property of Esperanto Technologies, Inc. All Rights Reserved.
+* The program(s) may be used and/or copied only with
+* the written permission of Esperanto Technologies and
+* in accordance with the terms and conditions stipulated in the
+* agreement/contract under which the program(s) have been supplied.
+*-------------------------------------------------------------------------
+
+*************************************************************************/
+/*! \file bl2_watchdog.c
+    \brief A C module that implements the WDT functions
+
+    Public interfaces:
+        watchdog_init
+        watchdog_error_init
+        watchdog_start
+        watchdog_stop
+        watchdog_kick
+        get_watchdog_timeout
+        get_watchdog_max_timeout
+*/
+/***********************************************************************/
+
+#include <stdio.h>
+
+#include "io.h"
+
 #include <bl2_watchdog.h>
+#include <wdt.h>
+#include <interrupt.h>
+#include <bl2_pmic_controller.h>
 
 /* The driver can populate this structure with the defaults that will be used during the init
  phase.*/
 
 static struct watchdog_control_block wdog_control_block __attribute__((section(".data")));
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_init
+*
+*   DESCRIPTION
+*
+*       This function initializes WDT
+*
+*   INPUTS
+*
+*       timeout_msec    Timeout in miliseconds
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t watchdog_init(uint32_t timeout_msec)
 {
-    //TODO : Init Watchdog counter with the given timeout */
-    //TODO: Enable interrupt handler for the SPIO_WDT_INTR*/
-    //TODO: Enable PMIC interface to handle second interrupt
-    //   and reset the system, spio_wdt_sys_rstn*.
-    //TODO: Starts the watch dog
-    //https://esperantotech.atlassian.net/browse/SW-6751
+    //Init Watchdog counter with the given timeout */
+    //Enable interrupt handler for the SPIO_WDT_INTR*/
+    //Enable PMIC interface to handle second interrupt
+    //and reset the system, spio_wdt_sys_rstn*.
+    //Start the watch dog
 
-    (void)timeout_msec;
+    wdog_control_block.timeout_msec = timeout_msec;
+
+    INT_enableInterrupt(SPIO_PLIC_WDT_INTR, 1, watchdog_isr);
+
+    watchdog_start();
+
+    printf("Started WATCHDOG...\n");
 
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_error_init
+*
+*   DESCRIPTION
+*
+*       This function initializes error callback
+*
+*   INPUTS
+*
+*       event_cb    Callback function
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t watchdog_error_init(dm_event_isr_callback event_cb)
 {
     /* Save the handle to the timeout callback */
@@ -27,38 +102,154 @@ int32_t watchdog_error_init(dm_event_isr_callback event_cb)
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_start
+*
+*   DESCRIPTION
+*
+*       This function start WDT
+*
+*   INPUTS
+*
+*       none    
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t watchdog_start(void)
 {
-    //TODO:https://esperantotech.atlassian.net/browse/SW-6751
+    iowrite32(WDT_WDT_CR_ADDRESS, WDT_WDT_CR_WDT_EN_WDT_EN_ENABLED);
+
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_stop
+*
+*   DESCRIPTION
+*
+*       This function stops WDT
+*
+*   INPUTS
+*
+*       none    
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t watchdog_stop(void)
 {
-    // TODO: https://esperantotech.atlassian.net/browse/SW-6751
+    iowrite32(WDT_WDT_CR_ADDRESS, WDT_WDT_CR_WDT_EN_WDT_EN_DISABLED);
+
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_kick
+*
+*   DESCRIPTION
+*
+*       This function restarts WDT
+*
+*   INPUTS
+*
+*       none    
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 void watchdog_kick(void)
 {
-    //TODO: Feed the wdog - https://esperantotech.atlassian.net/browse/SW-6751
+    //Feed the wdog
+    iowrite32(WDT_WDT_CRR_ADDRESS, WDT_WDT_CRR_WDT_CRR_WDT_CRR_RESTART);
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       get_watchdog_timeout
+*
+*   DESCRIPTION
+*
+*       This function reads WDT timeout
+*
+*   INPUTS
+*
+*       pointer to timeout variable to be populated    
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t get_watchdog_timeout(uint32_t *timeout_msec)
 {
     *timeout_msec = wdog_control_block.timeout_msec;
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       get_watchdog_max_timeout
+*
+*   DESCRIPTION
+*
+*       This function reads WDT max timeout
+*
+*   INPUTS
+*
+*       pointer to timeout variable to be populated    
+*
+*   OUTPUTS
+*
+*       error status
+*
+***********************************************************************/
 int32_t get_watchdog_max_timeout(uint32_t *timeout_msec)
 {
     *timeout_msec = wdog_control_block.max_timeout_msec;
     return 0;
 }
 
+/************************************************************************
+*
+*   FUNCTION
+*
+*       watchdog_isr
+*
+*   DESCRIPTION
+*
+*       WDT ISR
+*
+*   INPUTS
+*
+*       none   
+*
+*   OUTPUTS
+*
+*       none
+*
+***********************************************************************/
 void watchdog_isr(void)
 {
-    //TODO : https://esperantotech.atlassian.net/browse/SW-6751
     /* Take any corrective action required */
 
     /* Invoke the event handler callback */
