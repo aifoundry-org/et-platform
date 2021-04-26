4e29d89ecd4466c53491b6279453216eb8d96241
Haseeb Ashraf <Haseeb_Ashraf@mentor.com>
2021-04-26 12:11:55 +0500

    [devicelayer] fix race condition in quick response reception
    executor thread was previously submitting the command and on success
    adding it's entry to the list of commands sent under current execution
    scope. But there was a race condition where response was getting
    received before the command entry is saved. So making sure that
    if command is sent then it's entry must exist there already.
    
    Signed-off-by: Haseeb Ashraf <Haseeb_Ashraf@mentor.com>

--- devicelayer/tests/device-minion-runtime/testHelper/TestDevOpsApi.cpp
+++ devicelayer/tests/device-minion-runtime/testHelper/TestDevOpsApi.cpp
@@ -265,12 +265,14 @@ void TestDevOpsApi::initTagId(device_ops_api::tag_id_t value) {
 }
 
 bool TestDevOpsApi::pushCmd(uint16_t queueId, std::unique_ptr<IDevOpsApiCmd>& devOpsApiCmd) {
+  addCmdResultEntry(devOpsApiCmd->getCmdTagId(), CmdStatus::CMD_RSP_NOT_RECEIVED);
   auto res =
     devLayer_->sendCommandMasterMinion(kIDevice, queueId, devOpsApiCmd->getCmdPtr(), devOpsApiCmd->getCmdSize());
   if (res) {
     TEST_VLOG(1) << "=====> Command Sent (tag_id: " << std::hex << devOpsApiCmd->getCmdTagId()
                  << ") <====" << std::endl;
-    addCmdResultEntry(devOpsApiCmd->getCmdTagId(), CmdStatus::CMD_RSP_NOT_RECEIVED);
+  } else {
+    deleteCmdResultEntry(devOpsApiCmd->getCmdTagId());
   }
 
   return res;
