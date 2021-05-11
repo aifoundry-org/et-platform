a30df4f3e63e56109ea37b34b71fb37c017f44c8
Calvin Bulla <calvin.bulla@esperantotech.com>
2021-05-11 19:20:10 +0200

    [device-bootloaders] Add asset_config_header_t

--- device-bootloaders/shared/include/asset_config_info.h
+++ device-bootloaders/shared/include/asset_config_info.h
@@ -7,12 +7,15 @@
 #define ASSET_CONFIG_MODULE_REV_SIZE  sizeof(uint32_t)
 #define ASSET_CONFIG_FORM_FACTOR_SIZE sizeof(uint8_t)
 
+#define ASSET_CONFIG_HEADER_SIZE (sizeof(uint32_t) + sizeof(uint32_t) + sizeof(uint64_t))
+
+typedef struct asset_config_header_s {
+    uint32_t tag;
+    uint32_t version;
+    uint64_t hash;
+} asset_config_header_t;
+
 typedef struct asset_config_info_s {
-    struct {
-        uint32_t tag;
-        uint32_t version;
-        uint64_t hash;
-    } header;
     uint32_t part_num;
     uint64_t serial_num;
     uint8_t mem_size;
