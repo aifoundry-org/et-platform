e06722e559177bf86528b8841e9e0ef443a6cf24
Pau Farre <pau.farre@esperantotech.com>
2021-05-23 18:01:25 +0000

    [common-sw] [SW-7727] Refactor logging cmakes for sw-sysemu POC
    - Refactor project to make logger just a component of 'hostUtils' project
    - Add conanfile.py and conan/test_package
    - Add CI to test that packaging works
    - Provide additional paths in CMAKE_PREFIX_PATH

---
+++ common-sw/conan/test_package/test_package.cpp
@@ -0,0 +1,10 @@
+#include "hostUtils/logging/DefaultSinks.h"
+#include "hostUtils/logging/Logger.h"
+#include "hostUtils/logging/Logging.h"
+#include "hostUtils/logging/Instance.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "SUCCESS" << std::endl;
+}
