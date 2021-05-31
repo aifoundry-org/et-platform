86bd2c4a1aae9362f1e82188427971a435ef222b
Javier Vera <javier.vera@esperantotech.com>
2021-05-31 13:18:20 +0200

    [common-sw] [SW-7994] changed default cout to cerr

--- common-sw/logging/include/logging/DefaultSinks.h
+++ common-sw/logging/include/logging/DefaultSinks.h
@@ -32,7 +32,7 @@ struct ColoredOutput {
   void ReceiveLogMessage(g3::LogMessageMover logEntry) {
      auto level = logEntry.get()._level;
      auto color = GetColor(level);
-     std::cout << "\033[" << color << "m" << logEntry.get().toString() << "\033[m" << std::flush;
+     std::cerr << "\033[" << color << "m" << logEntry.get().toString() << "\033[m" << std::flush;
   }
 };
 }
