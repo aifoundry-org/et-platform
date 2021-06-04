7afa95a25c677b71e99a296d0fa790b4c035f7ff
Javier Vera <javier.vera@esperantotech.com>
2021-06-04 10:18:46 +0200

    [common-sw] Default color changed to NONE in DefaultLogger

--- common-sw/logging/include/logging/DefaultSinks.h
+++ common-sw/logging/include/logging/DefaultSinks.h
@@ -19,20 +19,24 @@ struct ColoredOutput {
 
 // Linux xterm color
 // http://stackoverflow.com/questions/2616906/how-do-i-output-coloured-text-to-a-linux-terminal
-  enum FG_Color {YELLOW = 33, RED = 31, GREEN=32, WHITE = 97};
+  enum FG_Color {YELLOW = 33, RED = 31, GREEN=32, WHITE = 97, NOCOLOR};
 
   FG_Color GetColor(const LEVELS level) const {
      if (level.value == WARNING.value) { return YELLOW; }
      if (level.value <= DEBUG.value) { return GREEN; }
      if (g3::internal::wasFatal(level)) { return RED; }
 
-     return WHITE;
+     return NOCOLOR;
   }
 
   void ReceiveLogMessage(g3::LogMessageMover logEntry) {
      auto level = logEntry.get()._level;
      auto color = GetColor(level);
-     std::cerr << "\033[" << color << "m" << logEntry.get().toString() << "\033[m" << std::flush;
+     if (color == NOCOLOR) {
+      std::cerr << logEntry.get().toString() << std::flush;  
+     } else {
+      std::cerr << "\033[" << color << "m" << logEntry.get().toString() << "\033[m" << std::flush;
+     }
   }
 };
 }
