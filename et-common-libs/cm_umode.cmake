6157d074712151469444f4f4821accf911e17380
Arvind Raghuraman <arvind_raghuraman@mentor.com>
2021-06-10 01:01:27 -0700

    [et-common-libs] Checking in baseline for cm-umode lib infra, this is just a starting poit, there is a lot of clean up needed for components that need to go into cm-umode lib before it can be added

---
+++ et-common-libs/cm_umode.cmake
@@ -0,0 +1,85 @@
+########################
+#Compute Minion User Mode
+########################
+
+set(CM_UMODE_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/cm-umode)
+
+################################################
+# List the public interfaces and headers to be
+#exposed to Compute Minion User Mode here
+################################################
+
+#Listing of header only public interfaces
+set(CM_UMODE_HDRS
+    # ETSOC ISA headers
+    include/etsoc/isa/atomic.h
+    include/etsoc/isa/atomic-impl.h
+    include/etsoc/isa/cacheops.h
+    include/etsoc/isa/fcc.h
+    include/etsoc/isa/flb.h
+    include/etsoc/isa/hart.h
+    include/etsoc/isa/shire.h
+    include/etsoc/isa/tensor.h
+    include/etsoc/isa/tensors.h
+    include/etsoc/isa/utils.h
+    #Floating point operations
+    include/float/Float16.h
+    #TODO:others to come ..
+)
+
+#Listing of public headers that expose services provided by
+#the CM UMODE (Compute Minion User Mode) Library
+set(CM_UMODE_LIB_HDRS
+    include/etsoc/hal/pmu.h
+    #TODO:others to come ..
+)
+
+########################
+#Create cm-umode library
+########################
+#Listing of sources that implement services provided by
+#the CM UMODE (Compute Minion User Mode) Library
+add_library(cm-umode STATIC
+    src/etsoc/hal/pmu.c
+    #TODO:others to come ..
+)
+
+set_target_properties(cm-umode PROPERTIES LINKER_LANGUAGE C)
+
+target_include_directories(cm-umode
+    PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:${CM_UMODE_INSTALL_PREFIX}/include>
+)
+
+#This macro preserves the driectory structure as defined by the
+#CM UMODE listing above
+MACRO(InstallHdrsWithDirStruct HEADER_LIST)
+    FOREACH(HEADER ${${HEADER_LIST}})
+    STRING(REGEX MATCH "(.*)[/\]" DIR ${HEADER})
+    INSTALL(FILES ${HEADER} DESTINATION ${CM_UMODE_INSTALL_PREFIX}/${DIR})
+    ENDFOREACH(HEADER)
+ENDMACRO(InstallHdrsWithDirStruct)
+
+InstallHdrsWithDirStruct(CM_UMODE_HDRS)
+InstallHdrsWithDirStruct(CM_UMODE_LIB_HDRS)
+
+################################################
+#Install and export cm-umode library and headers
+################################################
+
+install(
+    TARGETS cm-umode
+    EXPORT cm-umodeTargets
+    LIBRARY DESTINATION ${CM_UMODE_INSTALL_PREFIX}/lib
+    ARCHIVE DESTINATION ${CM_UMODE_INSTALL_PREFIX}/lib
+    INCLUDES DESTINATION ${CM_UMODE_INSTALL_PREFIX}/include
+)
+
+#TODO: Could be improved and made more flexible by exporting a package
+install(
+    EXPORT cm-umodeTargets
+    NAMESPACE cm-umode-lib::
+    DESTINATION ${CM_UMODE_INSTALL_PREFIX}/cmake/cm-umode-libs
+)
+
---
+++ et-common-libs/include/etsoc/isa/fcc.h
@@ -0,0 +1,54 @@
+#ifndef FCC_H
+#define FCC_H
+
+#include "device-common/esr_defines.h"
+
+#include <stdint.h>
+
+#define THIS_SHIRE 0xFF
+#define THREAD_0   0
+#define THREAD_1   1
+
+typedef enum { FCC_0 = 0, FCC_1 = 1 } fcc_t;
+
+// shire = shire to send the credit to, 0-32 or 0xFF for "this shire"
+// thread = thread to send credit to, 0 or 1
+// fcc = fast credit counter to send credit to, 0 or 1
+// bitmask = which minion to send credit to. bit 0 = minion 0, bit 31 = minion 31
+#define SEND_FCC(shire, thread, fcc, bitmask) \
+    (*((volatile uint64_t *)ESR_SHIRE(shire, FCC_CREDINC_0) + (thread * 2) + fcc) = bitmask)
+
+// fcc = fast credit counter to block on, 0 or 1
+#define WAIT_FCC(fcc) asm volatile("csrwi fcc, %0" : : "I"(fcc))
+
+static inline void wait_fcc(fcc_t fcc)
+{
+    asm volatile("csrw fcc, %0" : : "r"(fcc));
+}
+
+static inline uint64_t read_fcc(fcc_t fcc)
+{
+    uint64_t temp;
+    uint64_t val;
+
+    asm volatile("   csrr  %0, fccnb  \n" // read FCCNB
+                 "   beqz  %2, 1f     \n" // if FCC1, shift FCCNB 31:16 down to 15:0
+                 "   srli  %0, %0, 16 \n"
+                 "1: lui   %1, 0x10   \n" // mask with 0xFFFF
+                 "   addiw %1, %1, -1 \n"
+                 "   and   %0, %0, %1 \n"
+                 : "=&r"(val), "=r"(temp)
+                 : "r"(fcc));
+
+    return val;
+}
+
+static inline void init_fcc(fcc_t fcc)
+{
+    // Consume all credits
+    for (uint64_t i = read_fcc(fcc); i > 0; i--) {
+        wait_fcc(fcc);
+    }
+}
+
+#endif // FCC_H
