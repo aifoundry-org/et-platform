8f2381963cc9e18fe480a428367bc59b31f0652d
Dennis Huang <dennis.huang@esperantotech.com>
2021-07-01 13:49:34 -0700

    [device-bootloaders] Add usleep() of libc

---
+++ device-bootloaders/src/ServiceProcessorBL2/utils/usleep.c
@@ -0,0 +1,26 @@
+/***********************************************************************
+*
+* Copyright (C) 2021 Esperanto Technologies Inc.
+* The copyright to the computer program(s) herein is the
+* property of Esperanto Technologies, Inc. All Rights Reserved.
+* The program(s) may be used and/or copied only with
+* the written permission of Esperanto Technologies and
+* in accordance with the terms and conditions stipulated in the
+* agreement/contract under which the program(s) have been supplied.
+*
+************************************************************************/
+
+#include <unistd.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*
+** Implementation for usleep of libc
+** This function should only be called after vTaskStartScheduler()
+*/
+int usleep(useconds_t usec)
+{
+  vTaskDelay(usec/portTICK_PERIOD_MS/1000);
+  return 0;
+}
