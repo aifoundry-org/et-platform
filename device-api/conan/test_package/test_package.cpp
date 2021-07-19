7d04b4c4eb343586b0da185d50074581abc674f1
Pau Farre <pau.farre@esperantotech.com>
2021-07-19 08:58:11 +0000

    [device-api] [SW-5190] Package device-api

---
+++ device-api/conan/test_package/test_package.cpp
@@ -0,0 +1,10 @@
+
+#include "esperanto/device-apis/device_apis_message_types.h"
+#include "esperanto/device-apis/management-api/device_mgmt_api_cxx.h"
+#include "esperanto/device-apis/operations-api/device_ops_api_cxx.h"
+
+#include <iostream>
+
+int main() {
+    std::cout << "SUCCESS" << std::endl;
+}
---
+++ device-api/conanfile.py
@@ -0,0 +1,36 @@
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+
+class DeviceApiConan(ConanFile):
+    name = "deviceApi"
+    version = "0.1.0"
+    
+    settings = "os", "compiler", "arch", "build_type"
+    exports_sources = "cmake/*", "src/*", "CMakeLists.txt", "deviceApiConfig.cmake.in"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+ 
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    _cmake = None
+    def _configure_cmake(self):
+        if not self._cmake:
+            cmake = CMake(self)
+            cmake.configure()
+            self._cmake = cmake
+        return self._cmake
+    
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+    
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+        
+    def package_id(self):
+        self.info.header_only()
