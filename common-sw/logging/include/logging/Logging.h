5a758e2e2ae145eaa3f73d6dd29e7ed17197bf52
Javier Vera <javier.vera@esperantotech.com>
2021-07-19 23:01:51 +0200

    [common-sw] Added new macro for LOG_IF

--- common-sw/logging/include/logging/Logging.h
+++ common-sw/logging/include/logging/Logging.h
@@ -13,11 +13,9 @@
 #include "DefaultSinks.h"
 #include "Logger.h"
 #include <thread>
-#define ET_LOG(channel, severity)                                                                                      \
-  LOG(severity) << "ET [" << #channel << "]: [TH: " << std::hex << std::this_thread::get_id() << "]: "
-#define ET_DLOG(channel, severity)                                                                                     \
-  LOG(DEBUG) << "ET [" << #channel << "]: [TH: " << std::hex << std::this_thread::get_id() << "]: "
-#define CAT_NX(A, B) A ## B
-#define ET_VLOG(channel, level)                                                                                        \
-  LOG(CAT_NX(logging::VLOG_, level)) << "ET [" << #channel << "][TH: " << std::hex << std::this_thread::get_id()       \
-                                     << "]: "
+
+#define ET_LOG(channel, severity) LOG(severity) << "ET [" << #channel << "]: "
+#define ET_DLOG(channel, severity) LOG(DEBUG) << "ET [" << #channel << "]: "
+#define CAT_NX(A, B) A##B
+#define ET_VLOG(channel, level) LOG(CAT_NX(logging::VLOG_, level)) << "ET [" << #channel << "]: "
+#define ET_LOG_IF(channel, severity, condition) LOG_IF(severity, condition) << "ET [" << #channel << "]: "
