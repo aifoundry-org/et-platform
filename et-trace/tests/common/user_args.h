8ef3cc74c04ee36d44e02a2d6918e3b4146c529a
Calvin Bulla <calvin.bulla@esperantotech.com>
2021-08-04 18:20:47 +0200

    [et-trace] Add extern "C"

--- et-trace/tests/common/user_args.h
+++ et-trace/tests/common/user_args.h
@@ -1,11 +1,19 @@
 #ifndef USER_ARGS_H
 #define USER_ARGS_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct user_args {
     int seed;
     const char *output;
 };
 
 void parse_args(int argc, const char **argv, struct user_args *uargs);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
