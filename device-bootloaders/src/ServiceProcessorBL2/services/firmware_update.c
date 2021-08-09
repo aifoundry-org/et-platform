0af246c70eefbc64219a661df349e1d644443236
Muhammad Bilal <muhammad_bilal@mentor.com>
2021-08-09 10:19:12 +0500

    [device-bootloaders] Fix dm_svc_get_firmware_status when it gets fail

--- device-bootloaders/src/ServiceProcessorBL2/services/firmware_update.c
+++ device-bootloaders/src/ServiceProcessorBL2/services/firmware_update.c
@@ -102,6 +102,7 @@ static int32_t dm_svc_get_firmware_status(void)
 
     if (0 != flash_fs_get_boot_counters(&attempted_boot_counter, &completed_boot_counter)) {
         Log_Write(LOG_LEVEL_ERROR, "flash_fs_get_boot_counters: failed to get boot counters!\n");
+        return DEVICE_FW_UPDATED_IMAGE_BOOT_FAILED;
     } else {
         Log_Write(LOG_LEVEL_ERROR, "flash_fs_get_boot_counters: Success!\n");
         if (attempted_boot_counter != completed_boot_counter) {
