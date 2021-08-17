347573d45172a5b115e7a9acdf33cc3378c538ff
Javier Vera <javier.vera@esperantotech.com>
2021-08-17 15:20:38 +0000

    [common-sw] Jvera/sw 8011 implement threadpool

---
+++ common-sw/threadPool/include/threadPool/ThreadPool.h
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------------------
+ * Copyright (C) 2021, Esperanto Technologies Inc.
+ * The copyright to the computer program(s) herein is the
+ * property of Esperanto Technologies, Inc. All Rights Reserved.
+ * The program(s) may be used and/or copied only with
+ * the written permission of Esperanto Technologies and
+ * in accordance with the terms and conditions stipulated in the
+ * agreement/contract under which the program(s) have been supplied.
+ *-------------------------------------------------------------------------*/
+
+#pragma once
+#include <condition_variable>
+#include <cstddef>
+#include <functional>
+#include <mutex>
+#include <thread>
+#include <vector>
+namespace threadPool {
+
+class ThreadPool {
+public:
+  using Task = std::function<void()>;
+  explicit ThreadPool(size_t numThreads);
+  void pushTask(Task task);
+  ~ThreadPool();
+
+private:
+  void workerFunc();
+  std::mutex mutex_;
+  bool running_;
+  std::condition_variable condVar_;
+  std::vector<Task> tasks_;
+  std::vector<std::thread> threads_;
+};
+} // namespace threadPool
---
+++ common-sw/threadPool/tests/testThreadPool.cpp
@@ -0,0 +1,40 @@
+//******************************************************************************
+// Copyright (C) 2021, Esperanto Technologies Inc.
+// The copyright to the computer program(s) herein is the
+// property of Esperanto Technologies, Inc. All Rights Reserved.
+// The program(s) may be used and/or copied only with
+// the written permission of Esperanto Technologies and
+// in accordance with the terms and conditions stipulated in the
+// agreement/contract under which the program(s) have been supplied.
+//------------------------------------------------------------------------------
+
+#include "threadPool/ThreadPool.h"
+#include <atomic>
+#include <gtest/gtest.h>
+#include <logging/Logger.h>
+using namespace threadPool;
+TEST(ThreadPool, simple) {
+  bool taskExecuted = false;
+  {
+    ThreadPool tp(5);
+    tp.pushTask([&taskExecuted] { taskExecuted = true; });
+  }
+  ASSERT_TRUE(taskExecuted);
+}
+
+TEST(ThreadPool, 1000tasks) {
+  std::atomic<int> acum = 0;
+  {
+    ThreadPool tp(20);
+    for (int i = 1; i <= 1000; ++i) {
+      tp.pushTask([&acum, i] { acum += i; });
+    }
+  }
+  ASSERT_EQ(acum, (1000 * 1001) / 2);
+}
+
+int main(int argc, char** argv) {
+  logging::LoggerDefault logger_;
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
