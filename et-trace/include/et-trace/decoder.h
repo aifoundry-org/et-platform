f64834bd35d04544f8cff6ddcb39701bbdd910b9
Muhammad Umair Anwar <muhammadumair.anwar@esperantotech.com>
2021-08-31 21:39:23 +0500

    [et-trace] SW-9047: Updated Trace decoder to decode buffer with trace size header.

--- et-trace/include/et-trace/decoder.h
+++ et-trace/include/et-trace/decoder.h
@@ -59,11 +59,12 @@ struct trace_buffer_std_header_t;
  *
  *   DESCRIPTION
  *
- *       This function decoders a trace buffer one entry at a time.
+ *       This function decodes a trace sub buffer (with trace standard header)
+ *       one entry at a time.
  *
  *   INPUTS
  *
- *       tb     Pointer to the trace buffer header.
+ *       tb     Pointer to the trace buffer standard header.
  *       prev   Pointer to the last entry, or
  *              NULL if this is the first time this function is called.
  *
@@ -76,7 +77,37 @@ struct trace_buffer_std_header_t;
  *              On decoder errors or wrong inputs, the function returns NULL.
  *
  ***********************************************************************/
-const void *Trace_Decode(const struct trace_buffer_std_header_t *tb, const void *prev);
+const struct trace_entry_header_t *Trace_Decode(const struct trace_buffer_std_header_t *tb,
+                const struct trace_entry_header_t *prev);
+
+/***********************************************************************
+ *
+ *   FUNCTION
+ *
+ *       Trace_Decode_Sub
+ *
+ *   DESCRIPTION
+ *
+ *       This function decodes a trace sub buffer (with trace size header)
+ *       one entry at a time.
+ *
+ *   INPUTS
+ *
+ *       tb     Pointer to the trace buffer size header.
+ *       prev   Pointer to the last entry, or
+ *              NULL if this is the first time this function is called.
+ *
+ *   OUTPUTS
+ *
+ *       void*  Pointer to the next entry in the trace buffer, or
+ *              NULL if the end of the trace buffer has been reached.
+ *              Note: This is a void* to support both trace_entry_ext_t
+ *              and trace_entry_t types depending on the trace type.
+ *              On decoder errors or wrong inputs, the function returns NULL.
+ *
+ ***********************************************************************/
+const struct trace_entry_header_t *Trace_Decode_Sub(const struct trace_buffer_size_header_t *tb,
+                const struct trace_entry_header_t *prev);
 
 #ifdef ET_TRACE_DECODER_IMPL
 
@@ -84,37 +115,16 @@ const void *Trace_Decode(const struct trace_buffer_std_header_t *tb, const void 
 
 #include <stdlib.h>
 
-const void *Trace_Decode(const struct trace_buffer_std_header_t *tb, const void *prev)
+static inline const struct trace_entry_header_t *get_next_trace_event(const struct trace_entry_header_t *packet)
 {
-    if (tb == NULL)
-        return NULL;
-
-    const size_t buffer_size = tb->data_size;
-
-    if (prev == NULL) {
-        /* Check if valid trace buffer */
-        if (tb->magic_header != TRACE_MAGIC_HEADER)
-            return NULL;
-        if (buffer_size <= sizeof(struct trace_buffer_std_header_t))
-            return NULL;
-        /* First entry */
-        return tb + 1;
-    }
-
-    /* Invalid prev entry */
-    if (prev < (const void *)tb)
-        return NULL;
-
-    const uint16_t entry_type = ((const struct trace_entry_header_t *)prev)->type;
-    size_t payload_size;
-
+    size_t payload_size = 0;
 #define ET_TRACE_PAYLOAD_SIZE(E, S) \
     case TRACE_TYPE_##E:            \
         payload_size = S;           \
         break;
 
     /* Get payload size depending on entry type */
-    switch (entry_type) {
+    switch (packet->type) {
         ET_TRACE_PAYLOAD_SIZE(VALUE_U8, sizeof(struct trace_value_u8_t))
         ET_TRACE_PAYLOAD_SIZE(VALUE_U16, sizeof(struct trace_value_u16_t))
         ET_TRACE_PAYLOAD_SIZE(VALUE_U32, sizeof(struct trace_value_u32_t))
@@ -127,7 +137,7 @@ const void *Trace_Decode(const struct trace_buffer_std_header_t *tb, const void 
         payload_size = sizeof(struct trace_entry_header_t)      /* header*/
                        + sizeof(uint64_t)                       /* src_addr */
                        + sizeof(uint64_t)                       /* size */
-                       + ((struct trace_memory_t *)prev)->size; /* data */
+                       + ((struct trace_memory_t *)packet)->size; /* data */
         break;
     }
         ET_TRACE_PAYLOAD_SIZE(EXCEPTION, 0)
@@ -142,7 +152,63 @@ const void *Trace_Decode(const struct trace_buffer_std_header_t *tb, const void 
     if (payload_size == 0)
         return NULL;
 
-    const void *next = (const uint8_t *)prev + payload_size;
+    return ((struct trace_entry_header_t *)((const uint8_t *)packet + payload_size));
+}
+
+const struct trace_entry_header_t *Trace_Decode(const struct trace_buffer_std_header_t *tb,
+                const struct trace_entry_header_t *prev)
+{
+    if (tb == NULL)
+        return NULL;
+
+    const size_t buffer_size = tb->data_size;
+
+    if (prev == NULL) {
+        /* Check if valid trace buffer */
+        if (tb->magic_header != TRACE_MAGIC_HEADER)
+            return NULL;
+        if (buffer_size <= sizeof(struct trace_buffer_std_header_t))
+            return NULL;
+        /* First entry */
+        return (struct trace_entry_header_t *)(tb + 1);
+    }
+
+    /* Invalid prev entry */
+    if (prev < (const void *)tb)
+        return NULL;
+
+    const struct trace_entry_header_t *next = get_next_trace_event(prev);;
+
+    /* End of buffer? */
+    const size_t cur_size = (const uint8_t *)next - (const uint8_t *)tb;
+    if (cur_size >= buffer_size)
+        return NULL;
+
+    return next;
+}
+
+
+const struct trace_entry_header_t *Trace_Decode_Sub(const struct trace_buffer_size_header_t *tb,
+                const struct trace_entry_header_t *prev)
+{
+    if (tb == NULL)
+        return NULL;
+
+    const size_t buffer_size = tb->data_size;
+
+    if (prev == NULL) {
+        /* Check if valid trace buffer */
+        if (buffer_size <= sizeof(struct trace_buffer_size_header_t))
+            return NULL;
+        /* First entry */
+        return (struct trace_entry_header_t *)(tb + 1);
+    }
+
+    /* Invalid prev entry */
+    if (prev < (const void *)tb)
+        return NULL;
+
+    const struct trace_entry_header_t *next = get_next_trace_event(prev);;
 
     /* End of buffer? */
     const size_t cur_size = (const uint8_t *)next - (const uint8_t *)tb;
