1b7f93060a7a2b0435ef338955b00dc382778abd
Mahad Saleem <Mahad_Saleem@mentor.com>
2021-09-06 01:13:54 +0500

    [device-minion-runtime] [SW-9126]: Defining ranges for MM receverable error codes.

--- device-minion-runtime/src/shared/firmware_helpers/include/sp_mm_comms_spec.h
+++ device-minion-runtime/src/shared/firmware_helpers/include/sp_mm_comms_spec.h
@@ -27,25 +27,37 @@ typedef int16_t mm2sp_mm_recoverable_error_code_e;
            When the threshold is hit on SP side, SP will inform the Host
 */
 enum mm2sp_mm_recoverable_error_code_e {
-    MM_HANG_ERROR                     = -1,
-    MM_EXCEPTION_ERROR                = -2,
-    MM_DMA_CONFIG_ERROR               = -3,
-    MM_DMA_TIMEOUT_ERROR              = -4,
-    MM_KERNEL_LAUNCH_ERROR            = -5,
-    MM_CQ_PUSH_ERROR                  = -6,
-    MM_SQ_PROCESSING_ERROR            = -7,
-    MM_SQ_BUFFER_ALIGNMENT_ERROR      = -8,
-    MM_CMD_BARRIER_TIMEOUT_ERROR      = -9,
-    MM_CM_RESERVE_SLOT_ERROR          = -10,
-    MM_CM_RESERVE_TIMEOUT_ERROR       = -11,
-    MM_CM_KERNEL_ABORT_TIMEOUT_ERROR  = -12,
-    MM_MM2CM_CMD_ERROR                = -13,
-    MM_CM2MM_CMD_ERROR                = -14,
-    MM_CM2MM_KERNEL_LAUNCH_ERROR      = -15,
-    MM_CM2MM_KERNEL_EXCEPTION_ERROR   = -16,
-    MM_SQ_HP_PROCESSING_ERROR         = -17,
-    MM_SQ_HP_BUFFER_ALIGNMENT_ERROR   = -18,
-    MM_SQ_CMDS_ABORTED                = -19
+    /* DMAW error codes */
+    MM_DMA_ERRORS_START               = 0,
+    MM_DMA_CONFIG_ERROR               = -1,
+    MM_DMA_TIMEOUT_ERROR              = -2,
+    MM_DMA_ERRORS_END                 = -19,
+    /* KW error codes */
+    MM_KW_ERRORS_START                = -20,
+    MM_KERNEL_LAUNCH_ERROR            = -21,
+    MM_KW_ERRORS_END                  = -39,
+    /* SQW error codes */
+    MM_SQW_ERRORS_START               = -40,
+    MM_CQ_PUSH_ERROR                  = -41,
+    MM_SQ_PROCESSING_ERROR            = -42,
+    MM_SQ_BUFFER_ALIGNMENT_ERROR      = -43,
+    MM_SQ_HP_PROCESSING_ERROR         = -44,
+    MM_SQ_HP_BUFFER_ALIGNMENT_ERROR   = -45,
+    MM_SQ_CMDS_ABORTED                = -46,
+    MM_SQW_ERRORS_END                 = -59,
+    /* CM runtime error codes */
+    MM_CM_RUNTIME_ERRORS_START        = -60,
+    MM_CM_RESERVE_SLOT_ERROR          = -61,
+    MM_CM_KERNEL_ABORT_TIMEOUT_ERROR  = -62,
+    MM_MM2CM_CMD_ERROR                = -63,
+    MM_CM2MM_CMD_ERROR                = -64,
+    MM_CM2MM_KERNEL_LAUNCH_ERROR      = -65,
+    MM_CM2MM_KERNEL_EXCEPTION_ERROR   = -66,
+    MM_CM_RUNTIME_ERRORS_END          = -79,
+    /* Dispatcher error codes */
+    MM_DISPATCHER_ERROR_START         = -80,
+    MM_DISPATCHER_ERROR_END           = -99,
+    MM_HANG_ERROR                     = -100,
 };
 
 typedef int16_t mm2sp_sp_recoverable_error_code_e;
