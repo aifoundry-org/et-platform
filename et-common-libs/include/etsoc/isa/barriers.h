68f3996ab8345ad602b55921b8be4d16b0741bb1
Arvind Raghuraman <arvind_raghuraman@mentor.com>
2021-09-09 16:09:47 +0000

    [et-common-libs] [SW-9093] minion-bl.lib

---
+++ et-common-libs/include/etsoc/isa/barriers.h
@@ -0,0 +1,44 @@
+/*-------------------------------------------------------------------------
+* Copyright (C) 2019, Esperanto Technologies Inc.
+* The copyright to the computer program(s) herein is the
+* property of Esperanto Technologies, Inc. All Rights Reserved.
+* The program(s) may be used and/or copied only with
+* the written permission of Esperanto Technologies and
+* in accordance with the terms and conditions stipulated in the
+* agreement/contract under which the program(s) have been supplied.
+*-------------------------------------------------------------------------
+*/
+
+#ifndef _BARRIERS_H_
+#define _BARRIERS_H_
+
+#include <inttypes.h>
+
+#include "utils.h"
+#include "esr_defines.h"
+
+#ifdef __cplusplus
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
+#endif
+
+// Shire-only barrier using FLBs and FCCs
+inline uint64_t __attribute__((always_inline)) shire_barrier(uint64_t flb, uint64_t fcc, uint64_t thread_count, uint64_t minion_mask_t0, uint64_t minion_mask_t1)
+{
+  uint64_t last = flbarrier(flb, thread_count - 1);
+
+  if (last) {
+    uint64_t minion_id, thread_id;
+    minion_id = get_minion_id() & (SOC_MINIONS_PER_SHIRE - 1);
+    thread_id = get_thread_id();
+    fcc_send(SHIRE_OWN, THREAD_0, fcc, minion_mask_t0);
+    fcc_send(SHIRE_OWN, THREAD_1, fcc, minion_mask_t1);
+  }
+  fcc_consume(fcc);
+
+  return last;
+}
+
+#endif
+
---
+++ et-common-libs/minion_bl.cmake
@@ -0,0 +1,92 @@
+########################
+# Minion Boot Loader
+########################
+
+#Minion Boot Loader requires etsoc_hal library
+find_package(etsoc_hal REQUIRED)
+
+#Install prefix for Minion Bootloader library
+set(MINION_BL_INSTALL_PREFIX ${CMAKE_INSTALL_PREFIX}/minion-bl)
+
+################################################
+# List the public interfaces and headers to be
+# exposed to Minion Boot Loader
+################################################
+
+#Listing of header only public interfaces
+set(MINION_BL_HDRS
+    include/etsoc/isa/atomic.h
+    include/etsoc/isa/atomic-impl.h
+    include/etsoc/isa/esr_defines.h
+    include/etsoc/isa/macros.h
+    include/etsoc/isa/fcc.h
+    include/etsoc/isa/flb.h
+    include/etsoc/isa/hart.h
+    include/etsoc/isa/sync.h
+    include/etsoc/isa/cacheops.h
+    include/etsoc/isa/syscall.h
+    include/etsoc/isa/syscall_internal.h
+    include/etsoc/isa/riscv_encoding.h
+    include/etsoc/isa/utils.h
+    include/etsoc/hal/pmu.h
+    include/system/layout.h
+    include/system/etsoc_ddr_region_map.h
+)
+
+#Listing of public headers that expose services provided by
+#the MINION_BL (Minion Bootloader Library
+set(MINION_BL_LIB_HDRS
+    include/transports/mm_cm_iface/broadcast.h
+)
+
+#########################
+#Create minion-bl library
+#########################
+
+#Listing of sources that implement services provided by
+#the MINION_BL (Minion Bootloader) Library
+add_library(minion-bl STATIC
+    src/transports/mm_cm_iface/broadcast.c
+)
+
+set_target_properties(minion-bl PROPERTIES LINKER_LANGUAGE C)
+
+target_link_libraries(minion-bl PUBLIC etsoc_hal::etsoc_hal)
+
+target_include_directories(minion-bl
+    PUBLIC
+        $<BUILD_INTERFACE:${CMAKE_CURRENT_SOURCE_DIR}/include>
+        $<INSTALL_INTERFACE:${MINION_BL_INSTALL_PREFIX}/include>
+)
+
+#################################################
+#Install and export minion-bl library and headers
+#################################################
+
+#This macro preserves the driectory structure as defined by the
+#MINION_BL listing above
+MACRO(InstallHdrsWithDirStruct HEADER_LIST)
+    FOREACH(HEADER ${${HEADER_LIST}})
+    STRING(REGEX MATCH "(.*)[/\]" DIR ${HEADER})
+    INSTALL(FILES ${HEADER} DESTINATION ${MINION_BL_INSTALL_PREFIX}/${DIR})
+    ENDFOREACH(HEADER)
+ENDMACRO(InstallHdrsWithDirStruct)
+
+InstallHdrsWithDirStruct(MINION_BL_HDRS)
+InstallHdrsWithDirStruct(MINION_BL_LIB_HDRS)
+
+install(
+    TARGETS minion-bl
+    EXPORT minion-blTargets
+    LIBRARY DESTINATION ${MINION_BL_INSTALL_PREFIX}/lib
+    ARCHIVE DESTINATION ${MINION_BL_INSTALL_PREFIX}/lib
+    INCLUDES DESTINATION ${MINION_BL_INSTALL_PREFIX}/include
+)
+
+#TODO: Could be improved and made more flexible by exporting a package
+install(
+    EXPORT minion-blTargets
+    NAMESPACE minion-bl-lib::
+    DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/cmake/et-common-libs/minion-bl
+)
+
