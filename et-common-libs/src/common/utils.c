f790f25387076bf47896a1e4f501b082ad201050
Mahad Saleem <Mahad_Saleem@mentor.com>
2021-09-17 01:48:30 +0500

    [et-common-libs] [SW-9286]: Adding abiltiy to self abort a kernel.
    Signed-off-by: Mahad Saleem <Mahad_Saleem@mentor.com>

--- et-common-libs/src/common/utils.c
+++ et-common-libs/src/common/utils.c
@@ -55,8 +55,7 @@ size_t et_strlen(const char *str)
 
 __attribute__((noreturn)) void et_abort(void)
 {
-  /* TODO: Need to differentiate from self abort and normal kernel execution exit */
-  syscall(SYSCALL_RETURN_FROM_KERNEL, (uint64_t)-1, 0, 0);
+  syscall(SYSCALL_RETURN_FROM_KERNEL, 0, KERNEL_RETURN_SELF_ABORT, 0);
 
   /* Should never get here */
   while (1) {
