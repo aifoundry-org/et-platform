576992761e520778f8036062d3a677a81abcd5df
Muhammad Umair Anwar <muhammadumair.anwar@esperantotech.com>
2021-09-21 23:01:46 +0500

    [test-compute-kernels] SW-9341: Updated Trace test for variable et_printf arguments

--- test-compute-kernels/trace/trace.c
+++ test-compute-kernels/trace/trace.c
@@ -4,8 +4,11 @@
 
 int64_t main(void)
 {
+    char str[] = "Message";
+
     et_printf("Hello from CM UMode.\n\r");
     et_printf("Now you can Debug me using Trace (~_^).\n\r");
+    et_printf("Test: String \"%s\" has length %d.\n\r", str, et_strlen(str));
 
     return 0;
 }
