0b882ccd32357784ee5ece6cfe674775cf5c4496
Shankar Jayaratnam <shankar.jayaratnam@esperantotech.com>
2021-09-22 15:48:20 -0700

    [et-common-libs] Update UART to differentiate between SP/PU

--- et-common-libs/include/etsoc/drivers/serial/serial.h
+++ et-common-libs/include/etsoc/drivers/serial/serial.h
@@ -4,8 +4,10 @@
 #include <stdint.h>
 #include "etsoc_hal/inc/hal_device.h"
 
-#define UART0 R_PU_UART_BASEADDR
-#define UART1 R_PU_UART1_BASEADDR
+#define SP_UART0 R_SP_UART0_BASEADDR 
+#define SP_UART1 R_SP_UART1_BASEADDR  
+#define PU_UART0 R_PU_UART_BASEADDR
+#define PU_UART1 R_PU_UART1_BASEADDR
 
 int SERIAL_init(uintptr_t uartRegs);
 int SERIAL_write(uintptr_t uartRegs, const char *const string, int length);
