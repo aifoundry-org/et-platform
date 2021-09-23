c9b7917c619bce87727916d215636c8ba429b85c
msalman <muhammad.salman@esperantotech.com>
2021-09-23 17:40:54 +0000

    [sw-sysemu] [sw-9404]: adding SPI driver changes for flash fs

--- sw-sysemu/devices/spi.h
+++ sw-sysemu/devices/spi.h
@@ -13,6 +13,7 @@
 
 #include <array>
 #include <cstdint>
+#include <vector>
 #include "literals.h"
 #include "memory/memory_error.h"
 #include "memory/memory_region.h"
@@ -27,26 +28,47 @@ struct Spi : public MemoryRegion {
     using pointer       = typename MemoryRegion::pointer;
     using const_pointer = typename MemoryRegion::const_pointer;
 
+    enum : unsigned long long {
+        SSI_RXFLR_ADDRESS = 0x24,
+        SSI_SR_ADDRESS = 0x28,
+        SSI_DR0_ADDRESS = 0x60,
+    };
     static_assert(N == 4_KiB, "bemu::Spi has illegal size");
+    std::vector<std::uint32_t> data;
 
     void read(const Agent& agent, size_type pos, size_type n, pointer result) override {
         uint32_t *result32 = reinterpret_cast<uint32_t *>(result);
-        (void) result32;
 
         LOG_AGENT(DEBUG, agent, "Spi%d::read(pos=0x%llx)", ID, pos);
 
         if (n != 4)
             throw memory_error(first() + pos);
+
+        switch(pos)
+        {
+            case SSI_SR_ADDRESS:
+                *result32 = 0x04; /* Emulating Empty FIFO (Bit[2]==1) and busy (BIT[0] == 0) */
+                break;
+            case SSI_RXFLR_ADDRESS:
+                *result32 = 0x01; /* Emulate that RX FIFO has data */
+                break;
+            default:
+                *result32 = 0;
+        }
     }
 
     void write(const Agent& agent, size_type pos, size_type n, const_pointer source) override {
         const uint32_t *source32 = reinterpret_cast<const uint32_t *>(source);
-        (void) source32;
 
         LOG_AGENT(DEBUG, agent, "Spi%d::write(pos=0x%llx)", ID, pos);
 
         if (n != 4)
             throw memory_error(first() + pos);
+
+        if(pos == SSI_DR0_ADDRESS)
+        {
+            data.push_back(*source32);
+        }
     }
 
     void init(const Agent& agent, size_type pos, size_type n, const_pointer source) override {
