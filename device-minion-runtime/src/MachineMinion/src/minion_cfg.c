2bb9b7f2dd38cc02fb6cf99d5f5d68d8366a8563
Milomir Matijevic <milomir.matijevic@esperantotech.com>
2021-10-01 09:58:04 -0700

    [device-minion-runtime] update minion-runtime pointer

--- device-minion-runtime/src/MachineMinion/src/minion_cfg.c
+++ device-minion-runtime/src/MachineMinion/src/minion_cfg.c
@@ -36,13 +36,6 @@ static uint8_t get_highest_set_bit_offset(uint64_t shire_mask)
     return (uint8_t)(64 - __builtin_clzl(shire_mask));
 }
 
-// get_highest_set_bit_offset
-// This function returns highest set bit offset
-static uint8_t get_highest_set_bit_offset(uint64_t shire_mask)
-{
-    return (uint8_t)(64 - __builtin_clzl(shire_mask));
-}
-
 // Configure Minion PLL to specific mode. This uses the broadcast mechanism hence all Minions
 // will be programmed to the same frequency.
 static int64_t minion_configure_cold_boot_pll(uint64_t shire_mask, uint8_t lvdpll_strap)
@@ -92,17 +85,6 @@ static int64_t minion_configure_cold_boot_pll(uint64_t shire_mask, uint8_t lvdpl
 int64_t dynamic_minion_pll_frequency_update(uint64_t freq)
 {
     int64_t status;
-    uint8_t num_shires;
-    bool up_down;
-
-    if(0 != booted_shire_mask)
-    {
-        num_shires = get_highest_set_bit_offset(booted_shire_mask);
-    }
-    else
-    {
-        return -1;
-    }
 
 #if MM_DVFS_USE_FCW == 1
     status = dvfs_fcw_update_minion_pll_freq(freq, booted_shire_mask, num_shires,
