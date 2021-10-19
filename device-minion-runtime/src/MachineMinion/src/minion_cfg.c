b895ae2749e07af3c89d04e9ab3d7ea06942296f
Milomir Matijevic <milomir.matijevic@esperantotech.com>
2021-10-19 04:25:34 -0700

    [device-minion-runtime] MR AIs

--- device-minion-runtime/src/MachineMinion/src/minion_cfg.c
+++ device-minion-runtime/src/MachineMinion/src/minion_cfg.c
@@ -57,34 +57,22 @@ static int64_t minion_configure_cold_boot_pll(uint64_t shire_mask, uint8_t lvdpl
         get_pll_conf_reg()->num_shires = get_highest_set_bit_offset(shire_mask);
     }
 
-#if MM_DVFS_NORMALIZATION_OFF == 1
+#if DVFS_NORMALIZATION_OFF == 1
     int64_t status;
     uint8_t lvdpll_mode;
     
-    // Normalize clock at 800MHz
-    lvdpll_mode = freq_to_mode(800, lvdpll_strap);
-    status = pll_normalize_and_turn_of_normalization(lvdpll_mode, shire_mask, get_pll_conf_reg()->num_shires);
-    if (status != 0)
-    {
-        return status;
-    }
-
-    // Postdiv times normalization frequency gives oscilator frequency
-    get_pll_conf_reg()->minion_norm_freq = gs_lvdpll_settings[(lvdpll_mode-1)].values[14] * 800;
+    // Disable auto-normalization
+    turn_off_pll_auto_normalization(shire_mask, get_pll_conf_reg()->num_shires);
 
     lvdpll_mode = freq_to_mode(INITIAL_MINION_FREQ, lvdpll_strap);
-    status = update_minion_pll_freq_quick(lvdpll_mode, shire_mask, get_pll_conf_reg()->num_shires);
-    if (status != 0)
-    {
-        return status;
-    }
-    get_pll_conf_reg()->minion_current_freq = (uint64_t)mode_to_freq(lvdpll_mode, minion_lvdpll_strap);
+    // Postdiv times normalization frequency gives oscilator frequency
+    get_pll_conf_reg()->minion_norm_freq = gs_lvdpll_settings[(lvdpll_mode-1)].values[14] * INITIAL_MINION_FREQ;
+    get_pll_conf_reg()->minion_current_freq = INITIAL_MINION_FREQ;
 #else
     /* Switch to LVDPLL output */
     pll_broadcast_req (PP_MACHINE, REGION_OTHER, ETSOC_SHIRE_OTHER_ESR_SHIRE_CTRL_CLOCKMUX_ADDRESS,
                         shire_mask, SELECT_PLL_CLOCK_0);
     get_pll_conf_reg()->minion_current_freq = INITIAL_MINION_FREQ;
-    get_pll_conf_reg()->minion_norm_freq = INITIAL_MINION_FREQ;
 #endif
 
     get_pll_conf_reg()->booted_shire_mask = shire_mask;
@@ -98,12 +86,13 @@ int64_t dynamic_minion_pll_frequency_update(uint64_t freq)
 {
     int64_t status;
 
-#if MM_DVFS_USE_FCW == 1
+#if DVFS_USE_FCW == 1
+    uint16_t ref_clock = (lvdpll_strap == 0) ? 100 : (lvdpll_strap == 1) ? 24 : 40;
     status = dvfs_fcw_update_minion_pll_freq(freq, booted_shire_mask, num_shires,
-                minion_current_freq, minion_norm_freq, minion_lvdpll_strap, MM_DVFS_POLL_FOR_LOCK);
+                minion_current_freq, minion_norm_freq, ref_clock, DVFS_POLL_FOR_LOCK);
 #else
     status = dvfs_update_minion_pll_mode(freq_to_mode((uint16_t)freq, get_pll_conf_reg()->minion_lvdpll_strap),
-                get_pll_conf_reg()->booted_shire_mask, get_pll_conf_reg()->num_shires, MM_DVFS_POLL_FOR_LOCK);
+                get_pll_conf_reg()->booted_shire_mask, get_pll_conf_reg()->num_shires, DVFS_POLL_FOR_LOCK);
 #endif
 
     get_pll_conf_reg()->minion_current_freq = (uint16_t)freq;
