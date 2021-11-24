d4d0626b94779e1d5fe52303416b6717a53049f0
Calvin Bulla <calvin.bulla@esperantotech.com>
2021-11-24 13:33:41 +0100

    [sw-sysemu] [BEMU] Fix array size for PMU types
    These arrays are used as `pmu_events[NTHREADS][6]`.
    For std::arrays the dimensions need to be inverted.
    The same memory (NTHREADS x 6) is allocated, but compiling
    with `-fsanitize-address` will produce an error.

--- sw-sysemu/system.h
+++ sw-sysemu/system.h
@@ -60,8 +60,8 @@ using Coop_tload_table  = std::array<std::array<Coop_tload_state, 32>, 2>;
 class System {
 public:
     // ----- Types -----
-    using neigh_pmu_counters_t  = std::array<std::array<uint64_t, EMU_THREADS_PER_MINION>, 6>;
-    using neigh_pmu_events_t    = std::array<std::array<uint8_t, EMU_THREADS_PER_NEIGH>, 6>;
+    using neigh_pmu_counters_t  = std::array<std::array<uint64_t, 6>, EMU_THREADS_PER_MINION>;
+    using neigh_pmu_events_t    = std::array<std::array<uint8_t, 6>, EMU_THREADS_PER_NEIGH>;
 
     using msg_func_t = std::function<void(unsigned)>;
     using hart_mask_t = std::bitset<EMU_NUM_THREADS>;
