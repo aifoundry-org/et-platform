ed1299d59fcc575a01716b4ac0d06d0a83541832
Pau Farre <pau.farre@esperantotech.com>
2021-12-10 18:14:43 +0100

    [sw-sysemu] Update conanfile
    - move test_package to the project-top-level dir

--- sw-sysemu/conanfile.py
+++ sw-sysemu/conanfile.py
@@ -1,19 +1,13 @@
-from conans import ConanFile, CMake, tools
-from conans.tools import Version, load
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake
+from conan.tools.layout import cmake_layout
+from conans import tools
 import os
 import re
 
-def get_version():
-    try:
-        content = load("CMakeLists.txt")
-        version = re.search("project\(sw-sysemu VERSION (.*)\)", content).group(1)
-        return version.strip()
-    except Exception as e:
-        return None
 
 class SwSysemuConan(ConanFile):
     name = "sw-sysemu"
-    version = get_version()
     url = "https://gitlab.esperanto.ai/software/sw-sysemu"
     description = "The functional ETSOC-1 emulator. Able to execute RISC-V instructions with Esperanto extensions"
     license = "Esperanto Technologies"
@@ -21,90 +15,97 @@ class SwSysemuConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
-        "fPIC": [True, False],
         "profiling": [True, False],
         "backtrace": [True, False]
     }
     default_options = {
         "shared": False,
-        "fPIC": True,
         "profiling": False,
         "backtrace": False
     }
 
-    generators = "cmake_find_package_multi"
+    scm = {
+        "type": "git",
+        "url": "git@gitlab.esperanto.ai:software/sw-sysemu.git",
+        "revision": "auto",
+    }
+    generators = "CMakeDeps"
 
-    #TODO: Ideally we should have all sources inside 'src'
-    exports_sources = [
-        "CMakeLists.txt",
-        "*.h",
-        "*.cpp",
-        "devices/*",
-        "fpu/*",
-        "insns/*",
-        "memory/*",
-        "softfloat/*",
-        "sw-sysemu/*",
-        "sys_emu/*",
-        "sw-sysemuConfig.cmake.in"
-    ]
+    python_requires = "conan-common/[>=0.2.0 <1.0.0]"
 
-    requires = [
-        "glog/0.4.0",
-        "elfio/3.8"
-    ]
-    python_requires = "conan-common/0.1.0"
+    def set_version(self):
+        content = tools.load(os.path.join(self.recipe_folder, "CMakeLists.txt"))
+        version = re.search(r"set\(PROJECT_VERSION (.*)\)", content).group(1)
+        self.version = version.strip()
 
     def requirements(self):
+        self.requires("glog/0.4.0")
+        self.requires("elfio/3.8")
         if self.options.backtrace:
             self.requires("libunwind/1.5.0")
-    
-    @property
-    def _build_subfolder(self):
-        return "build_subfolder"
-        
-    def configure(self):
+
+    def validate(self):
         check_req_min_cppstd = self.python_requires["conan-common"].module.check_req_min_cppstd
         check_req_min_cppstd(self, "17")
 
-    _cmake = None
-    def _configure_cmake(self):
-        if not self._cmake:
-            cmake = CMake(self)
-            cmake.definitions["CMAKE_INSTALL_LIBDIR"] = "lib"
-            cmake.definitions["PROFILING"] = self.options.profiling
-            cmake.definitions["BACKTRACE"] = self.options.backtrace
-            cmake.configure(build_folder=self._build_subfolder)
-            self._cmake = cmake
-        return self._cmake
-    
+        if self.settings.os != "Linux":
+            self.output.warn("%s has only been tested under Linux. You're on your own" % self.name)
+
+    def layout(self):
+        cmake_layout(self)
+        self.cpp.source.includedirs = ["."]
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["PROFILING"] = self.options.profiling
+        tc.variables["BACKTRACE"] = self.options.backtrace
+        tc.variables["CMAKE_INSTALL_LIBDIR"] = "lib"
+        tc.generate()
+
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
     
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
     
     def package_info(self):
         # library components
-        self.cpp_info.components["libsysemu"].names["cmake_find_package"] = "sysemu"
-        self.cpp_info.components["libsysemu"].names["cmake_find_package_multi"] = "sysemu"
-        self.cpp_info.components["libsysemu"].requires = ["elfio::elfio"]
-        self.cpp_info.components["libsysemu"].libs = ["sysemu"]
+        libsysemu_comp_name = "libsysemu"
+        libsysemu_cmake_name = "sysemu"
+        self.cpp_info.components[libsysemu_comp_name].names["cmake_find_package"] = libsysemu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libsysemu_comp_name].names["cmake_find_package_multi"] = libsysemu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libsysemu_comp_name].set_property("cmake_target_name", libsysemu_cmake_name)
+        self.cpp_info.components[libsysemu_comp_name].requires = ["elfio::elfio"]
+        self.cpp_info.components[libsysemu_comp_name].libs = ["sysemu"]
+        self.cpp_info.components[libsysemu_comp_name].defines = ["SYS_EMU"]
+        self.cpp_info.components[libsysemu_comp_name].includedirs =  ["include"]
+        self.cpp_info.components[libsysemu_comp_name].libdirs = ["lib"]
         if self.options.backtrace:
-            self.cpp_info.components["libsysemu"].requires.append("libunwind::libunwind")
-        
-        self.cpp_info.components["libfpu"].names["cmake_find_package"] = "fpu"
-        self.cpp_info.components["libfpu"].names["cmake_find_package_multi"] = "fpu"
-        self.cpp_info.components["libfpu"].requires = []
-        self.cpp_info.components["libfpu"].libs = ["fpu"]
+            self.cpp_info.components[libsysemu_comp_name].requires.append("libunwind::libunwind")
 
-        self.cpp_info.components["libsw-sysemu"].names["cmake_find_package"] = "sw-sysemu"
-        self.cpp_info.components["libsw-sysemu"].names["cmake_find_package_multi"] = "sw-sysemu"
-        self.cpp_info.components["libsw-sysemu"].requires = ["libsysemu", "libfpu", "glog::glog"]
-        self.cpp_info.components["libsw-sysemu"].libs = ["sw-sysemu"]
+        libfpu_comp_name = "libfpu"
+        libfpu_cmake_name = "fpu"
+        self.cpp_info.components[libfpu_comp_name].names["cmake_find_package"] = libfpu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libfpu_comp_name].names["cmake_find_package_multi"] = libfpu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libfpu_comp_name].set_property("cmake_target_name", libfpu_cmake_name)
+        self.cpp_info.components[libfpu_comp_name].requires = []
+        self.cpp_info.components[libfpu_comp_name].libs = ["fpu"]
+        self.cpp_info.components[libfpu_comp_name].includedirs =  ["include"]
+        self.cpp_info.components[libfpu_comp_name].libdirs = ["lib"]
+
+        libsw_sysemu_comp_name = "libsw-sysemu"
+        libsw_sysemu_cmake_name = "sw-sysemu"
+        self.cpp_info.components[libsw_sysemu_comp_name].names["cmake_find_package"] = libsw_sysemu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libsw_sysemu_comp_name].names["cmake_find_package_multi"] = libsw_sysemu_cmake_name # deprecated (remove with conan 2.0)
+        self.cpp_info.components[libsw_sysemu_comp_name].set_property("cmake_target_name", libsw_sysemu_cmake_name)
+        self.cpp_info.components[libsw_sysemu_comp_name].requires = [libsysemu_comp_name, libfpu_comp_name, "glog::glog"]
+        self.cpp_info.components[libsw_sysemu_comp_name].libs = ["sw-sysemu"]
+        self.cpp_info.components[libsw_sysemu_comp_name].includedirs =  ["include"]
+        self.cpp_info.components[libsw_sysemu_comp_name].libdirs = ["lib"]
 
         # utilities
         bin_path = os.path.join(self.package_folder, "bin")
@@ -113,7 +114,6 @@ class SwSysemuConan(ConanFile):
         self.output.info("Appending PATH env var with : {}".format(bin_path))
         self.env_info.PATH.append(bin_path)
 
-        sys_emu = tools.unix_path(os.path.join(self.package_folder, "bin", "sys_emu" + bin_ext))
+        sys_emu = os.path.join(self.package_folder, "bin", "sys_emu" + bin_ext)
         self.output.info("Setting SYS_EMU to {}".format(sys_emu))
         self.env_info.SYS_EMU = sys_emu
-
