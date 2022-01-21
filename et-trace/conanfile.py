5cc572e7f4f23a93a30dc07a9e31d715f2fdd6c3
Pau Farre <pau.farre@esperantotech.com>
2022-01-21 12:46:39 +0100

    [et-trace] Update conanfile
    - Add 'with_tests' option (enabled by default) which will compile the tests
      and execute them in the 'build()' phase
    - Take package version from cmake
    - Cmake is in charge of defining what files are installed (except config files)
    - Add test_package
    - Ignore test_package/build

--- et-trace/conanfile.py
+++ et-trace/conanfile.py
@@ -1,18 +1,58 @@
-from conans import ConanFile
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain
+from conans import tools
+import re
+import os
+
 
 class EsperantoTraceConan(ConanFile):
     name = "esperantoTrace"
-    version = "0.1.0"
-    # No settings/options are necessary, this is header only
-    exports_sources = "include/*.h"
-    no_copy_source = True
+    url = "https://gitlab.esperanto.ai/software/et-trace"
+    license = "Esperanto Technologies"
 
+    settings = "os", "compiler", "arch", "build_type"
+    options = {
+        "with_tests": [True, False]
+    }
+    default_options = {
+        "with_tests": True
+    }
+
+    scm = {
+        "type": "git",
+        "url": "git@gitlab.esperanto.ai:software/et-trace.git",
+        "revision": "auto",
+    }
+    generator = "CMakeDeps"
+
+    def set_version(self):
+        content = tools.load(os.path.join(self.recipe_folder, "CMakeLists.txt"))
+        version = re.search(r"set\(PROJECT_VERSION (.*)\)", content).group(1)
+        self.version = version.strip()
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["ET_TRACE_TEST"] = self.options.with_tests
+        tc.generate()
+    
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+        if self.options.with_tests and not tools.cross_building(self.settings):
+            cmake.test()
+    
     def package(self):
-        self.copy("*.h")
+        cmake = CMake(self)
+        cmake.install()
+        tools.rmdir(os.path.join(self.package_folder, "lib"))
     
     def package_id(self):
         self.info.header_only()
 
     def package_info(self):
-        self.cpp_info.components["et_trace"].names["cmake_find_package"] = "et_trace"
-        self.cpp_info.components["et_trace"].names["cmake_find_package_multi"] = "et_trace"
+        et_trace_cmake_name = "esperantoTrace::et_trace"
+        self.cpp_info.components["et_trace"].names["cmake_find_package"] = et_trace_cmake_name
+        self.cpp_info.components["et_trace"].names["cmake_find_package_multi"] = et_trace_cmake_name
+        self.cpp_info.components["et_trace"].set_property("cmake_target_name", et_trace_cmake_name)
+        self.cpp_info.components["et_trace"].includedirs = ["include", "include/esperanto"]
---
+++ et-trace/test_package/test_package.c
@@ -0,0 +1,10 @@
+
+#include <et-trace/encoder.h>
+#include <et-trace/decoder.h>
+#include <et-trace/layout.h>
+
+#include <stdio.h>
+
+int main() {
+    printf("SUCCESS\n");
+}
