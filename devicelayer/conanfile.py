75a9ad66e52277161948ac1affe4e1917b0d954f
Pau Farre <pau.farre@esperantotech.com>
2022-01-24 19:39:57 +0100

    [devicelayer] Update conanfile

--- devicelayer/conanfile.py
+++ devicelayer/conanfile.py
@@ -1,11 +1,13 @@
-from conans import ConanFile, tools
+from conan import ConanFile
+from conan import tools
 from conan.tools.cmake import CMake, CMakeToolchain
 from conan.tools.layout import cmake_layout
-from conans.errors import ConanInvalidConfiguration
 import os
+import re
+
+
 class DeviceLayerConan(ConanFile):
     name = "deviceLayer"
-    version = "0.1.0"
     url = "https://gitlab.esperanto.ai/software/deviceLayer"
     description = ""
     license = "Esperanto Technologies"
@@ -14,38 +16,46 @@ class DeviceLayerConan(ConanFile):
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
-        "with_fake_interface": [True, False],
-        "with_mock_interface": [True, False],
+        "with_tests": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
-        "with_fake_interface": False,
-        "with_mock_interface": False
+        "with_tests": False,
     }
 
-    generators = "cmake_find_package_multi"
+    scm = {
+        "type": "git",
+        "url": "git@gitlab.esperanto.ai:software/devicelayer.git",
+        "revision": "auto",
+    }
+    generators = "CMakeDeps"
 
-    exports_sources = [ "CMakeLists.txt", "cmake/*", "include/*", "src/*", "DeviceLayerConfig.cmake.in" ]
 
-    requires = [ 
-        "sw-sysemu/0.2.0",
-        "hostUtils/0.1.0",
-        "linuxDriver/0.1.0",
-        "boost/1.72.0"
-    ]
-    build_requires = "cmake-modules/[>=0.4.1 <1.0.0]"
     python_requires = "conan-common/[>=0.1.0 <1.0.0]"
+        
+    def set_version(self):
+        content = tools.load(os.path.join(self.recipe_folder, "CMakeLists.txt"))
+        version = re.search(r"set\(PROJECT_VERSION (.*)\)", content).group(1)
+        self.version = version.strip()
     
-    def configure(self):
+    def requirements(self):
+        self.requires("sw-sysemu/0.2.0")
+        self.requires("hostUtils/0.1.0")
+        self.requires("linuxDriver/0.1.0")
+        self.requires("boost/1.72.0")
+
+        # IDeviceLayerFake.h
+        self.requires("deviceApi/0.1.0")
+        # IDeviceLayerMock.h
+        self.requires("gtest/1.8.1")
+
+    def build_requirements(self):
+        self.test_requires("cmake-modules/[>=0.4.1 <1.0.0]")
+    
+    def validate(self):
         check_req_min_cppstd = self.python_requires["conan-common"].module.check_req_min_cppstd
         check_req_min_cppstd(self, "17")
-
-    def requirements(self):
-        if self.options.get_safe("with_fake_interface"):
-            self.requires("deviceApi/0.1.0")
-        if self.options.get_safe("with_mock_interface"):
-            self.requires("gtest/1.8.1")
         
     def layout(self):
         cmake_layout(self)
@@ -53,41 +63,35 @@ class DeviceLayerConan(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["ENABLE_TESTS"] = False
+        tc.variables["ENABLE_TESTS"] = self.options.get_safe("with_tests")
         tc.variables["ENABLE_DEPRECATED"] = False
         tc.variables["CMAKE_INSTALL_LIBDIR"] = "lib"
-        tc.variables["CMAKE_MODULE_PATH"] = os.path.join(self.deps_cpp_info["cmake-modules"].rootpath, "cmake")
+        tc.variables["CMAKE_MODULE_PATH"] = os.path.join(self.dependencies["cmake-modules"].package_folder, "cmake")
         tc.generate()
     
-    _cmake = None
-    def _configure_cmake(self):
-        if not self._cmake:
-            cmake = CMake(self)
-            cmake.configure()
-            self._cmake = cmake
-        return self._cmake
-    
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
     
     def package(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
     
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.requires = [
-            # deviceLayer public
+            # IDeviceLayer.h
             "sw-sysemu::sw-sysemu",
             "hostUtils::debug",
+            # IDeviceLayerFake.h
+            "deviceApi::deviceApi",
+            # IDeviceLayerMock.h
+            "gtest::gmock",
+
             # deviceLayer private
             "hostUtils::logging",
             "linuxDriver::linuxDriver",
             "boost::boost"
         ]
-        if self.options.get_safe("with_fake_interface"):
-            self.cpp_info.requires.append("deviceApi::deviceApi")
-        if self.options.get_safe("with_mock_interface"):
-            self.cpp_info.requires.append("gtest::gmock")
--- devicelayer/test_package/conanfile.py
+++ devicelayer/test_package/conanfile.py
@@ -1,9 +1,11 @@
-from conans import ConanFile, CMake, tools
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conans import tools
 import os
 
-class LoggingTestConan(ConanFile):
+class DeviceLayerTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def build(self):
         cmake = CMake(self)
