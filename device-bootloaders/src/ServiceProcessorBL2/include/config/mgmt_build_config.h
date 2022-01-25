51869a144c9db0279fdd4d754134a4f383d9d57d
Muhammad Umair Anwar <muhammadumair.anwar@esperantotech.com>
2022-01-25 18:37:16 +0500

    [device-bootloaders] [SW-10907] clang-format changes.
    Signed-off-by: Muhammad Umair Anwar <muhammadumair.anwar@esperantotech.com>

--- device-bootloaders/src/ServiceProcessorBL2/include/config/mgmt_build_config.h
+++ device-bootloaders/src/ServiceProcessorBL2/include/config/mgmt_build_config.h
@@ -42,41 +42,41 @@
     \brief Macro that provides the base address of the DIRs
     SP DEV Interface Register at PC_SP Mailbox
 */
-#define SP_DEV_INTF_BASE_ADDR      R_PU_MBOX_PC_SP_BASEADDR
+#define SP_DEV_INTF_BASE_ADDR R_PU_MBOX_PC_SP_BASEADDR
 
 /*! \def SP_DEV_INTF_SIZE
     \brief Macro that provides the total allowed size of the SP DIRs
     at PC_SP Mailbox
 */
-#define SP_DEV_INTF_SIZE           0x200
+#define SP_DEV_INTF_SIZE 0x200
 
 /*! \def SP_INTERRUPT_TRG_REGION_BASE_ADDR
     \brief This is the region dedicated to device's MBOX triggers
 */
-#define SP_INTERRUPT_TRG_REGION_BASE_ADDR  R_PU_TRG_PCIE_BASEADDR
+#define SP_INTERRUPT_TRG_REGION_BASE_ADDR R_PU_TRG_PCIE_BASEADDR
 
 /*! \def SP_INTERRUPT_TRG_REGION_SIZE
     \brief Total size of the region dedicated to device's MBOX triggers
 */
-#define SP_INTERRUPT_TRG_REGION_SIZE       R_PU_TRG_PCIE_SIZE
+#define SP_INTERRUPT_TRG_REGION_SIZE R_PU_TRG_PCIE_SIZE
 
 /*! \def SP_INTERRUPT_TRG_OFFSET
     \brief A macro that provides the offset for triggering the interrupt for SP
     in Interrupt Trigger Region.
 */
-#define SP_INTERRUPT_TRG_OFFSET     0U
+#define SP_INTERRUPT_TRG_OFFSET 0U
 
 /*! \def SP_INTERRUPT_TRG_SIZE
     \brief A macro that provides the size of the field for SP
     in Interrupt Trigger Region.
 */
-#define SP_INTERRUPT_TRG_SIZE       4U
+#define SP_INTERRUPT_TRG_SIZE 4U
 
 /*! \def SP_INTERRUPT_TRG_ID
     \brief A macro that provides the ID/value to write for SP
     in Interrupt Trigger Region.
 */
-#define SP_INTERRUPT_TRG_ID         1U
+#define SP_INTERRUPT_TRG_ID 1U
 
 /* DDR Region 0 DEV_MANAGEMENT_SCRATCH (BAR=0, Offset=0, Size=4MB) */
 
@@ -84,7 +84,7 @@
     \brief A macro that provides the PCI BAR region using which
     the Service Processor scratch space can be accessed
 */
-#define SP_DEV_INTF_DEV_MANAGEMENT_SCRATCH_BAR    0
+#define SP_DEV_INTF_DEV_MANAGEMENT_SCRATCH_BAR 0
 
 /*! \def SP_DEV_INTF_DEV_MANAGEMENT_SCRATCH_OFFSET
     \brief A macro that provides the offset of Service Processor scratch space
@@ -96,7 +96,7 @@
     \brief A macro that provides the total size of Service Processor scratch space
     on PCI BAR.
 */
-#define SP_DEV_INTF_DEV_MANAGEMENT_SCRATCH_SIZE   SP_DM_SCRATCH_REGION_SIZE
+#define SP_DEV_INTF_DEV_MANAGEMENT_SCRATCH_SIZE SP_DM_SCRATCH_REGION_SIZE
 
 /* DDR Region 1 TRACE_BUFFER (BAR=0, Offset=0x400000, Size=4KB) */
 
@@ -104,20 +104,19 @@
     \brief A macro that provides the PCI BAR region using which
     the Service Processor trace buffer space can be accessed
 */
-#define SP_DEV_INTF_TRACE_BUFFER_BAR               0
+#define SP_DEV_INTF_TRACE_BUFFER_BAR 0
 
 /*! \def SP_DEV_INTF_TRACE_BUFFER_OFFSET
     \brief A macro that provides the offset of Service Processor
     trace buffer space on PCI BAR
 */
-#define SP_DEV_INTF_TRACE_BUFFER_OFFSET            0x400000UL
+#define SP_DEV_INTF_TRACE_BUFFER_OFFSET 0x400000UL
 
 /*! \def SP_DEV_INTF_TRACE_BUFFER_SIZE
     \brief A macro that provides the total size of Service Processor
     trace buffer space on PCI BAR.
 */
-#define SP_DEV_INTF_TRACE_BUFFER_SIZE              SP_TRACE_BUFFER_SIZE
-
+#define SP_DEV_INTF_TRACE_BUFFER_SIZE SP_TRACE_BUFFER_SIZE
 
 /* DDR Region 2 MM TRACE_BUFFER (BAR=0, Offset=0x401000, Size=1MB) */
 
@@ -125,20 +124,20 @@
     \brief A macro that provides the PCI BAR region using which
     the Service Processor trace buffer space can be accessed
 */
-#define SP_DEV_INTF_MM_TRACE_BUFFER_BAR               0
+#define SP_DEV_INTF_MM_TRACE_BUFFER_BAR 0
 
 /*! \def SP_DEV_INTF_TRACE_BUFFER_OFFSET
     \brief A macro that provides the offset of Service Processor
     trace buffer space on PCI BAR
 */
-#define SP_DEV_INTF_MM_TRACE_BUFFER_OFFSET            (SP_DEV_INTF_TRACE_BUFFER_OFFSET + \
-                                                        SP_DEV_INTF_TRACE_BUFFER_SIZE)
+#define SP_DEV_INTF_MM_TRACE_BUFFER_OFFSET \
+    (SP_DEV_INTF_TRACE_BUFFER_OFFSET + SP_DEV_INTF_TRACE_BUFFER_SIZE)
 
 /*! \def SP_DEV_INTF_TRACE_BUFFER_SIZE
     \brief A macro that provides the total size of Service Processor
     trace buffer space on PCI BAR.
 */
-#define SP_DEV_INTF_MM_TRACE_BUFFER_SIZE               MM_TRACE_BUFFER_SIZE
+#define SP_DEV_INTF_MM_TRACE_BUFFER_SIZE MM_TRACE_BUFFER_SIZE
 
 /* DDR Region 2 CM TRACE_BUFFER (BAR=0, Offset=0x501000, Size=8MB) */
 
@@ -146,20 +145,20 @@
     \brief A macro that provides the PCI BAR region using which
     the Service Processor trace buffer space can be accessed
 */
-#define SP_DEV_INTF_CM_TRACE_BUFFER_BAR               0
+#define SP_DEV_INTF_CM_TRACE_BUFFER_BAR 0
 
 /*! \def SP_DEV_INTF_CM_TRACE_BUFFER_OFFSET
     \brief A macro that provides the offset of Service Processor
     trace buffer space on PCI BAR
 */
-#define SP_DEV_INTF_CM_TRACE_BUFFER_OFFSET            (SP_DEV_INTF_MM_TRACE_BUFFER_OFFSET + \
-                                                        SP_DEV_INTF_MM_TRACE_BUFFER_SIZE)
+#define SP_DEV_INTF_CM_TRACE_BUFFER_OFFSET \
+    (SP_DEV_INTF_MM_TRACE_BUFFER_OFFSET + SP_DEV_INTF_MM_TRACE_BUFFER_SIZE)
 
 /*! \def SP_DEV_INTF_CM_TRACE_BUFFER_SIZE
     \brief A macro that provides the total size of Service Processor
     trace buffer space on PCI BAR.
 */
-#define SP_DEV_INTF_CM_TRACE_BUFFER_SIZE               CM_SMODE_TRACE_BUFFER_SIZE
+#define SP_DEV_INTF_CM_TRACE_BUFFER_SIZE CM_SMODE_TRACE_BUFFER_SIZE
 
 /* Interrupt Trigger Region (BAR=2, Offset=0x2000, Size=8KB) */
 
@@ -167,19 +166,19 @@
     \brief A macro that provides the PCI BAR region using which
     the interrupt trigger region space can be accessed
 */
-#define SP_DEV_INTF_INTERRUPT_TRG_REGION_BAR        2
+#define SP_DEV_INTF_INTERRUPT_TRG_REGION_BAR 2
 
 /*! \def SP_DEV_INTF_INTERRUPT_TRG_REGION_OFFSET
     \brief A macro that provides the offset of Service Processor
     interrupt trigger region space on PCI BAR
 */
-#define SP_DEV_INTF_INTERRUPT_TRG_REGION_OFFSET     0x2000UL
+#define SP_DEV_INTF_INTERRUPT_TRG_REGION_OFFSET 0x2000UL
 
 /*! \def SP_DEV_INTF_INTERRUPT_TRG_REGION_SIZE
     \brief A macro that provides the total size of Service Processor
     interrupt trigger region space on PCI BAR.
 */
-#define SP_DEV_INTF_INTERRUPT_TRG_REGION_SIZE       SP_INTERRUPT_TRG_REGION_SIZE
+#define SP_DEV_INTF_INTERRUPT_TRG_REGION_SIZE SP_INTERRUPT_TRG_REGION_SIZE
 
 /*****************************************************/
 /* Definitions to locate and manage Host to SP SQ/CQ */
@@ -190,25 +189,25 @@
     of the PC_SP_Mailbox is placed
     R_PU_MBOX_PC_SP   BAR2 + 0x1000   4k     Mailbox shared memory
 */
-#define SP_PC_MAILBOX_BAR_OFFSET  0x1000UL
+#define SP_PC_MAILBOX_BAR_OFFSET 0x1000UL
 
 /*! \def SP_VQ_BAR
     \brief A macro that provides the PCI BAR region using which
     the Service Processor virtual queues can be accessed
 */
-#define SP_VQ_BAR          2
+#define SP_VQ_BAR 2
 
 /*! \def SP_VQ_OFFSET
     \brief A macro that provides the PCI BAR region offset using which
     the Service Processor virtual queues can be accessed
 */
-#define SP_VQ_OFFSET       0x200UL
+#define SP_VQ_OFFSET 0x200UL
 
 /*! \def SP_SQ_OFFSET
     \brief A macro that provides the PCI BAR region offset relative to
     SP_VQ_BAR using which the Service Processor submission queues can be accessed
 */
-#define SP_SQ_OFFSET       0x0UL
+#define SP_SQ_OFFSET 0x0UL
 
 /*! \def SP_SQS_BASE_ADDR
     \brief A macro that provides the Service Processor's 32 bit base address
@@ -220,13 +219,13 @@
     \brief A macro that provides the total size for SP VQs (SQs + CQs)
     on PCI BAR.
 */
-#define SP_VQ_BAR_SIZE      0xE00UL
+#define SP_VQ_BAR_SIZE 0xE00UL
 
 /*! \def SP_SQ_COUNT
     \brief A macro that provides the Service Processor submission queue
     count
 */
-#define SP_SQ_COUNT         1
+#define SP_SQ_COUNT 1
 
 /*! \def SP_MAX_SUPPORTED_SQS
     \brief Maximum supported submission queues by Service Processor
@@ -237,7 +236,7 @@
     \brief A macro that provides size of the Service Processor
     submission queue. All submision queues will be of same size.
 */
-#define SP_SQ_SIZE          (SP_VQ_BAR_SIZE / 2)
+#define SP_SQ_SIZE (SP_VQ_BAR_SIZE / 2)
 
 /*! \def SP_SQ_MEM_TYPE
     \brief A macro that provides the memory type for SP submission queues
@@ -245,7 +244,7 @@
     1 - SRAM
     2 - DRAM
 */
-#define SP_SQ_MEM_TYPE      UNCACHED
+#define SP_SQ_MEM_TYPE UNCACHED
 
 /*! \def SP_CQS_BASE_ADDRESS
     \brief A macro that provides the Service Processor's base address
@@ -257,19 +256,19 @@
     \brief A macro that provides the PCI BAR region offset relative to
     SP_VQ_BAR using which the Service Processor completion queues can be accessed
 */
-#define SP_CQ_OFFSET       (SP_SQ_OFFSET + (SP_SQ_COUNT * SP_SQ_SIZE))
+#define SP_CQ_OFFSET (SP_SQ_OFFSET + (SP_SQ_COUNT * SP_SQ_SIZE))
 
 /*! \def SP_CQ_SIZE
     \brief A macro that provides size of the Service Processor
     completion queue.
 */
-#define SP_CQ_SIZE          (SP_VQ_BAR_SIZE / 2)
+#define SP_CQ_SIZE (SP_VQ_BAR_SIZE / 2)
 
 /*! \def SP_CQ_COUNT
     \brief A macro that provides the Service Processor completion queue
     count
 */
-#define SP_CQ_COUNT         1
+#define SP_CQ_COUNT 1
 
 /*! \def SP_MAX_SUPPORTED_CQS
     \brief Maximum supported completion queues by Service Processor
@@ -288,48 +287,47 @@
     1 - SRAM
     2 - DRAM
 */
-#define SP_CQ_MEM_TYPE      UNCACHED
+#define SP_CQ_MEM_TYPE UNCACHED
 
 /*! \def SP_CMD_MAX_SIZE
     \brief A macro that provides the maximum command size on the host <> mm
     communication interface in bytes
 */
 /* TODO: Fine tune this value according to the final device-ops-api spec */
-#define SP_CMD_MAX_SIZE      64U
+#define SP_CMD_MAX_SIZE 64U
 
 /*! \def SP_CMD_MAX_TIMEOUT
     \brief A macro that provides the timeout for SP to MM commands. SP will
            wait for response from MM for this much time at max.
            NOTE: This value is in milliseconds. Should be fine tuned for Sillicon.
 */
-#define SP2MM_CMD_TIMEOUT      5
-
+#define SP2MM_CMD_TIMEOUT 5
 
 /*! \def HOST_VQ_MAX_TIMEOUT
     \brief A macro that provides the timeout for host VQ locks
            NOTE: This value is in terms of OS ticks i.e. TickType_t.
 */
-#define HOST_VQ_MAX_TIMEOUT      200
+#define HOST_VQ_MAX_TIMEOUT 200
 
 /*! \def PMIC_TEMP_LOWER_SET_LIMIT
     \brief A macro that provides pmic minimum temperature threshold value
 */
-#define PMIC_TEMP_LOWER_SET_LIMIT      55
+#define PMIC_TEMP_LOWER_SET_LIMIT 55
 
 /*! \def PMIC_TEMP_UPPER_SET_LIMIT
     \brief A macro that provides pmic maximum temperature threshold value
 */
-#define PMIC_TEMP_UPPER_SET_LIMIT      85
+#define PMIC_TEMP_UPPER_SET_LIMIT 85
 
 /*! \def TEMP_THRESHOLD_HI
     \brief A macro that provides pmic temperature threshold value
 */
-#define TEMP_THRESHOLD_HW_CATASTROPHIC    80
+#define TEMP_THRESHOLD_HW_CATASTROPHIC 80
 
 /*! \def TEMP_THRESHOLD_LO
     \brief A macro that provides early indication temperature threshold value
 */
-#define TEMP_THRESHOLD_SW_MANAGED         70
+#define TEMP_THRESHOLD_SW_MANAGED 70
 
 /*! \def POWER_THRESHOLD_HW_CATASTROPHIC
     \brief A macro that provides power threshold value.
@@ -337,14 +335,14 @@
 *          to raise the interrupt if power goes beyond this threshold.
 *          Following M.2 spec.
 */
-#define POWER_THRESHOLD_HW_CATASTROPHIC   40
+#define POWER_THRESHOLD_HW_CATASTROPHIC 40
 
 /*! \def POWER_THRESHOLD_SW_MANAGED
     \brief A macro that provides early indication power threshold value.
 *          This power threshold will be set as initial value for SW power managing.
 *          Following M.2 spec.
 */
-#define POWER_THRESHOLD_SW_MANAGED        25
+#define POWER_THRESHOLD_SW_MANAGED 25
 
 /*! \def SAFE_POWER_THRESHOLD
     \brief A macro that provides safe power threshold value.
@@ -352,92 +350,92 @@
 *          this power threshold.
 *          Following M.2 spec.
 */
-#define SAFE_POWER_THRESHOLD              12
+#define SAFE_POWER_THRESHOLD 12
 
 /*! \def SAFE_STATE_FREQUENCY
     \brief A macro that provides safe state frequency
 */
-#define SAFE_STATE_FREQUENCY      400
+#define SAFE_STATE_FREQUENCY 400
 
 /*! \def POWER_SCALE_FACTOR
     \brief A macro that provides power scale factor in percentages.
 */
-#define POWER_SCALE_FACTOR        10
+#define POWER_SCALE_FACTOR 10
 
 /*! \def DELTA_TEMP_UPDATE_PERIOD
     \brief A macro that provides dTj/dt - time(uS) for Junction temperature to be updated
            This value needs to be characterized in Silicon
 */
-#define DELTA_TEMP_UPDATE_PERIOD  1000
+#define DELTA_TEMP_UPDATE_PERIOD 1000
 
 /*! \def USE_FCW_FOR_LVDPLL
     \brief A macro that provides are we using FCW sequence for LVDPLL programming
 */
-#define USE_FCW_FOR_LVDPLL        0
+#define USE_FCW_FOR_LVDPLL 0
 
 /*! \def DELTA_POWER_UPDATE_PERIOD
     \brief A macro that provides power update period
 */
-#define DELTA_POWER_UPDATE_PERIOD  100
+#define DELTA_POWER_UPDATE_PERIOD 100
 
 /*! \def RT_ERROR_THRESHOLD
     \brief Predefined runtime error threshold.
 */
-#define RT_ERROR_THRESHOLD  0
+#define RT_ERROR_THRESHOLD 0
 
 /*! \def PCIE_CORR_ERROR_THRESHOLD
     \brief Predefined threshold for PCIE correctable errors.
 */
-#define PCIE_CORR_ERROR_THRESHOLD  32
+#define PCIE_CORR_ERROR_THRESHOLD 32
 
 /*! \def DDR_CORR_ERROR_THRESHOLD
     \brief Predefined threshold for DDR correctable errors.
 */
-#define DDR_CORR_ERROR_THRESHOLD  32
+#define DDR_CORR_ERROR_THRESHOLD 32
 
 /*! \def SRAM_CORR_ERROR_THRESHOLD
     \brief Predefined threshold for SRAM correctable errors.
 */
-#define SRAM_CORR_ERROR_THRESHOLD  32
+#define SRAM_CORR_ERROR_THRESHOLD 32
 
 /*! \def ICACHE_CORR_ERROR_THRESHOLD
     \brief Predefined threshold for ICACHE correctable errors.
 */
-#define ICACHE_CORR_ERROR_THRESHOLD  32
+#define ICACHE_CORR_ERROR_THRESHOLD 32
 
 /************************
   RTOS Task Properties
  ************************/
 // Main Task
-#define MAIN_TASK_PRIORITY           4
+#define MAIN_TASK_PRIORITY        4
 #define MAIN_TASK_STACK_SIZE      4096
 #define MAIN_DEFAULT_TIMEOUT_MSEC 1000
 // WDOG Service
-#define WDOG_TASK_PRIORITY            4
-#define WDOG_TASK_STACK_SIZE        512
+#define WDOG_TASK_PRIORITY        4
+#define WDOG_TASK_STACK_SIZE      512
 #define WDOG_DEFAULT_TIMEOUT_MSEC 10000
-#define WDOG_TIME_SLICE_PERC         40
+#define WDOG_TIME_SLICE_PERC      40
 // Error Reporting
-#define DM_EVENT_TASK_PRIORITY       4
-#define DM_EVENT_TASK_STACK        1024
+#define DM_EVENT_TASK_PRIORITY 4
+#define DM_EVENT_TASK_STACK    1024
 // Host/MM VQ Command Handler
-#define VQUEUE_TASK_PRIORITY         3
-#define VQUEUE_STACK_SIZE         2048
+#define VQUEUE_TASK_PRIORITY 3
+#define VQUEUE_STACK_SIZE    2048
 // Sampler
-#define DM_TASK_PRIORITY             2
-#define DM_TASK_STACK             1024
-#define DM_TASK_DELAY_MS           100
+#define DM_TASK_PRIORITY 2
+#define DM_TASK_STACK    1024
+#define DM_TASK_DELAY_MS 100
 // Power Management
-#define TT_TASK_PRIORITY             2
-#define TT_TASK_STACK_SIZE        1024
+#define TT_TASK_PRIORITY   2
+#define TT_TASK_STACK_SIZE 1024
 // SMBUS
-#define SMBUS_TASK_PRIORITY          1
+#define SMBUS_TASK_PRIORITY 1
 // Vault IP
-#define VAULTIP_DRIVER_TASK_PRIORITY       1
-#define VAULTIP_DRIVER_TASK_STACK_SIZE  4096
+#define VAULTIP_DRIVER_TASK_PRIORITY   1
+#define VAULTIP_DRIVER_TASK_STACK_SIZE 4096
 // Flash File System
-#define FLASHFS_DRIVER_TASK_PRIORITY       1
-#define FLASHFS_DRIVER_TASK_STACK_SIZE  4096
+#define FLASHFS_DRIVER_TASK_PRIORITY   1
+#define FLASHFS_DRIVER_TASK_STACK_SIZE 4096
 
 /************************/
 /* Compile-time checks  */
--- device-bootloaders/src/test_framework/tf_trace_tests.c
+++ device-bootloaders/src/test_framework/tf_trace_tests.c
@@ -9,75 +9,73 @@
 *-------------------------------------------------------------------------
 */
 
-
 #include "tf.h"
 #include "trace.h"
 
 /* mm_rt_helpers */
 #include "system/layout.h"
 
-int8_t Sp_Trace_Run_Control_Cmd_Handler(const void* test_cmd);
-int8_t Sp_Trace_Run_Config_Cmd_Handler(const void* test_cmd);
-int8_t Sp_Trace_Get_Info_Cmd_Handler(const void* test_cmd);
-int8_t Sp_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd);
-int8_t MM_Trace_Get_Info_Cmd_Handler(const void* test_cmd);
-int8_t MM_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd);
-int8_t CM_Trace_Get_Info_Cmd_Handler(const void* test_cmd);
-int8_t CM_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd);
+int8_t Sp_Trace_Run_Control_Cmd_Handler(const void *test_cmd);
+int8_t Sp_Trace_Run_Config_Cmd_Handler(const void *test_cmd);
+int8_t Sp_Trace_Get_Info_Cmd_Handler(const void *test_cmd);
+int8_t Sp_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd);
+int8_t MM_Trace_Get_Info_Cmd_Handler(const void *test_cmd);
+int8_t MM_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd);
+int8_t CM_Trace_Get_Info_Cmd_Handler(const void *test_cmd);
+int8_t CM_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd);
 
-
-int8_t Sp_Trace_Run_Control_Cmd_Handler(const void* test_cmd)
+int8_t Sp_Trace_Run_Control_Cmd_Handler(const void *test_cmd)
 {
     struct device_mgmt_trace_run_control_cmd_t dm_cmd;
-    const struct tf_cmd_sp_trace_run_control_t* run_control =
-        (const struct tf_cmd_sp_trace_run_control_t*) test_cmd;
+    const struct tf_cmd_sp_trace_run_control_t *run_control =
+        (const struct tf_cmd_sp_trace_run_control_t *)test_cmd;
 
     dm_cmd.control = run_control->control;
     Trace_Process_Control_Cmd(&dm_cmd);
 
-    struct tf_rsp_sp_trace_run_control_t rsp = {0};
+    struct tf_rsp_sp_trace_run_control_t rsp = { 0 };
 
     rsp.rsp_hdr.id = TF_RSP_SP_TRACE_RUN_CONTROL;
     rsp.rsp_hdr.flags = TF_RSP_ONLY;
-    rsp.rsp_hdr.payload_size =  0;
+    rsp.rsp_hdr.payload_size = 0;
 
     TF_Send_Response(&rsp, sizeof(rsp));
 
     return 0;
 }
 
-int8_t Sp_Trace_Run_Config_Cmd_Handler(const void* test_cmd)
+int8_t Sp_Trace_Run_Config_Cmd_Handler(const void *test_cmd)
 {
     struct device_mgmt_trace_config_cmd_t dm_cmd;
-    const struct tf_cmd_sp_trace_run_config_t* config =
-        (const struct tf_cmd_sp_trace_run_config_t*) test_cmd;
+    const struct tf_cmd_sp_trace_run_config_t *config =
+        (const struct tf_cmd_sp_trace_run_config_t *)test_cmd;
 
     dm_cmd.event_mask = config->event_mask;
     dm_cmd.filter_mask = config->filter_mask;
 
     Trace_Process_Config_Cmd(&dm_cmd);
 
-    struct tf_rsp_sp_trace_run_config_t rsp = {0};
+    struct tf_rsp_sp_trace_run_config_t rsp = { 0 };
 
     rsp.rsp_hdr.id = TF_RSP_SP_TRACE_RUN_CONFIG;
     rsp.rsp_hdr.flags = TF_RSP_ONLY;
-    rsp.rsp_hdr.payload_size =  0;
+    rsp.rsp_hdr.payload_size = 0;
 
     TF_Send_Response(&rsp, sizeof(rsp));
 
     return 0;
 }
 
-int8_t Sp_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
+int8_t Sp_Trace_Get_Info_Cmd_Handler(const void *test_cmd)
 {
-    (void) test_cmd;
+    (void)test_cmd;
 
-    struct tf_rsp_sp_trace_get_info_t rsp = {0};
-    const struct trace_control_block_t* cb = Trace_Get_SP_CB();
+    struct tf_rsp_sp_trace_get_info_t rsp = { 0 };
+    const struct trace_control_block_t *cb = Trace_Get_SP_CB();
 
     rsp.rsp_hdr.id = TF_RSP_SP_TRACE_GET_INFO;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =  TF_GET_PAYLOAD_SIZE(rsp);
+    rsp.rsp_hdr.payload_size = TF_GET_PAYLOAD_SIZE(rsp);
     rsp.offset = cb->offset_per_hart;
     rsp.size = cb->size_per_hart;
     rsp.base = cb->base_per_hart;
@@ -87,37 +85,35 @@ int8_t Sp_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
     return 0;
 }
 
-int8_t Sp_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd)
+int8_t Sp_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd)
 {
-    (void) test_cmd;
+    (void)test_cmd;
     struct tf_rsp_sp_trace_get_buffer_t rsp;
-    const struct trace_control_block_t* cb = Trace_Get_SP_CB();
+    const struct trace_control_block_t *cb = Trace_Get_SP_CB();
 
     rsp.bytes_read = cb->offset_per_hart;
     rsp.rsp_hdr.id = TF_RSP_SP_TRACE_GET_BUFFER;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =
-        (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
+    rsp.rsp_hdr.payload_size = (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
 
-    TF_Send_Response_With_Payload(&rsp,
-        (uint32_t) (sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
-        (void*) cb->base_per_hart, rsp.bytes_read);
+    TF_Send_Response_With_Payload(&rsp, (uint32_t)(sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
+                                  (void *)cb->base_per_hart, rsp.bytes_read);
 
     return 0;
 }
 
-int8_t MM_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
+int8_t MM_Trace_Get_Info_Cmd_Handler(const void *test_cmd)
 {
-    (void) test_cmd;
+    (void)test_cmd;
 
-    struct tf_rsp_mm_trace_get_info_t rsp = {0};
+    struct tf_rsp_mm_trace_get_info_t rsp = { 0 };
     struct trace_buffer_std_header_t trace_header;
 
-    memcpy(&trace_header, (void*)MM_TRACE_BUFFER_BASE, sizeof(struct trace_buffer_std_header_t));
+    memcpy(&trace_header, (void *)MM_TRACE_BUFFER_BASE, sizeof(struct trace_buffer_std_header_t));
 
     rsp.rsp_hdr.id = TF_RSP_MM_TRACE_GET_INFO;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =  TF_GET_PAYLOAD_SIZE(rsp);
+    rsp.rsp_hdr.payload_size = TF_GET_PAYLOAD_SIZE(rsp);
     rsp.offset = trace_header.data_size;
     rsp.size = MM_TRACE_BUFFER_SIZE;
     rsp.base = MM_TRACE_BUFFER_BASE;
@@ -127,39 +123,36 @@ int8_t MM_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
     return 0;
 }
 
-int8_t MM_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd)
+int8_t MM_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd)
 {
-    (void) test_cmd;
+    (void)test_cmd;
     struct tf_rsp_mm_trace_get_buffer_t rsp;
     struct trace_buffer_std_header_t trace_header;
 
-    memcpy(&trace_header, (void*)MM_TRACE_BUFFER_BASE, sizeof(struct trace_buffer_std_header_t));
+    memcpy(&trace_header, (void *)MM_TRACE_BUFFER_BASE, sizeof(struct trace_buffer_std_header_t));
 
     rsp.bytes_read = trace_header.data_size;
     rsp.rsp_hdr.id = TF_RSP_MM_TRACE_GET_BUFFER;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =
-        (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
+    rsp.rsp_hdr.payload_size = (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
 
-    TF_Send_Response_With_Payload(&rsp,
-        (uint32_t) (sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
-        (void*) MM_TRACE_BUFFER_BASE, rsp.bytes_read);
+    TF_Send_Response_With_Payload(&rsp, (uint32_t)(sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
+                                  (void *)MM_TRACE_BUFFER_BASE, rsp.bytes_read);
 
     return 0;
 }
 
-int8_t CM_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
+int8_t CM_Trace_Get_Info_Cmd_Handler(const void *test_cmd)
 {
     const struct tf_cmd_cm_trace_get_info_t *cmd =
         (const struct tf_cmd_cm_trace_get_info_t *)test_cmd;
-    struct tf_rsp_cm_trace_get_info_t rsp = {0};
+    struct tf_rsp_cm_trace_get_info_t rsp = { 0 };
 
     if (cmd->buffer_id == 0)
     {
         struct trace_buffer_std_header_t trace_header;
-        memcpy(&trace_header,
-            (void*)CM_SMODE_TRACE_BUFFER_BASE,
-            sizeof(struct trace_buffer_std_header_t));
+        memcpy(&trace_header, (void *)CM_SMODE_TRACE_BUFFER_BASE,
+               sizeof(struct trace_buffer_std_header_t));
         rsp.offset = trace_header.data_size;
         rsp.base = CM_SMODE_TRACE_BUFFER_BASE;
     }
@@ -167,15 +160,17 @@ int8_t CM_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
     {
         struct trace_buffer_size_header_t trace_header;
         memcpy(&trace_header,
-            (void*)(CM_SMODE_TRACE_BUFFER_BASE + (cmd->buffer_id * CM_SMODE_TRACE_BUFFER_SIZE_PER_HART)),
-            sizeof(struct trace_buffer_size_header_t));
+               (void *)(CM_SMODE_TRACE_BUFFER_BASE +
+                        (cmd->buffer_id * CM_SMODE_TRACE_BUFFER_SIZE_PER_HART)),
+               sizeof(struct trace_buffer_size_header_t));
         rsp.offset = trace_header.data_size;
-        rsp.base = CM_SMODE_TRACE_BUFFER_BASE + (cmd->buffer_id * CM_SMODE_TRACE_BUFFER_SIZE_PER_HART);
+        rsp.base =
+            CM_SMODE_TRACE_BUFFER_BASE + (cmd->buffer_id * CM_SMODE_TRACE_BUFFER_SIZE_PER_HART);
     }
 
     rsp.rsp_hdr.id = TF_RSP_CM_TRACE_GET_INFO;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =  TF_GET_PAYLOAD_SIZE(rsp);
+    rsp.rsp_hdr.payload_size = TF_GET_PAYLOAD_SIZE(rsp);
     rsp.size = CM_SMODE_TRACE_BUFFER_SIZE_PER_HART;
 
     TF_Send_Response(&rsp, sizeof(rsp));
@@ -183,23 +178,22 @@ int8_t CM_Trace_Get_Info_Cmd_Handler(const void* test_cmd)
     return 0;
 }
 
-int8_t CM_Trace_Get_Buffer_Cmd_Handler(const void* test_cmd)
+int8_t CM_Trace_Get_Buffer_Cmd_Handler(const void *test_cmd)
 {
-    (void) test_cmd;
+    (void)test_cmd;
     struct tf_rsp_cm_trace_get_buffer_t rsp;
     struct trace_buffer_std_header_t trace_header;
 
-    memcpy(&trace_header, (void*)CM_SMODE_TRACE_BUFFER_BASE, sizeof(struct trace_buffer_std_header_t));
+    memcpy(&trace_header, (void *)CM_SMODE_TRACE_BUFFER_BASE,
+           sizeof(struct trace_buffer_std_header_t));
 
     rsp.bytes_read = trace_header.data_size;
     rsp.rsp_hdr.id = TF_RSP_CM_TRACE_GET_BUFFER;
     rsp.rsp_hdr.flags = TF_RSP_WITH_PAYLOAD;
-    rsp.rsp_hdr.payload_size =
-        (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
+    rsp.rsp_hdr.payload_size = (uint32_t)(sizeof(uint32_t) + rsp.bytes_read);
 
-    TF_Send_Response_With_Payload(&rsp,
-        (uint32_t) (sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
-        (void*) CM_SMODE_TRACE_BUFFER_BASE, rsp.bytes_read);
+    TF_Send_Response_With_Payload(&rsp, (uint32_t)(sizeof(tf_rsp_hdr_t) + sizeof(uint32_t)),
+                                  (void *)CM_SMODE_TRACE_BUFFER_BASE, rsp.bytes_read);
 
     return 0;
 }
