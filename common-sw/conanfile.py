1f20902de5a0aa69d02b2c915392b03f2f853884
Pau Farre <pau.farre@esperantotech.com>
2022-02-07 19:42:55 +0100

    [common-sw] cmake-modules as requires

--- common-sw/conanfile.py
+++ common-sw/conanfile.py
@@ -40,9 +40,8 @@ class HostUtilsConan(ConanFile):
         self.requires("g3log/1.3.3")
         if self.options.with_tests:
             self.requires("gtest/1.8.1")
-    
-    def build_requirements(self):
-        self.test_requires("cmake-modules/[>=0.4.1 <1.0.0]")
+        
+        self.requires("cmake-modules/[>=0.4.1 <1.0.0]")
     
     def validate(self):
         check_req_min_cppstd = self.python_requires["conan-common"].module.check_req_min_cppstd
