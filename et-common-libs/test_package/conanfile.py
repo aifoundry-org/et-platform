93f2a7f5ae30f239663e92071b7d022691b5bedf
Pau Farre <pau.farre@esperantotech.com>
2022-02-08 19:50:51 +0100

    [et-common-libs] Improve test_package

--- et-common-libs/test_package/conanfile.py
+++ et-common-libs/test_package/conanfile.py
@@ -1,9 +1,22 @@
-from conans import ConanFile, CMake, tools
+from pickle import TRUE
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain
+from conans import tools
 import os
+
 
 class EtCommonLibsTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake", "cmake_find_package"
+    generators = "CMakeDeps"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["WITH_SP_BL"] = self.options["et-common-libs"].with_sp_bl
+        tc.variables["WITH_CM_UMODE"] = self.options["et-common-libs"].with_cm_umode
+        tc.variables["WITH_MINION_BL"] = self.options["et-common-libs"].with_minion_bl
+        tc.variables["WITH_MM_RT_SVCS"] = self.options["et-common-libs"].with_mm_rt_svcs
+        tc.variables["WITH_CM_RT_SVCS"] = self.options["et-common-libs"].with_cm_rt_svcs
+        tc.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -11,6 +24,19 @@ class EtCommonLibsTestConan(ConanFile):
         cmake.build()
     
     def test(self):
+        tests = []
+        if self.options["et-common-libs"].with_sp_bl:
+            tests.append("test_package_sp_bl")
+        if self.options["et-common-libs"].with_cm_umode:
+            tests.append("test_package_cm_umode")
+        if self.options["et-common-libs"].with_minion_bl:
+            tests.append("test_package_minion_bl")
+        if self.options["et-common-libs"].with_mm_rt_svcs:
+            tests.append("test_package_mm_rt_svcs")
+        if self.options["et-common-libs"].with_cm_rt_svcs:
+            tests.append("test_package_cm_rt_svcs")
+
         if not tools.cross_building(self.settings):
-            bin_path = os.path.join("bin", "test_package")
-            self.run(bin_path, run_environment=True)
+            for test_package in tests:
+                bin_path = os.path.join("bin", test_package)
+                self.run(bin_path, run_environment=True)
--- et-common-libs/test_package/test_package.c
+++ et-common-libs/test_package/test_package.c
@@ -1,23 +1,161 @@
-#include <etsoc/common/log_common.h>
-#include <etsoc/common/utils.h>
+#if defined(TEST_PACKAGE_SP_BL)
+
+// sp_bl1
 #include <etsoc/drivers/pmu/pmu.h>
+#include <etsoc/isa/io.h>
+#include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
+#include <etsoc/isa/etsoc_memory.h>
+#include <etsoc/isa/esr_defines.h>
+#include <etsoc/isa/macros.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/flb.h>
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/sync.h>
+#include <etsoc/isa/cacheops.h>
+#include <etsoc/isa/riscv_encoding.h>
+#include <etsoc/isa/utils.h>
+#include <system/layout.h>
+#include <system/etsoc_ddr_region_map.h>
+
+#include <etsoc/drivers/serial/serial.h>
+
+// sp_bl2
+#include <etsoc/common/common_defs.h>
+#include <etsoc/common/log_common.h>
+// #include <etsoc/common/utils.h> // includes trace/trace_umode_intern.h but umode trace headers aren't shipped with sp_bl2
+#include <etsoc/drivers/pcie/pcie_int.h>
+#include <etsoc/drivers/pmu/pmu.h>
+#include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
+#include <etsoc/isa/etsoc_memory.h>
+#include <etsoc/isa/io.h>
+#include <etsoc/isa/cacheops.h>
+#include <etsoc/isa/esr_defines.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/utils.h>
+//#include <transports/vq/vq.h>  // commented out due 'etsoc_rt_memory.h:53:2: error: #error "Definition for device runtime memory access not provided!"'
+#include <transports/circbuff/circbuff.h>
+#include <transports/sp_mm_iface/sp_mm_comms_spec.h>
+#include <transports/mm_cm_iface/message_types.h>
+//#include <transports/sp_mm_iface/sp_mm_iface.h> // commented out due 'etsoc_rt_memory.h:53:2: error: #error "Definition for device runtime memory access not provided!"'
+#include <transports/sp_mm_iface/sp_mm_shared_config.h>
+#include <system/layout.h>
+#include <system/etsoc_ddr_region_map.h>
+
+#include <etsoc/drivers/serial/serial.h>
+//#include <etsoc/isa/etsoc_rt_memory.h>
+
+#elif defined(TEST_PACKAGE_CM_UMODE)
 
 #include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
 #include <etsoc/isa/barriers.h>
+//#include <etsoc/isa/cacheops.h> clases with cacheops-umode.h
 #include <etsoc/isa/cacheops-umode.h>
 #include <etsoc/isa/esr_defines.h>
 #include <etsoc/isa/fcc.h>
+#include <etsoc/isa/flb.h>
 #include <etsoc/isa/hart.h>
 #include <etsoc/isa/syscall.h>
 #include <etsoc/isa/tensors.h>
 #include <etsoc/isa/utils.h>
-#include <etsoc/isa/macros.h>
-
 #include <trace/trace_umode.h>
+#include <trace/trace_umode_cb.h>
+#include <trace/trace_umode_intern.h>
 
-#include <stdio.h>
+#include <etsoc/common/utils.h>
+#include <etsoc/drivers/pmu/pmu.h>
 
-int main() { 
-    printf("SUCCESS\n");
+#elif defined(TEST_PACKAGE_MINION_BL)
+
+#include <etsoc/drivers/pmu/pmu.h>
+#include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
+#include <etsoc/isa/etsoc_memory.h>
+#include <etsoc/isa/esr_defines.h>
+#include <etsoc/isa/macros.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/flb.h>
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/io.h>
+#include <etsoc/isa/sync.h>
+#include <etsoc/isa/cacheops.h>
+#include <etsoc/isa/syscall.h>
+#include <etsoc/isa/riscv_encoding.h>
+#include <etsoc/isa/utils.h>
+#include <system/etsoc_ddr_region_map.h>
+#include <system/layout.h>
+
+#include <transports/mm_cm_iface/broadcast.h>
+
+#elif defined(TEST_PACKAGE_MM_RT_SVCS)
+
+#include <etsoc/common/common_defs.h>
+#include <etsoc/common/log_common.h>
+#include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
+#include <etsoc/isa/cacheops.h>
+#include <etsoc/isa/esr_defines.h>
+#include <etsoc/isa/etsoc_memory.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/flb.h>
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/io.h>
+#include <etsoc/isa/macros.h>
+#include <etsoc/isa/sync.h>
+#include <etsoc/isa/syscall.h>
+#include <etsoc/isa/riscv_encoding.h>
+#include <etsoc/isa/utils.h>
+#include <system/etsoc_ddr_region_map.h>
+#include <system/layout.h>
+
+#include <common/printf.h>
+#include <etsoc/drivers/serial/serial.h>
+#include <etsoc/drivers/pcie/pcie_int.h>
+#include <etsoc/drivers/pcie/pcie_device.h>
+#include <etsoc/drivers/pmu/pmu.h>
+#include <etsoc/isa/etsoc_rt_memory.h>
+#include <transports/circbuff/circbuff.h>
+#include <transports/vq/vq.h>
+#include <transports/mm_cm_iface/broadcast.h>
+#include <transports/mm_cm_iface/message_types.h>
+#include <transports/sp_mm_iface/sp_mm_comms_spec.h>
+#include <transports/sp_mm_iface/sp_mm_iface.h>
+#include <transports/sp_mm_iface/sp_mm_shared_config.h>
+
+#elif defined(TEST_PACKAGE_CM_RT_SVCS)
+
+#include <etsoc/common/common_defs.h>
+#include <etsoc/common/log_common.h>
+#include <etsoc/isa/atomic.h>
+//#include <etsoc/isa/atomic-impl.h> clashes with atomic.h
+#include <etsoc/isa/esr_defines.h>
+#include <etsoc/isa/fcc.h>
+#include <etsoc/isa/flb.h>
+#include <etsoc/isa/hart.h>
+#include <etsoc/isa/io.h>
+#include <etsoc/isa/etsoc_memory.h>
+#include <etsoc/isa/macros.h>
+#include <etsoc/isa/cacheops.h>
+#include <etsoc/isa/sync.h>
+#include <etsoc/isa/syscall.h>
+#include <etsoc/isa/riscv_encoding.h>
+#include <etsoc/isa/utils.h>
+#include <system/etsoc_ddr_region_map.h>
+#include <system/layout.h>
+
+#include <common/printf.h>
+#include <transports/circbuff/circbuff.h>
+#include <etsoc/drivers/pmu/pmu.h>
+#include <etsoc/drivers/serial/serial.h>
+#include <transports/mm_cm_iface/message_types.h>
+
+#else
+#error "we should not reach here.."
+#endif
+
+int main()
+{
     return 0;
 }
