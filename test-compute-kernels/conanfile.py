9c78d90d9f00f07ee5f11985711b2511bf38d81b
Pau Farre <pau.farre@esperantotech.com>
2022-03-08 14:50:18 +0100

    [test-compute-kernels] Adapt recipe to conan 1.46 user_toolchains

--- test-compute-kernels/conanfile.py
+++ test-compute-kernels/conanfile.py
@@ -5,6 +5,8 @@ from conans.errors import ConanInvalidConfiguration
 import os
 import textwrap
 
+required_conan_version = ">=1.46.0"
+
 
 class EsperantoTestKenelsConan(ConanFile):
     name = "esperanto-test-kernels"
@@ -33,7 +35,7 @@ class EsperantoTestKenelsConan(ConanFile):
 
     def requirements(self):
         self.requires("esperantoTrace/0.1.0")
-        self.requires("et-common-libs/0.0.4")
+        self.requires("et-common-libs/0.0.5")
 
     def package_id(self):
         self.python_requires["conan-common"].module.x86_64_compatible(self)
@@ -52,22 +54,9 @@ class EsperantoTestKenelsConan(ConanFile):
                 raise ConanInvalidConfiguration("{0} requires {1} package with '-o {1}:{2}'".format(self.name, "et-common-libs", flag))
 
     def generate(self):
-        # Get the toolchains from "tools.cmake.cmaketoolchain:user_toolchain" conf at the
-        # tool_requires
-        user_toolchains = []
-        for dep in self.dependencies.direct_build.values():
-            ut = dep.conf_info["tools.cmake.cmaketoolchain:user_toolchain"]
-            if ut:
-                user_toolchains.append(ut)
-
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_MODULE_PATH"] = os.path.join(self.dependencies.build["cmake-modules"].package_folder, "cmake")
         tc.variables["CMAKE_INSTALL_LIBDIR"] = "lib"
-
-        if user_toolchains:
-            self.output.info("Applying user_toolchains: %s" % user_toolchains)
-            tc.blocks["user_toolchain"].values["paths"] = user_toolchains
-
         tc.generate()
 
     def build(self):
