068226121529f246fc2a18fab97d4d05005a4228
Pau Farre <pau.farre@esperantotech.com>
2022-03-21 19:29:09 +0100

    [sw-sysemu] [SW-11456] Generate conan build with ecpt

---
+++ sw-sysemu/conan/ci_build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+
+from ecpt.packager import Packager
+from ecpt.builder import GitlabCiBuilder
+
+
+def main():
+    conanfile_path = os.path.join(os.path.dirname(__file__), "..", "conanfile.py")
+    
+    build = Packager(GitlabCiBuilder())
+    build.add_package(conanfile_path)
+    build.add_consumer("runtime/0.2.0@")
+    build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
+    build.report()
+
+    build.run()
+
+
+if __name__ == '__main__':
+    main()
