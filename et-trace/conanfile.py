3de954d517969bfcf50d025fe6a0f8538c6e076b
Pau Farre <pau.farre@esperantotech.com>
2022-04-08 16:10:58 +0200

    [et-trace] Improve conanfile

--- et-trace/conanfile.py
+++ et-trace/conanfile.py
@@ -28,7 +28,7 @@ class EsperantoTraceConan(ConanFile):
     python_requires = "conan-common/[>=0.5.0 <1.0.0]"
 
     def set_version(self):
-        self.version = self.python_requires["conan-common"].module.get_version_from_cmake_project(self, "esperantoTrace")
+        self.version = self.python_requires["conan-common"].module.get_version_from_cmake_project(self, self.name)
     
     def generate(self):
         tc = CMakeToolchain(self)
