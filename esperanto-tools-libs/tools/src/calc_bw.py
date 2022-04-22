62923b3137655aec62cee6f5e4ccdab10eaabf96
Javier Vera <javier.vera@esperantotech.com>
2022-04-22 11:40:57 +0200

    [esperanto-tools-libs] Fixed h2d53

--- esperanto-tools-libs/tools/src/calc_bw.py
+++ esperanto-tools-libs/tools/src/calc_bw.py
@@ -35,7 +35,7 @@ h2d53 = [
 execPath = ""
 
 def bench(h2d, d2h, dmask=1, th=2, wl=10, numh2d=1, numd2h=1, cma_size=1<<30, dl=2):
-  argos='dummy -json -wl {wl} -th {th} -h2d {h2d} -d2h {d2h} -th {th} -numh2d {numh2d} -numd2h {numd2h} -dmask {dmask} -deviceLayer {dl}'.format(dl=dl, wl=wl, th=th, dmask=dmask, h2d=h2d, d2h=d2h, numh2d=numh2d, numd2h=numd2h).split()
+  argos='dummy -json -wl {wl} -h2d {h2d} -d2h {d2h} -th {th} -numh2d {numh2d} -numd2h {numd2h} -dmask {dmask} -deviceLayer {dl}'.format(dl=dl, wl=wl, th=th, dmask=dmask, h2d=h2d, d2h=d2h, numh2d=numh2d, numd2h=numd2h).split()
   res = proc.run(env={'ET_CMA_SIZE': str(cma_size)}, executable=execPath, stdout=proc.PIPE, args=argos, encoding='utf-8').stdout
   return json.loads(res)
 
@@ -48,7 +48,7 @@ def main():
   os.access(execPath, os.X_OK) or sys.exit('provide bench tool executable')
 
   alltransfers = list(map(lambda pair: bench(pair[0], pair[1]), transfers))
-  alltransfers.extend(list(map(lambda pair: bench(pair[0], pair[1], 53), transfers)))
+  alltransfers.extend(list(map(lambda pair: bench(pair[0], pair[1], 53), h2d53)))
   print(json.dumps(alltransfers, indent=2))
 
 
