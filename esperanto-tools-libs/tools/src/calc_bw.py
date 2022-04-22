615495ac1e8ee43c40959359094601f227d299eb
Javier Vera <javier.vera@esperantotech.com>
2022-04-22 11:41:59 +0200

    [esperanto-tools-libs] changed default wl size to 100

--- esperanto-tools-libs/tools/src/calc_bw.py
+++ esperanto-tools-libs/tools/src/calc_bw.py
@@ -34,7 +34,7 @@ h2d53 = [
 
 execPath = ""
 
-def bench(h2d, d2h, dmask=1, th=2, wl=10, numh2d=1, numd2h=1, cma_size=1<<30, dl=2):
+def bench(h2d, d2h, dmask=1, th=2, wl=100, numh2d=1, numd2h=1, cma_size=1<<30, dl=2):
   argos='dummy -json -wl {wl} -h2d {h2d} -d2h {d2h} -th {th} -numh2d {numh2d} -numd2h {numd2h} -dmask {dmask} -deviceLayer {dl}'.format(dl=dl, wl=wl, th=th, dmask=dmask, h2d=h2d, d2h=d2h, numh2d=numh2d, numd2h=numd2h).split()
   res = proc.run(env={'ET_CMA_SIZE': str(cma_size)}, executable=execPath, stdout=proc.PIPE, args=argos, encoding='utf-8').stdout
   return json.loads(res)
