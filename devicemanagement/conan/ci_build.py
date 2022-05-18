0ad1f0f85ee15add4e9619f5c70fb114095a4598
Pau Farre <pau.farre@esperantotech.com>
2022-05-18 16:14:21 +0000

    [devicemanagement] Remove rt consumer checks from deviceManagement CI

--- devicemanagement/conan/ci_build.py
+++ devicemanagement/conan/ci_build.py
@@ -9,7 +9,6 @@ def main():
     
     build = Packager(ci_build=True)
     build.add_package(conanfile_path)
-    build.add_consumer("rt/0.12.0@")
     build.add_consumer("ss/0.11.0@")
     build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
     build.report()
