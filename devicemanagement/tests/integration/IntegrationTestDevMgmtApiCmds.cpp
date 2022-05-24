b8f5879bce786fdc49e61b5366a3aad0f9aaa5c8
Shankar Jayaratnam <shankar.jayaratnam@esperantotech.com>
2022-05-24 10:39:45 -0700

    [devicemanagement] SW-12549 Re-enabling MM_RESET test

--- devicemanagement/tests/integration/IntegrationTestDevMgmtApiCmds.cpp
+++ devicemanagement/tests/integration/IntegrationTestDevMgmtApiCmds.cpp
@@ -54,7 +54,7 @@ TEST_F(IntegrationTestDevMgmtApiCmds, getDeviceErrorEvents) {
   }
 }
 
-TEST_F(IntegrationTestDevMgmtApiCmds, DISABLED_resetMM) {
+TEST_F(IntegrationTestDevMgmtApiCmds, resetMM) {
   // TODO: SW-11288: Enable back when it's stable
   if (targetInList({Target::FullBoot, Target::FullChip, Target::Bemu, Target::Silicon})) {
     resetMM(false);
