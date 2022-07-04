2b0f77624ecb89616ac74efb6948f4a9f362ffe5
msalman <muhammad.salman@esperantotech.com>
2022-07-04 11:53:18 +0000

    [test-compute-kernels] [SW-13248]: Adding a fix to bus error kernel

--- test-compute-kernels/src/bus_error/bus_error.c
+++ test-compute-kernels/src/bus_error/bus_error.c
@@ -8,7 +8,7 @@ int main(void)
     /* Generate bus error on single hart */
     if (get_hart_id() == 0)
     {
-        cache_ops_prefetch_va(0, 2, 0x80000000, 16, 0x40, 0);
+        cache_ops_prefetch_va(0, 2, 0x80000000, 16, 0, 0);
     }
 
     return 0;
