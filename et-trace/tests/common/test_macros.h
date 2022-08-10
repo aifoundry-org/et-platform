3cf1292118ed6d131def80b3e5d2a62fb4ed2c91
Haseeb Ashraf <haseeb.ashraf@esperantotech.com>
2022-08-10 15:35:35 +0500

    [et-trace] Add workaround for GCC bug
    In CI, a GCC bug is identified with image
    docker-sw-team.sc-artifactory1.esperanto.ai/convoke/ubuntu-18.04-gcc7-conan:0.3.5
    
    ```
    tests/decode_mixed_test.c: In function 'check_entry_u8':
    tests/decode_mixed_test.c:48:51: internal compiler error: in linemap_macro_map_loc_to_exp_point, at libcpp/line-map.c:1072
         static void check_entry_##shtype(const struct trace_value_##shtype##_t *entry) \
                                                       ^~~~~~~~~~~~
    tests/decode_mixed_test.c:60:1: note: in expansion of macro 'CHECK_ENTRY'
     CHECK_ENTRY(u8, uint8_t)
     ^~~~~~~~~~~
    Please submit a full bug report,
    with preprocessed source if appropriate.
    See <https://gcc.gnu.org/bugs/> for instructions.
    ```
    This bug occurs because CHECK_ENTRY() macro defines function which
    requires expansion and uses macro _Pragma().
    (CHECK_EQ() -> CHECK_IMPL() -> CHECK_PRINTF() -> _Pragma())
    
    As a workaround, removing the use of _Pragma() from CHECK_PRINTF()
    and to suppress unavoidable implicit conversion from float to double
    in fprintf() adding -Wno-double-promotion exclusion in
    target_compile_options()
    
    Signed-off-by: Haseeb Ashraf <haseeb.ashraf@esperantotech.com>

--- et-trace/tests/common/test_macros.h
+++ et-trace/tests/common/test_macros.h
@@ -6,22 +6,17 @@
 #include <string.h>
 
 /* clang-format off */
-#define CHECK_PRINTF(str, a)                                      \
-    do {                                                          \
-        _Pragma("GCC diagnostic push");                           \
-        _Pragma("GCC diagnostic ignored \"-Wdouble-promotion\""); \
-        _Generic(a,                                               \
-                 int8_t:   fprintf(str, "%d",  (int8_t)a),        \
-                 int16_t:  fprintf(str, "%d",  (int16_t)a),       \
-                 int32_t:  fprintf(str, "%d",  (int32_t)a),       \
-                 int64_t:  fprintf(str, "%ld", (int64_t)a),       \
-                 uint8_t:  fprintf(str, "%u",  (uint8_t)a),       \
-                 uint16_t: fprintf(str, "%u",  (uint16_t)a),      \
-                 uint32_t: fprintf(str, "%u",  (uint32_t)a),      \
-                 uint64_t: fprintf(str, "%lu", (uint64_t)a),      \
-                 float:    fprintf(str, "%g",  (float)a));        \
-        _Pragma("GCC diagnostic pop");                            \
-    } while (0)
+#define CHECK_PRINTF(str, a)                             \
+    _Generic(a,                                          \
+             int8_t:   fprintf(str, "%d",  (int8_t)a),   \
+             int16_t:  fprintf(str, "%d",  (int16_t)a),  \
+             int32_t:  fprintf(str, "%d",  (int32_t)a),  \
+             int64_t:  fprintf(str, "%ld", (int64_t)a),  \
+             uint8_t:  fprintf(str, "%u",  (uint8_t)a),  \
+             uint16_t: fprintf(str, "%u",  (uint16_t)a), \
+             uint32_t: fprintf(str, "%u",  (uint32_t)a), \
+             uint64_t: fprintf(str, "%lu", (uint64_t)a), \
+             float:    fprintf(str, "%g",  (float)a))
 /* clang-format on */
 
 #define CHECK_IMPL(file, line, a, b, cmp)                     \
