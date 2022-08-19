4bc30c0a060f22deb7fb3c001562abae7a6d34f3
Ying Hao Xu Lin <yinghao.xu@esperantotech.com>
2022-08-19 01:07:10 -0700

    [test-compute-kernels] Fix SCW Power Virus
    Main changes:
    - Limit test execution to Shire0..Shire31, as Shire32 is used by the SW kernel
    
    Shire32 Neigh2 and Neigh3 can be used too, however, the function to drain the Coalescing Buffer
    expects 32 Minions per shire, so to keep things simple, we avoid using the whole Shire 32

--- test-compute-kernels/src/scw_power_virus/scw_power_virus.c
+++ test-compute-kernels/src/scw_power_virus/scw_power_virus.c
@@ -12,7 +12,12 @@ typedef struct {
 } Parameters;
 
 int main(const Parameters* const kernel_params_ptr) {
-    uint64_t ret = shire_cache_power_virus(kernel_params_ptr->loop_size);
-    if (ret == 0) return 0;
-    else return -1;
+    uint64_t sid = get_shire_id();
+    /* Limit SCW test to only Shire0..Shire31 */
+    if (sid < 32) {
+        uint64_t ret = shire_cache_power_virus(kernel_params_ptr->loop_size);
+        if (ret == 0) return 0;
+        else return -1;
+    }
+    return 0;
 }
