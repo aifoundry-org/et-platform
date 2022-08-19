4d21632567a65160cf52591b4b33cdbc88b63e79
Mahad Saleem <Mahad_Saleem@mentor.com>
2022-08-19 19:52:37 +0500

    [test-compute-kernels] [SW-13248] Modifying bus error kernel to access non-existent ESR in U-mode.
    Signed-off-by: Mahad Saleem <Mahad_Saleem@mentor.com>

--- test-compute-kernels/src/bus_error/bus_error.c
+++ test-compute-kernels/src/bus_error/bus_error.c
@@ -1,6 +1,11 @@
 #include <stdint.h>
-#include <etsoc/isa/cacheops-umode.h>
+#include <etsoc/isa/esr_defines.h>
 #include <etsoc/isa/hart.h>
+
+/* Defines for dummy MPROT in U-mode */
+#define ESR_NEIGH_DUMMY_MPROT_REGNO  0x4
+#define ESR_NEIGH_DUMMY_MPROT_PROT   PRV_U
+#define ESR_NEIGH_DUMMY_MPROT_ACCESS esr_access_RW
 
 /* Generates bus error */
 int main(void)
@@ -8,7 +13,8 @@ int main(void)
     /* Generate bus error on single hart */
     if (get_hart_id() == 0)
     {
-        cache_ops_prefetch_va(0, 2, 0x80000000, 16, 0, 0);
+        /* Access a non-existent ESR. According to spec, it would generate a bus error */
+        volatile uint64_t dummy = *((volatile uint64_t *)ESR_NEIGH(THIS_SHIRE, 0, DUMMY_MPROT));
     }
 
     return 0;
