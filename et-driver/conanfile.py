4a4a5193ae1f874c01ac7289b3e8e8ede346788d
Haseeb Ashraf <haseeb.ashraf@esperantotech.com>
2022-10-05 15:00:31 +0500

    [et-driver] Assign same version to conan package as set in VERSION file
    Signed-off-by: Haseeb Ashraf <haseeb.ashraf@esperantotech.com>

--- et-driver/conanfile.py
+++ et-driver/conanfile.py
@@ -1,8 +1,8 @@
 from conan import ConanFile
+import os
 
 class UserspaceEtDriverConan(ConanFile):
     name = "linuxDriver"
-    version = "0.1.0"
     # No settings/options are necessary, this is header only
     exports_sources = "et_ioctl.h"
     no_copy_source = True
@@ -12,3 +12,7 @@ class UserspaceEtDriverConan(ConanFile):
     
     def package_id(self):
         self.info.header_only()
+
+    def set_version(self):
+        with open(os.path.join(self.recipe_folder, "VERSION"), 'r') as f:
+            self.version = f.readline().strip()
