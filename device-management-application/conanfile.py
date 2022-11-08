9c5df1958960c896a6e1c00e914102cd24f7ea82
Pau Farre <pau.farre@esperantotech.com>
2022-11-08 11:01:03 +0100

    [device-management-application] Fix VirtualRunEnv usage + modernize recipe to conan 2.0
    Do not define runenv_info with PATH as this seems broken. Let conan
    automatically detect `bin` folder and define PATH/LD_LIBRARY_PATH
    - This change fixes incorrect PATH/LD_LIBRARY_PATH generated in conanrunenv files.
    
    Modernize recipe to conan 2.0

--- device-management-application/conanfile.py
+++ device-management-application/conanfile.py
@@ -1,8 +1,10 @@
-from conans import ConanFile, tools
-from conan.tools.cmake import CMake, CMakeToolchain
-from conan.tools.layout import cmake_layout
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+from conan.tools.files import rmdir
 import os
+
+required_conan_version = ">=1.52.0"
+
 
 class DeviceManagementApplicationConan(ConanFile):
     name = "deviceManagementApplication"
@@ -23,13 +25,16 @@ class DeviceManagementApplicationConan(ConanFile):
         "url": "git@gitlab.esperanto.ai:software/device-management-application.git",
         "revision": "auto",
     }
-    generators = "CMakeDeps"
 
     python_requires = "conan-common/[>=0.1.0 <1.0.0]"
 
     def set_version(self):
         self.version = self.python_requires["conan-common"].module.get_version_from_cmake_project(self, self.name)
 
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+    
     def configure(self):
         check_req_min_cppstd = self.python_requires["conan-common"].module.check_req_min_cppstd
         check_req_min_cppstd(self, "17")
@@ -50,9 +55,11 @@ class DeviceManagementApplicationConan(ConanFile):
     def generate(self):
         tc = CMakeToolchain(self)
         tc.variables["CMAKE_INSTALL_LIBDIR"] = "lib"
-        tc.variables["CMAKE_VERBOSE_MAKEFILE"] = True
         tc.generate()
     
+        deps = CMakeDeps(self)
+        deps.generate()
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
@@ -62,14 +69,15 @@ class DeviceManagementApplicationConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
     
     def package_info(self):
-        self.cpp_info.libs = tools.collect_libs(self)
+        # this pkg only contains executables
+        self.cpp_info.frameworkdirs = []
+        self.cpp_info.libdirs = []
+        self.cpp_info.resdirs = []
+        self.cpp_info.includedirs = []
 
-        # utilities
-        bin_path = os.path.join(self.package_folder, "bin")
-
-        self.output.info("Appending PATH env var with : {}".format(bin_path))
-        self.runenv_info.append("PATH", bin_path)
-        self.env_info.PATH.append(bin_path) # deprecated remove in Conan 2.0.0
+        # TODO: Legacy, to be removed on Conan 2.0
+        bin_folder = os.path.join(self.package_folder, "bin")
+        self.env_info.PATH.append(bin_folder)
