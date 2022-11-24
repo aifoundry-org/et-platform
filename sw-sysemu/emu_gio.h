25a1a6033284e759b9249e771ee6b8c8b4ad5d56
Calvin Bulla <calvin.bulla@esperantotech.com>
2022-11-24 06:41:12 -0800

    [sw-sysemu] SysEmu: Disable logging when not in U-mode
    The new `-DONLY_LOG_UMODE` option will disable all logs except:
     - Global logs (system, checkers, etc.), and
     - Hart logs when in U-mode
    
    See also: ARCHSIM-694

--- sw-sysemu/emu_gio.h
+++ sw-sysemu/emu_gio.h
@@ -19,46 +19,53 @@
 #include "system.h"
 #include "testLog.h"
 
+#ifdef ONLY_LOG_UMODE
+#define HART_LOG_EN(hart) \
+    (hart).chip->log_thread[bemu::hart_index(hart)] && ((hart).prv == bemu::Privilege::U)
+#else
+#define HART_LOG_EN(hart) \
+    (hart).chip->log_thread[bemu::hart_index(hart)]
+#endif
+
 //! Log for a given hart, if enabled.
-#define LOG_HART(severity, hart, format, ...)                                  \
-    do {                                                                       \
-        assert((hart).chip);                                                   \
-        if (LOG_##severity >= (hart).chip->log.getLogLevel()                   \
-            && (hart).chip->log_thread[bemu::hart_index(hart)])                \
-            bemu::lprintf(LOG_##severity, (hart), format, __VA_ARGS__);        \
+#define LOG_HART(severity, hart, format, ...)                           \
+    do {                                                                \
+        assert((hart).chip);                                            \
+        if (LOG_##severity >= (hart).chip->log.getLogLevel()            \
+            && HART_LOG_EN(hart))                                       \
+            bemu::lprintf(LOG_##severity, (hart), format, __VA_ARGS__); \
     } while (0)
 
 //! Log for a given agent.
-#define LOG_AGENT(severity, agent, format, ...)                                \
-    do {                                                                       \
-        assert((agent).chip);                                                  \
-        if (LOG_##severity >= (agent).chip->log.getLogLevel())                 \
-            bemu::lprintf(LOG_##severity, (agent), format, __VA_ARGS__);       \
+#define LOG_AGENT(severity, agent, format, ...)                          \
+    do {                                                                 \
+        assert((agent).chip);                                            \
+        if (LOG_##severity >= (agent).chip->log.getLogLevel())           \
+            bemu::lprintf(LOG_##severity, (agent), format, __VA_ARGS__); \
     } while (0)
 
 //! Log a warning/error for a given hart, depending on settings.
-#define WARN_HART(category, hart, format, ...)                                 \
-    do {                                                                       \
-        assert((hart).chip);                                                   \
-        const logLevel severity                                                \
-            = (hart).chip->warning.severity(bemu::Warning::category);          \
-        if (severity >= (hart).chip->log.getLogLevel()                         \
-            && (hart).chip->log_thread[bemu::hart_index(hart)])                \
-            bemu::lprintf(severity, (hart), format, __VA_ARGS__);              \
+#define WARN_HART(category, hart, format, ...)                        \
+    do {                                                              \
+        assert((hart).chip);                                          \
+        const logLevel severity                                       \
+            = (hart).chip->warning.severity(bemu::Warning::category); \
+        if (severity >= (hart).chip->log.getLogLevel()                \
+            && HART_LOG_EN(hart))                                     \
+            bemu::lprintf(severity, (hart), format, __VA_ARGS__);     \
     } while (0)
 
 //! Log a warning/error for a given agent, depending on settings.
-#define WARN_AGENT(category, agent, format, ...)                               \
-    do {                                                                       \
-        assert((agent).chip);                                                  \
-        const logLevel severity                                                \
-            = (agent).chip->warning.severity(bemu::Warning::category);         \
-        if (severity >= (agent).chip->log.getLogLevel())                       \
-            bemu::lprintf(severity, (agent), format, __VA_ARGS__);             \
+#define WARN_AGENT(category, agent, format, ...)                       \
+    do {                                                               \
+        assert((agent).chip);                                          \
+        const logLevel severity                                        \
+            = (agent).chip->warning.severity(bemu::Warning::category); \
+        if (severity >= (agent).chip->log.getLogLevel())               \
+            bemu::lprintf(severity, (agent), format, __VA_ARGS__);     \
     } while (0)
 
-namespace bemu
-{
+namespace bemu {
 
 //! Format and print log message for a given agent.
 void lprintf(logLevel level, const Agent& agent, const char* fmt, ...)
