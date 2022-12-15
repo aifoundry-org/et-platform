31e57745ffd400cd517070b6528f49033c980eeb
Mahad Saleem <Mahad_Saleem@mentor.com>
2022-12-15 16:27:32 +0500

    [test-compute-kernels] Disabling SS bundle phase to break dependency chain.
    Signed-off-by: Mahad Saleem <Mahad_Saleem@mentor.com>

--- test-compute-kernels/conan/ci_build.py
+++ test-compute-kernels/conan/ci_build.py
@@ -6,10 +6,11 @@ from ecpt.packager import Packager
 
 def main():
     conanfile_path = os.path.join(os.path.dirname(__file__), "..", "conanfile.py")
-    
+
     build = Packager(ci_build=True)
     build.add_package(conanfile_path)
-    build.add_consumer("ss/0.11.0@")
+    # Disabling SS bundle phase to break dependency chain.
+    #build.add_consumer("ss/0.11.0@")
     build.add_configuration("default", "baremetal-rv64-gcc8.2-release")
     build.report()
 
--- test-compute-kernels/conan/ci_promote.py
+++ test-compute-kernels/conan/ci_promote.py
@@ -6,7 +6,7 @@ from ecpt.packager import Packager
 
 def main():
     conanfile_path = os.path.join(os.path.dirname(__file__), "..", "conanfile.py")
-    
+
     build = Packager()
     build.promote("lockfiles_info.json")
 
