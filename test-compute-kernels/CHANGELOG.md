094e1754a61ec4046add1c5b729d5c7768ca68e9
Mahad Saleem <Mahad_Saleem@mentor.com>
2023-03-03 22:04:56 +0500

    [test-compute-kernels] [SW-16253] Added environment kernel for verifying the kernel env params.
    Signed-off-by: Mahad Saleem <Mahad_Saleem@mentor.com>

--- test-compute-kernels/CHANGELOG.md
+++ test-compute-kernels/CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 ### Added
+- [SW-16253] Added environment kernel for verifying the kernel env params.
 ### Changed
 ### Deprecated
 ### Removed
---
+++ test-compute-kernels/src/environment/environment.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <etsoc/common/utils.h>
+#include <trace/trace_umode.h>
+#include <system/abi.h>
+
+typedef struct {
+  uint64_t shire_mask;
+} kernel_args_t;
+
+int64_t main(const kernel_args_t *kernel_args, const kernel_environment_t *kernel_env)
+{
+    /* Print the args and env */
+    et_printf("Kernel Args:shire_mask: 0x%lx\r\n", kernel_args->shire_mask);
+    et_printf("Kernel Environment:ABI ver:%u.%u.%u:shire_mask:0x%lx:frequency:%u\n\r",
+        kernel_env->version.major, kernel_env->version.minor, kernel_env->version.patch,
+        kernel_env->shire_mask, kernel_env->frequency);
+
+    /* Verify the kernel env parameters */
+    if (kernel_args->shire_mask != kernel_env->shire_mask)
+    {
+        return -1;
+    }
+    else if ((kernel_env->version.major == 0) && (kernel_env->version.minor == 0) && (kernel_env->version.patch == 0))
+    {
+        return -1;
+    }
+
+    return 0;
+}
