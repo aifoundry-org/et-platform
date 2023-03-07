5c50302ff92364d0ee254bb7f675974892d31d4a
Marc Lupon <marc.lupon@esperantotech.com>
2023-03-07 11:43:13 +0100

    [devicelayer] Do not rebuild system-sw components that consume deviceLayer

--- devicelayer/conan/ci_build.py
+++ devicelayer/conan/ci_build.py
@@ -12,7 +12,6 @@ def main():
     c1 = build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
     c2 = build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-debug")
     build.add_consumer("rt/0.12.0@", config_ids=[c1])
-    build.add_consumer("ss/0.11.0@", config_ids=[c1])
     build.report()
 
     build.run()
