d693dbbaa851141e44ce29b3e1c530a5c674e768
Pau Farre <pau.farre@esperantotech.com>
2023-03-14 10:51:28 +0100

    [device-api] Improve deviceApi test_package

--- device-api/test_package/test_package.cpp
+++ device-api/test_package/test_package.cpp
@@ -3,6 +3,11 @@
 #include "device-apis/management-api/device_mgmt_api_cxx.h"
 #include "device-apis/operations-api/device_ops_api_cxx.h"
 
+// should also be able to include esperanto prefixed include path
+#include "esperanto/device-apis/device_apis_message_types.h"
+#include "esperanto/device-apis/management-api/device_mgmt_api_cxx.h"
+#include "esperanto/device-apis/operations-api/device_ops_api_cxx.h"
+
 #include <iostream>
 
 int main() {
