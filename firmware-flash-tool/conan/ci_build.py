e6a6fab562748628f28d1def223ff9acc75e91c4
Pau Farre <pau.farre@esperantotech.com>
2023-03-14 17:16:13 +0100

    [firmware-flash-tool] Disable rebuild pipeline

--- firmware-flash-tool/conan/ci_build.py
+++ firmware-flash-tool/conan/ci_build.py
@@ -11,8 +11,8 @@ def main():
     build.add_package(conanfile_path)
     c1 = build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
     c2 = build.add_configuration("default", "baremetal-rv64-gcc8.2-release")
-    build.add_consumer("rt/0.12.0@", config_ids=[c1])
-    build.add_consumer("ss/0.11.0@", config_ids=[c1, c2])
+    #build.add_consumer("rt/0.12.0@", config_ids=[c1])
+    #build.add_consumer("ss/0.11.0@", config_ids=[c1, c2])
     build.report()
 
     build.run()
