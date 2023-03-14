d6ed76a258743e1acaeb09df37106633580bc4bc
Pau Farre <pau.farre@esperantotech.com>
2023-03-14 10:48:53 +0100

    [device-api] [SW-16448] Generate pre-releases and enable tags pipelines

--- device-api/conanfile.py
+++ device-api/conanfile.py
@@ -1,10 +1,8 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps
-from conans import tools
+from conan.tools.files import rmdir
 import os
-import re
-import sys
-import subprocess
+
 
 class DeviceApiConan(ConanFile):
     name = "deviceApi"
@@ -18,19 +16,20 @@ class DeviceApiConan(ConanFile):
         "url": "git@gitlab.esperanto.ai:software/device-api.git",
         "revision": "auto",
     }
-    generator = "CMakeDeps"
 
-    python_requires = "conan-common/[>=0.5.0 <1.0.0]"
-
+    python_requires = "conan-common/[>=1.1.0 <2.0.0]"
 
     def set_version(self):
-        self.version = self.python_requires["conan-common"].module.get_version_from_cmake_project(self, "deviceApi")
+        get_version = self.python_requires["conan-common"].module.get_version
+        self.version = get_version(self, self.name)
 
     def generate(self):
         tc = CMakeToolchain(self)
         # not including documentation for now
         tc.variables["DEVICEAPI_BUILD_DOC"] = False
         tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
 
     def build(self):
         cmake = CMake(self)
@@ -40,7 +39,7 @@ class DeviceApiConan(ConanFile):
     def package(self):
         cmake = CMake(self)
         cmake.install()
-        tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         
     def package_id(self):
         self.info.header_only()
