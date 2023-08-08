2dbaa574d1df98c8c2606caf542f057dd79e6de0
Marc Lupon <marc.lupon@esperantotech.com>
2023-08-08 17:41:55 +0200

    [et-driver] [SW-18139] Change gitlab-ci to generate pre-releases and versions through tags

--- et-driver/conanfile.py
+++ et-driver/conanfile.py
@@ -7,6 +7,8 @@ class UserspaceEtDriverConan(ConanFile):
     exports_sources = "et_ioctl.h"
     no_copy_source = True
 
+    python_requires = "conan-common/[>=1.1.0 <2.0.0]"
+
     def package(self):
         self.copy("*.h", dst="include")
     
@@ -14,5 +16,7 @@ class UserspaceEtDriverConan(ConanFile):
         self.info.header_only()
 
     def set_version(self):
-        with open(os.path.join(self.recipe_folder, "VERSION"), 'r') as f:
-            self.version = f.readline().strip()
+        version_file = os.path.join(self.recipe_folder, "VERSION")
+        get_version = self.python_requires["conan-common"].module.get_version
+        self.version = get_version(self, self.name, version_file)
+
