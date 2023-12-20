a21fe96e765a14e4d0e087880aa797ff3645489c
muhammad.kamran4 <muhammad.kamran@esperantotech.com>
2023-12-20 12:40:18 +0100

    [test-compute-kernels] [SW-19533] Use different stack_used for gcc and clang

--- test-compute-kernels/src/custom_stack/custom_stack.c
+++ test-compute-kernels/src/custom_stack/custom_stack.c
@@ -13,15 +13,21 @@ int64_t entry_point(const custom_stack_params_t *const kernel_params)
     if (kernel_params->stack_size > 0)
     {
         uint64_t *ptr;
+        uint64_t stack_used;
         asm volatile("mv %[ptr], sp  \n"
                     : [ptr] "=r"(ptr));
 
         et_printf("Stack size received: 0x%lx Stack ptr:0x%lx\r\n", kernel_params->stack_size, (uint64_t)(uintptr_t)ptr);
 
-        /* Allocate on stack and do a dummy copy
-        Allocate Half */
-        __attribute__((aligned(64))) char arr[(kernel_params->stack_size)/2];
-        et_memset(arr, 'A', (kernel_params->stack_size)/2);
+        /* Allocate on stack and do a dummy copy */
+#ifdef __clang__
+        stack_used = 1408;
+#else
+        stack_used = 64;
+#endif
+        /* Allocate stack_size - stack already used */
+        __attribute__((aligned(64))) char arr[kernel_params->stack_size - stack_used];
+        et_memset(arr, 'A', kernel_params->stack_size - stack_used);
     }
     else
     {
