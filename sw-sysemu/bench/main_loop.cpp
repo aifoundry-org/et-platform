81ec032bf39e801483512550933afc0b9946d66a
Pau Farre <pau.farre@esperantotech.com>
2024-02-14 14:25:36 +0000

    [sw-sysemu] Do not avoid benchmark loop + make path to elf binaries more generic

--- sw-sysemu/bench/main_loop.cpp
+++ sw-sysemu/bench/main_loop.cpp
@@ -1,13 +1,16 @@
 #include <benchmark/benchmark.h>
+#include <cstdlib>
+
 #include "sys_emu.h"
+#include "elfs.h"
 
 // Define the list of ELF files to preload
 std::vector<std::string> preload_elfs_list = {
-     "./build/Release/device-bootloaders/lib/esperanto-fw/BootromTrampolineToBL2/BootromTrampolineToBL2.elf",
-     "./build/Release/device-bootloaders/lib/esperanto-fw/ServiceProcessorBL2/fast-boot/ServiceProcessorBL2_fast-boot.elf",
-     "./build/Release/device-minion-runtime/lib/esperanto-fw/MachineMinion/MachineMinion.elf",
-     "./build/Release/device-minion-runtime/lib/esperanto-fw/MasterMinion/MasterMinion.elf",
-     "./build/Release/device-minion-runtime/lib/esperanto-fw/WorkerMinion/WorkerMinion.elf"
+    std::string{PROJECT_BINARY_DIR} + std::string{"/device-bootloaders/lib/esperanto-fw/BootromTrampolineToBL2/BootromTrampolineToBL2.elf"},
+    std::string{PROJECT_BINARY_DIR} + std::string{"/device-bootloaders/lib/esperanto-fw/ServiceProcessorBL2/fast-boot/ServiceProcessorBL2_fast-boot.elf"},
+    std::string{PROJECT_BINARY_DIR} + std::string{"/device-minion-runtime/lib/esperanto-fw/MachineMinion/MachineMinion.elf"},
+    std::string{PROJECT_BINARY_DIR} + std::string{"/device-minion-runtime/lib/esperanto-fw/MasterMinion/MasterMinion.elf"},
+    std::string{PROJECT_BINARY_DIR} + std::string{"/device-minion-runtime/lib/esperanto-fw/WorkerMinion/WorkerMinion.elf"}
 };
 
 static void BM_main_internal(benchmark::State& state) {
@@ -18,12 +21,16 @@ static void BM_main_internal(benchmark::State& state) {
         cmd_options.elf_files.push_back(elf_file);
     }
 
-    std::unique_ptr<sys_emu> emu(new sys_emu(cmd_options));
-    
-    // Run the benchmark
-    int status = emu.get()->main_internal();
+    auto emu = std::make_unique<sys_emu>(cmd_options);
 
-    std::cout << "Status " << status << std::endl; 
+    int status;
+    for (auto _ : state) {
+        // Run the benchmark
+        status = emu->main_internal();
+        assert(status == EXIT_SUCCESS);
+    }
+
+    std::cout << "Status " << status << std::endl;
 };
 
 // Register the function as a benchmark
