399932267c033150f9079e247faeae451a4392b9
Pau Farre <pau.farre@esperantotech.com>
2024-06-10 10:55:26 +0000

    [sw-sysemu] Do not generate packages ubuntu18

--- sw-sysemu/conan/ci_build.py
+++ sw-sysemu/conan/ci_build.py
@@ -9,8 +9,6 @@ def main():
 
     build = Packager(ci_build=True)
     build.add_package(conanfile_path)
-    c1 = build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-release")
-    c2 = build.add_configuration("default", "linux-ubuntu18.04-x86_64-gcc7-debug")
     c3 = build.add_configuration("default", "linux-ubuntu22.04-x86_64-gcc11-release")
     # build.add_consumer("rt/0.12.0@", config_ids=[c1])
     build.report()
