381e6733c04c0b084c926031ad63bfcae2eaf0e3
Guillem Ferrer <guillem.ferrer@esperantotech.com>
2024-06-17 09:38:43 +0000

    [esperanto-tools-libs] Fix code-smell by using std::array instead of C-style array

--- esperanto-tools-libs/src/server/Server.cpp
+++ esperanto-tools-libs/src/server/Server.cpp
@@ -55,13 +55,14 @@ Server::Server(const std::string& socketPath, std::unique_ptr<dev::IDeviceLayer>
   : deviceLayer_{std::move(deviceLayer)} {
 
   cap_t caps;
-  cap_value_t capList[1] = {CAP_SYS_PTRACE};
+  std::array<cap_value_t, 1> capList = {CAP_SYS_PTRACE};
+  cap_value_t* p_capList = capList.data();
 
   caps = cap_get_proc();
   if (caps == nullptr) {
     throw Exception("Can't get process capabilities." + std::string{strerror(errno)});
   }
-  if (cap_set_flag(caps, CAP_EFFECTIVE, 1, capList, CAP_SET) == -1) {
+  if (cap_set_flag(caps, CAP_EFFECTIVE, 1, p_capList, CAP_SET) == -1) {
     throw Exception("Can't set flag for enabling CAP_SYS_PTRACE. " + std::string{strerror(errno)});
   }
 
