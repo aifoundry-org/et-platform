4b4df16fec3b7d6dc7bc7b22e1a451740205188a
Visnja Krsmanovic <visnja.krsmanovic@esperantotech.com>
2024-09-25 13:06:32 +0000

    [device-minion-runtime] Update conan dependencies versions in conanfile.py to released components

--- device-minion-runtime/conanfile.py
+++ device-minion-runtime/conanfile.py
@@ -42,12 +42,12 @@ class DeviceMinionRuntimeConan(ConanFile):
 
     def requirements(self):
         # header-only libs
-        self.requires("deviceApi/2.4.0")
+        self.requires("deviceApi/2.5.0")
         self.requires("esperantoTrace/2.1.0")
         self.requires("signedImageFormat/1.4.0")
         # libs
         self.requires("etsoc_hal/1.7.0")
-        self.requires("et-common-libs/0.23.0")
+        self.requires("et-common-libs/0.24.0")
 
     def package_id(self):
         self.python_requires["conan-common"].module.x86_64_compatible(self)
