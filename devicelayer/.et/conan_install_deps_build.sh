773a835d3b5676fb0b30c4a9bc1a4f2c1aec4d0a
Pau Farre <pau.farre@esperantotech.com>
2024-12-10 09:46:31 +0000

    [devicelayer] Improve conan_install_deps_build.sh script

--- devicelayer/.et/conan_install_deps_build.sh
+++ devicelayer/.et/conan_install_deps_build.sh
@@ -1,3 +1,3 @@
 rm -rf build/
-conan install . -pr:b=default -pr:h=linux-ubuntu22.04-x86_64-gcc11-debug-shared   --build missing -o deviceLayer:with_tests=True
-conan install . -pr:b=default -pr:h=linux-ubuntu22.04-x86_64-gcc11-release-shared --build missing
+conan install . -pr:b=default -pr:h=linux-ubuntu22.04-x86_64-gcc11-debug-shared   -o deviceLayer:fvisibility=default --build missing -o deviceLayer:with_tests=True
+conan install . -pr:b=default -pr:h=linux-ubuntu22.04-x86_64-gcc11-release-shared -o deviceLayer:fvisibility=hidden  --build missing
